e32fdb4166690e4330cc513ce55c3d5e1e50c34e
Thomas Roessler <roessler@does-not-exist.org>
1998-08-28 00:58:25 +0000

    [patch-0.94.4i.tlr.get_attachment.1-2] Get the attachment time stamping business right.

--- attach.c
+++ attach.c
@@ -46,6 +46,7 @@ int mutt_get_tmp_attachment (BODY *a)
   char tempfile[_POSIX_PATH_MAX];
   rfc1524_entry *entry = rfc1524_new_entry();
   FILE *fpin = NULL, *fpout = NULL;
+  struct stat st;
   
   if(a->unlink)
     return 0;
@@ -56,6 +57,9 @@ int mutt_get_tmp_attachment (BODY *a)
 			  tempfile, sizeof(tempfile));
   
   rfc1524_free_entry(&entry);
+
+  if(stat(a->filename, &st) == -1)
+    return -1;
   
   if((fpin = fopen(a->filename, "r")) && (fpout = safe_fopen(tempfile, "w")))
   {
@@ -63,6 +67,9 @@ int mutt_get_tmp_attachment (BODY *a)
     FREE(&a->filename);
     a->filename = safe_strdup(tempfile);
     a->unlink = 1;
+
+    if(a->stamp >= st.st_mtime)
+      mutt_stamp_attachment(a);
   }
   else
     mutt_perror(fpin ? tempfile : a->filename);
