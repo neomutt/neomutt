fee6584e5b0328e115f358f6670ca0c50d842302
Thomas Roessler <roessler@does-not-exist.org>
1998-09-15 10:54:46 +0000

    [patch-0.94.6i.gt.arrow.1] Add M_ARROWCURSOR on the compose menu.

--- compose.c
+++ compose.c
@@ -75,7 +75,7 @@ void snd_entry (char *b, size_t blen, MUTTMENU *menu, int num)
 {
     mutt_FormatString (b, blen, NONULL (AttachFormat), mutt_attach_fmt,
 	    (unsigned long)(((ATTACHPTR **) menu->data)[num]),
-	    M_FORMAT_STAT_FILE);
+	    M_FORMAT_STAT_FILE | M_FORMAT_ARROWCURSOR);
 }
 
 
