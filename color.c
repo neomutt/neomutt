9bcec3b759067fe779f1af19c69c41d426c9ca89
Thomas Roessler <roessler@does-not-exist.org>
1998-10-14 08:32:35 +0000

    Adding some necessary ifdefs to color.c, From Irving_Wolfe@wolfe.net.

--- color.c
+++ color.c
@@ -754,9 +754,14 @@ int mutt_parse_mono(BUFFER *buff, BUFFER *s, unsigned long data, BUFFER *err)
 {
   int dry_run = 0;
   
+#ifdef HAVE_COLOR
   if(option(OPTNOCURSES) || has_colors())
     dry_run = 1;
-  
+#else
+  if(option(OPTNOCURSES))
+    dry_run = 1;
+#endif
+
   return _mutt_parse_color(buff, s, err, parse_attr_spec, dry_run);
 }
 
