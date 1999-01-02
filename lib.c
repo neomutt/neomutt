4c54db9cd376c24f50ce5309b64da08db6cba2aa
Thomas Roessler <roessler@does-not-exist.org>
1999-01-02 09:11:35 +0000

    Fix safe_symlink to correctly handle relative paths.

--- lib.c
+++ lib.c
@@ -691,9 +691,25 @@ int safe_symlink(const char *oldpath, const char *newpath)
   if(unlink(newpath) == -1 && errno != ENOENT)
     return -1;
   
-  if(symlink(oldpath, newpath) == -1)
+  if (oldpath[0] == '/')
+  {
+    if (symlink (oldpath, newpath) == -1)
+      return -1;
+  }
+  else
+  {
+    char abs_oldpath[_POSIX_PATH_MAX];
+
+    if ((getcwd (abs_oldpath, sizeof abs_oldpath) == NULL) ||
+	(strlen (abs_oldpath) + 1 + strlen (oldpath) + 1 > sizeof abs_oldpath))
     return -1;
   
+    strcat (abs_oldpath, "/");
+    strcat (abs_oldpath, oldpath);
+    if (symlink (abs_oldpath, newpath) == -1)
+      return -1;
+  }
+
   if(stat(oldpath, &osb) == -1 || stat(newpath, &nsb) == -1
      || compare_stat(&osb, &nsb) == -1)
   {
