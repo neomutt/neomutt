881e37b6b00b3d1ec77badaace343d11f9a2bbb6
Thomas Roessler <roessler@does-not-exist.org>
1999-01-08 10:43:17 +0000

    Don't clear the history when "set history=..." doesn't change the buffer size.

--- history.c
+++ history.c
@@ -56,6 +56,9 @@ void mutt_init_history(void)
 {
   history_class_t hclass;
   
+  if (HistSize == OldSize)
+    return;
+  
   for(hclass = HC_FIRST; hclass < HC_LAST; hclass++)
     init_history(&History[hclass]);
 
