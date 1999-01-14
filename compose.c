7c8508c59fa90ae77ca7b35f82bf07d6ad77094b
Thomas Roessler <roessler@does-not-exist.org>
1999-01-14 13:08:46 +0000

    Mixmaster user interface beautification.

--- compose.c
+++ compose.c
@@ -225,27 +225,31 @@ static int pgp_send_menu (int bits, int *redraw)
 static void redraw_mix_line (LIST *chain)
 {
   int c;
+  char *t;
 
   mvaddstr (HDR_MIX, 0,     "     Mix: ");
 
   if (!chain)
   {
-    addstr ("no");
+    addstr ("<no chain defined>");
     clrtoeol ();
     return;
   }
   
   for (c = 12; chain; chain = chain->next)
   {
-    if (c + mutt_strlen ((char *) chain->data) + 2 >= COLS)
+    t = chain->data;
+    if (t && t[0] == '0' && t[1] == '\0')
+      t = "<random>";
+    
+    if (c + mutt_strlen (t) + 2 >= COLS)
       break;
 
-    
-    addstr (NONULL((char *) chain->data));
+    addstr (NONULL(t));
     if (chain->next)
       addstr (", ");
 
-    c += mutt_strlen ((char *) chain->data) + 2;
+    c += mutt_strlen (t) + 2;
   }
 }
 #endif
