9d8b554b7227bfa2bff1947edb6d7d92e1a3a5dc
Thomas Roessler <roessler@does-not-exist.org>
1999-02-28 08:29:59 +0000

    Fix a memory leak. From Liviu.

--- rfc822.c
+++ rfc822.c
@@ -301,7 +301,10 @@ add_addrspec (ADDRESS **top, ADDRESS **last, const char *phrase,
   ADDRESS *cur = rfc822_new_address ();
   
   if (parse_addr_spec (phrase, comment, commentlen, commentmax, cur) == NULL)
+  {
+    rfc822_free_address (&cur);
     return;
+  }
 
   if (*last)
     (*last)->next = cur;
