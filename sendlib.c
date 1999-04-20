01da42c4891edbaad96b4a05997d953bf84990f9
Thomas Roessler <roessler@does-not-exist.org>
1999-04-20 19:26:08 +0000

    Fix a time zone snafu.

--- sendlib.c
+++ sendlib.c
@@ -1153,10 +1153,12 @@ char *mutt_make_date (char *s, size_t len)
   struct tm *l = localtime (&t);
   time_t tz = mutt_local_tz (t);
 
+  tz /= 60;
+
   snprintf (s, len,  "Date: %s, %d %s %d %02d:%02d:%02d %+03d%02d\n",
 	    Weekdays[l->tm_wday], l->tm_mday, Months[l->tm_mon],
 	    l->tm_year + 1900, l->tm_hour, l->tm_min, l->tm_sec,
-	    tz / 3600, abs (tz) % 3600);
+	    tz / 60, abs (tz) % 60);
   return (s);
 }
 
