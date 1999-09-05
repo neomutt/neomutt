a191906fa43c94b817038b61a1fcfbed3473cf3c
Thomas Roessler <roessler@does-not-exist.org>
1999-09-05 08:29:18 +0000

    Fix "unignore *": We must not add "*" to the unignore list.

--- init.c
+++ init.c
@@ -364,7 +364,11 @@ static int parse_unignore (BUFFER *buf, BUFFER *s, unsigned long data, BUFFER *e
   do
   {
     mutt_extract_token (buf, s, 0);
-    add_to_list (&UnIgnore, buf->data);
+
+    /* don't add "*" to the unignore list */
+    if (strcmp (buf->data, "*")) 
+      add_to_list (&UnIgnore, buf->data);
+
     remove_from_list (&Ignore, buf->data);
   }
   while (MoreArgs (s));
