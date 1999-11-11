43e425ee2691575ae303958f64cb10cd2d050f38
Thomas Roessler <roessler@does-not-exist.org>
1999-11-11 18:22:07 +0000

    Fix a header quoting bug.

--- rfc822.c
+++ rfc822.c
@@ -130,14 +130,17 @@ parse_quote (const char *s, char *token, size_t *tokenlen, size_t tokenmax)
   while (*s)
   {
     if (*tokenlen < tokenmax)
-      token[(*tokenlen)++] = *s;
+      token[*tokenlen] = *s;
     if (*s == '"')
       return (s + 1);
     if (*s == '\\')
     {
       if (!*++s)
 	break;
+
+      token[*tokenlen] = *s;
     }
+    (*tokenlen)++;
     s++;
   }
   RFC822Error = ERR_MISMATCH_QUOTE;
