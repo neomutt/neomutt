14571615612a1f6b25b107358d04ffae3f045971
Thomas Roessler <roessler@does-not-exist.org>
2000-01-03 10:15:35 +0000

    Fix strncasecmp and strcasecmp replacement functions.  Problem noted by Stu Heiss <stu@jpusa1.chi.il.us>.

--- strcasecmp.c
+++ strcasecmp.c
@@ -5,33 +5,36 @@
 
 int strncasecmp (char *s1, char *s2, size_t n)
 {
-    register int c1, c2, l = 0;
-
-    while (*s1 && *s2 && l < n)
-    {
-      c1 = tolower (*s1);
-      c2 = tolower (*s2);
-      if (c1 != c2)
-	return (c1 - c2);
-      s1++;
-      s2++;
-      l++;
-    }
+  register int c1, c2, l = 0;
+  
+  while (*s1 && *s2 && l < n)
+  {
+    c1 = tolower (*s1);
+    c2 = tolower (*s2);
+    if (c1 != c2)
+      return (c1 - c2);
+    s1++;
+    s2++;
+    l++;
+  }
+  if (l == n)
     return (int) (0);
+  else
+    return (int) (*s1 - *s2);
 }
 
 int strcasecmp (char *s1, char *s2)
 {
-    register int c1, c2;
-
-    while (*s1 && *s2)
-    {
-      c1 = tolower (*s1);
-      c2 = tolower (*s2);
-      if (c1 != c2)
-	return (c1 - c2);
-      s1++;
-      s2++;
-    }                                                                           
-    return (int) (*s1 - *s2);
+  register int c1, c2;
+  
+  while (*s1 && *s2)
+  {
+    c1 = tolower (*s1);
+    c2 = tolower (*s2);
+    if (c1 != c2)
+      return (c1 - c2);
+    s1++;
+    s2++;
+  }                                                                           
+  return (int) (*s1 - *s2);
 }
