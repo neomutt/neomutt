745f97ea55111ad5f65c24999178ca05bede02df
Thomas Roessler <roessler@does-not-exist.org>
2000-02-15 08:31:45 +0000

    More OP_EXIT fixes.

--- keymap.c
+++ keymap.c
@@ -589,7 +589,7 @@ void km_init (void)
   km_bindkey ("<enter>", MENU_MAIN, OP_DISPLAY_MESSAGE);
 
   km_bindkey ("x", MENU_PAGER, OP_EXIT);
-  km_bindkey ("q", MENU_PAGER, OP_EXIT);
+  km_bindkey ("i", MENU_PAGER, OP_EXIT);
   km_bindkey ("<backspace>", MENU_PAGER, OP_PREV_LINE);
   km_bindkey ("<pagedown>", MENU_PAGER, OP_NEXT_PAGE);
   km_bindkey ("<pageup>", MENU_PAGER, OP_PREV_PAGE);
