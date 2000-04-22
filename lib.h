691499ad326367f4a4c3161500836aa636e1835c
Thomas Roessler <roessler@does-not-exist.org>
2000-04-22 08:47:31 +0000

    Comment cosmetics.  From Martin Michlmayer.

--- lib.h
+++ lib.h
@@ -16,7 +16,8 @@
  * 
  *     You should have received a copy of the GNU General Public
  *     License along with this program; if not, write to the Free
- *     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
+ *     Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *     Boston, MA  02111, USA.
  */ 
 
 /* mutt functions which are generally useful. */
