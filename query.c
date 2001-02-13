4116a84eb00f454e5aedfefb83f364595e20db0c
Thomas Roessler <roessler@does-not-exist.org>
2001-02-13 12:08:22 +0000

    Match all fields when searching on the query menu.

--- query.c
+++ query.c
@@ -146,7 +146,26 @@ static int query_search (MUTTMENU *m, regex_t *re, int n)
 {
   ENTRY *table = (ENTRY *) m->data;
 
-  return (regexec (re, table[n].data->name, 0, NULL, 0));
+  if (table[n].data->name && !regexec (re, table[n].data->name, 0, NULL, 0))
+    return 0;
+  if (table[n].data->other && !regexec (re, table[n].data->other, 0, NULL, 0))
+    return 0;
+  if (table[n].data->addr)
+  {
+    if (table[n].data->addr->personal && 
+	!regexec (re, table[n].data->addr->personal, 0, NULL, 0))
+      return 0;
+    if (table[n].data->addr->mailbox &&
+	!regexec (re, table[n].data->addr->mailbox, 0, NULL, 0))
+      return 0;
+#ifdef EXACT_ADDRESS
+    if (table[n].data->addr->val &&
+	!regexec (re, table[n].data->addr->val, 0, NULL, 0))
+      return 0;
+#endif
+  }
+  
+  return REG_NOMATCH;
 }
 
 /* This is the callback routine from mutt_menuLoop() which is used to generate
