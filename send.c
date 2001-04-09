2985e7257f11df74901d14c553a6682458c05a99
Thomas Roessler <roessler@does-not-exist.org>
2001-04-09 12:09:17 +0000

    Avoid a crash.  patch-1.3.17.tlr.pgpflags_fault.1.

--- send.c
+++ send.c
@@ -1033,15 +1033,16 @@ static int set_pgp_flags (HEADER *cur, CONTEXT *ctx)
   if (cur) 
     return _set_pgp_flags (cur);
   
-  /* else */
-  
+  if (!ctx)
+    return 0;
+    
   for (i = 0; i < ctx->vcount; i++)
   {
     cur = ctx->hdrs[ctx->v2r[i]];
     if (cur->tagged)
       flags |= _set_pgp_flags (cur);
   }
-  
+
   return flags;
 }
 
