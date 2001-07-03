665539f336af8a50b50f7ed9c5051391125496d4
Thomas Roessler <roessler@does-not-exist.org>
2001-07-03 17:05:47 +0000

    Don't sign-extend chars.

--- pgppubring.c
+++ pgppubring.c
@@ -768,7 +768,7 @@ static void print_userid (const char *id)
     if (*id >= ' ' && *id <= 'z' && *id != ':')
       putchar (*id);
     else
-      printf ("\\x%02x", *id);
+      printf ("\\x%02x", (*id) & 0xff);
   }
 }
 
