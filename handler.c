5d74154ef2a9f0c4844e139ee2bd494fc49afd60
Thomas Roessler <roessler@does-not-exist.org>
2001-11-08 00:58:15 +0000

    2.47 introduced another bug.  I should go to bed now.

--- handler.c
+++ handler.c
@@ -948,9 +948,10 @@ static int flowed_visual_strlen (char *l, int i)
   int j;
   for (j = 0; *l; l++)
   {
-    j++;
     if (*l == '\t')
       j += 8 - ((i + j) % 8);
+    else
+      j++;
   }
   
   return j;
