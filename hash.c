6259e751f7682db50271ec704ce556d225b129c0
Thomas Roessler <roessler@does-not-exist.org>
2001-11-19 10:41:32 +0000

    Fix #868, #876.  Work-around from Len Lattanzi <len@mss.migration.com>.

--- hash.c
+++ hash.c
@@ -44,6 +44,8 @@ int hash_string (const unsigned char *s, int n)
 HASH *hash_create (int nelem)
 {
   HASH *table = safe_malloc (sizeof (HASH));
+  if (nelem == 0)
+    nelem = 2;
   table->nelem = nelem;
   table->table = safe_calloc (nelem, sizeof (struct hash_elem *));
   return table;
