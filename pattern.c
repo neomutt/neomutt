487bb154f65243a8a40dc107fa9df489f725f590
Thomas Roessler <roessler@does-not-exist.org>
2002-01-15 09:03:33 +0000

    Fix an off-by-one error.

--- pattern.c
+++ pattern.c
@@ -1063,7 +1063,7 @@ static void quote_simple(char *tmp, size_t len, const char *p)
   int i = 0;
   
   tmp[i++] = '"';
-  while (*p && i < len - 2)
+  while (*p && i < len - 3)
   {
     if (*p == '\\' || *p == '"')
       tmp[i++] = '\\';
