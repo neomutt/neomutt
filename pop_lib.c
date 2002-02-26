35ccde472deb0f253e94b02f833518ebde1ea040
Thomas Roessler <roessler@does-not-exist.org>
2002-02-26 10:37:08 +0000

    CAPA response tags are case-insensitive.  Noted by Michael Vore, patch from Vsevolod Volkov.

--- pop_lib.c
+++ pop_lib.c
@@ -83,20 +83,20 @@ static int fetch_capa (char *line, void *data)
   POP_DATA *pop_data = (POP_DATA *)data;
   char *c;
 
-  if (!mutt_strncmp (line, "SASL", 4))
+  if (!ascii_strncasecmp (line, "SASL", 4))
   {
     c = line + 4;
     SKIPWS (c);
     pop_data->auth_list = safe_strdup (c);
   }
 
-  else if (!mutt_strncmp (line, "USER", 4))
+  else if (!ascii_strncasecmp (line, "USER", 4))
     pop_data->cmd_user = 1;
 
-  else if (!mutt_strncmp (line, "UIDL", 4))
+  else if (!ascii_strncasecmp (line, "UIDL", 4))
     pop_data->cmd_uidl = 1;
 
-  else if (!mutt_strncmp (line, "TOP", 3))
+  else if (!ascii_strncasecmp (line, "TOP", 3))
     pop_data->cmd_top = 1;
 
   return 0;
