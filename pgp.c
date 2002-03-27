1d390d2420fbdc9828c81c24a3c659e088d2c1c4
Thomas Roessler <roessler@does-not-exist.org>
2002-03-27 08:50:42 +0000

    There was some output missing.

--- pgp.c
+++ pgp.c
@@ -332,12 +332,23 @@ void pgp_application_pgp_handler (BODY *m, STATE *s)
 	  }
 	}
       }
+      
 
       /*
        * Now, copy cleartext to the screen.  NOTE - we expect that PGP
        * outputs utf-8 cleartext.  This may not always be true, but it 
        * seems to be a reasonable guess.
        */
+
+      if(s->flags & M_DISPLAY)
+      {
+	if (needpass)
+	  state_attach_puts (_("[-- BEGIN PGP MESSAGE --]\n\n"), s);
+	else if (pgp_keyblock)
+	  state_attach_puts (_("[-- BEGIN PGP PUBLIC KEY BLOCK --]\n"), s);
+	else
+	  state_attach_puts (_("[-- BEGIN PGP SIGNED MESSAGE --]\n\n"), s);
+      }
 
       if (clearsign)
       {
