25c6c2487b4f414fbf75cd65432f228c94011f0c
Tony Leneis <tony@cvr.ds.adp.com>
2002-09-09 21:04:03 +0000

    Tru64 Unix portability patch.

--- signal.c
+++ signal.c
@@ -41,7 +41,11 @@ RETSIGTYPE exit_handler (int sig)
 #if (__sun__ && __svr4__)
   printf(_("Caught %s...  Exiting.\n"), _sys_siglist[sig]);
 #else
+#if (__alpha && __osf__)
+  printf(_("Caught %s...  Exiting.\n"), __sys_siglist[sig]);
+#else
   printf(_("Caught signal %d...  Exiting.\n"), sig);
+#endif
 #endif
 #endif
   exit (0);
