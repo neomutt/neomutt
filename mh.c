70541472eb6d7f233a6a70b36595f3651ad83d4f
Michael Elkins <me@sigpipe.org>
2002-09-09 21:19:08 +0000

    Fix #1321.

--- mh.c
+++ mh.c
@@ -854,7 +854,15 @@ static void maildir_flags (char *dest, size_t destlen, HEADER * hdr)
 {
   *dest = '\0';
 
-  if (hdr && (hdr->flagged || hdr->replied || hdr->read || hdr->deleted))
+  /*
+   * The maildir specification requires that all files in the cur
+   * subdirectory have the :unique string appeneded, regardless of whether
+   * or not there are any flags.  If .old is set, we know that this message
+   * will end up in the cur directory, so we include it in the following
+   * test even though there is no associated flag.
+   */
+  
+  if (hdr && (hdr->flagged || hdr->replied || hdr->read || hdr->deleted || hdr->old))
   {
     snprintf (dest, destlen,
 	      ":2,%s%s%s%s",
