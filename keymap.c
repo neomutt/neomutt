30dc8d080c5156980f0cfd5f8f0c628547e55d3f
Thomas Roessler <roessler@does-not-exist.org>
2002-12-18 10:22:11 +0000

    Make the messages from the what-key function internationalizable.

--- keymap.c
+++ keymap.c
@@ -919,12 +919,12 @@ void mutt_what_key (void)
 {
   int ch;
 
-  mvprintw(LINES-1,0,"Enter keys (^G to abort): ");
+  mvprintw(LINES-1,0, _("Enter keys (^G to abort): "));
   do {
     ch = getch();
     if (ch != ERR && ch != ctrl ('G'))
     {
-      mutt_message("Char = %s, Octal = %o, Decimal = %d",
+      mutt_message(_("Char = %s, Octal = %o, Decimal = %d"),
 	       km_keyname(ch), ch, ch);
     }
   }
