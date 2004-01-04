c5e70d59b564a9f6afb1e714ddbb8177e9501cf6
Thomas Roessler <roessler@does-not-exist.org>
2004-01-04 10:51:49 +0000

    Permit list patterns that match the domain name of an address; these patterns begin with '@'.  Example:  subscribe @bugs.guug.de matches all messages sent to the bug tracking system.

--- hdrline.c
+++ hdrline.c
@@ -30,11 +30,17 @@
 
 static int _mutt_is_mail_list (ADDRESS *addr, LIST *p)
 {
+  char *s;
   if (addr->mailbox)
   {
     for (;p; p = p->next)
+    {
       if (mutt_strncasecmp (addr->mailbox, p->data, mutt_strlen (p->data)) == 0)
 	return 1;
+      else if (*p->data == '@' && (s = strchr (addr->mailbox, '@')))
+	if (mutt_strncasecmp (s, p->data, mutt_strlen (p->data)) == 0)
+	  return 1;
+    }
   }
   return 0;
 }
