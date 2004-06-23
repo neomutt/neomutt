77ba10565040c65c8b874b3eca1ebb1484691b63
Thomas Roessler <roessler@does-not-exist.org>
2004-06-23 08:09:43 +0000

    Zero out the PGP passphrase before asking for a new one.

--- pgp.c
+++ pgp.c
@@ -74,6 +74,8 @@ int pgp_valid_passphrase (void)
   if (now < PgpExptime)
     /* Use cached copy.  */
     return 1;
+  
+  pgp_void_passphrase ();
 
   if (mutt_get_password (_("Enter PGP passphrase:"), PgpPass, sizeof (PgpPass)) == 0)
     {
