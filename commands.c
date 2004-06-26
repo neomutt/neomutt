a9ee66ab0b14d6d64452a879bb42f3a6c531d0b4
Thomas Roessler <roessler@does-not-exist.org>
2004-06-26 08:45:29 +0000

    Fix #1909.

--- commands.c
+++ commands.c
@@ -966,7 +966,8 @@ int mutt_check_traditional_pgp (HEADER *h, int *redraw)
   else
   {
     for (i = 0; i < Context->vcount; i++)
-      if (Context->hdrs[Context->v2r[i]]->tagged && !(h->security & PGP_TRADITIONAL_CHECKED))
+      if (Context->hdrs[Context->v2r[i]]->tagged && 
+	  !(Context->hdrs[Context->v2r[i]]->security & PGP_TRADITIONAL_CHECKED))
 	rv = _mutt_check_traditional_pgp (Context->hdrs[Context->v2r[i]], redraw)
 	  || rv;
   }
