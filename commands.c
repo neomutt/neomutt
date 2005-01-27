7a100098d953da79c1846982e761c8ad6e41c7cc
TAKAHASHI Tamotsu <ttakah@lapis.plala.or.jp>
2005-01-27 20:38:02 +0000

    0. In compose mode, press Ctrl-t to change Content-Type. 1. Type "application/octet-stream" for example. 2. Press Ctrl-t again. 3. Type "text/plain; charset=us-ascii" 4. You will be asked "Convert to us-ascii upon sending?" 5. Press y or n. 6. You will see a strange message:  "Content-Type changed to Convert to us-ascii upon sending?."

--- commands.c
+++ commands.c
@@ -910,12 +910,17 @@ void mutt_edit_content_type (HEADER *h, BODY *b, FILE *fp)
 
   /* inform the user */
   
+  snprintf (tmp, sizeof (tmp), "%s/%s", TYPE (b), NONULL (b->subtype));
   if (type_changed)
     mutt_message (_("Content-Type changed to %s."), tmp);
-  else if (b->type == TYPETEXT && charset_changed)
+  if (b->type == TYPETEXT && charset_changed)
+  {
+    if (type_changed)
+      mutt_sleep (1);
     mutt_message (_("Character set changed to %s; %s."),
 		  mutt_get_parameter ("charset", b->parameter),
 		  b->noconv ? _("not converting") : _("converting"));
+  }
 
   b->force_charset |= charset_changed ? 1 : 0;
 
