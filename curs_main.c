757c93c4a685940bb695d909ad6294d7197aedbc
Mads Martin Joergensen <mmj@suse.de>
2005-02-28 18:33:45 +0000

    Fix a crash.

--- curs_main.c
+++ curs_main.c
@@ -173,7 +173,7 @@ int index_color (int index_no)
 {
   HEADER *h = Context->hdrs[Context->v2r[index_no]];
 
-  if (h->pair)
+  if (h && h->pair)
     return h->pair;
 
   mutt_set_header_color (Context, h);
