632578256c51aff0af6ada7dff1a8a486f82f582
Brendan Cully <brendan@kublai.com>
2005-08-24 15:17:43 +0000

    Don't treat PGP inline signature verification errors as fatal.

--- pgp.c
+++ pgp.c
@@ -387,7 +387,7 @@ int pgp_application_pgp_handler (BODY *m, STATE *s)
 	  c = fgetc (pgpout);
 	  ungetc (c, pgpout);
 	}
-        if (!pgpout || c == EOF)
+        if (!clearsign && (!pgpout || c == EOF))
 	{
           mutt_error _("Could not decrypt PGP message");
 	  mutt_sleep (1);
