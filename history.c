99293b4d18a8d5b61b09f91549d97baaac50cce9
Alain Bench <veronatif@free.fr>
2006-07-04 17:23:16 +0000

    Adopt shell convention of omitting from the history commands prefixed with a space.

--- history.c
+++ history.c
@@ -81,7 +81,12 @@ void mutt_history_add (history_class_t hclass, const char *s)
   {
     prev = h->last - 1;
     if (prev < 0) prev = HistSize - 1;
-    if (!h->hist[prev] || mutt_strcmp (h->hist[prev], s) != 0)
+
+    /* don't add to prompt history:
+     *  - lines beginning by a space
+     *  - repeated lines
+     */
+    if (*s != ' ' && (!h->hist[prev] || mutt_strcmp (h->hist[prev], s) != 0))
     {
       mutt_str_replace (&h->hist[h->last++], s);
       if (h->last > HistSize - 1)
