cd960be4509b40aae9b88c4bd3a0c09d856a164e
Brendan Cully <brendan@kublai.com>
2007-04-02 17:56:16 -0700

    Fix off-by-one allocating mh sequences

--- mh.c
+++ mh.c
@@ -98,9 +98,10 @@ static void mhs_alloc (struct mh_sequences *mhs, int i)
   if (i > mhs->max || !mhs->flags)
   {
     newmax = i + 128;
+    j = mhs->flags ? mhs->max + 1 : 0;
     safe_realloc (&mhs->flags, sizeof (mhs->flags[0]) * (newmax + 1));
-    for (j = mhs->max + 1; j <= newmax; j++)
-      mhs->flags[j] = 0;
+    while (j <= newmax)
+      mhs->flags[j++] = 0;
 
     mhs->max = newmax;
   }
