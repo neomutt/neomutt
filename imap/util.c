2508e70b35f30f779d85444a671430658f49bb2c
Brendan Cully <brendan@kublai.com>
2007-04-11 07:37:53 -0700

    Do not attempt to parse idata->ctx->path if an explicit path is given

--- imap/util.c
+++ imap/util.c
@@ -83,13 +83,13 @@ header_cache_t* imap_hcache_open (IMAP_DATA* idata, const char* path)
   char cachepath[LONG_STRING];
   char mbox[LONG_STRING];
 
-  if (imap_parse_path (idata->ctx->path, &mx) < 0)
-    return NULL;
-
   if (path)
     imap_cachepath (idata, path, mbox, sizeof (mbox));
   else
   {
+    if (imap_parse_path (idata->ctx->path, &mx) < 0)
+      return NULL;
+
     imap_cachepath (idata, mx.mbox, mbox, sizeof (mbox));
     FREE (&mx.mbox);
   }
