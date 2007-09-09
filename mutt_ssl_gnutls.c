dec0bf20ebf5c868073556499c81843b063568e5
Julien Cristau <unknown>
2007-09-09 15:20:12 -0700

    Only check whether GNUTLS error is fatal when an error occurs (closes #2954). The API for GNUTLS changed to require this in 1.7.18.

--- mutt_ssl_gnutls.c
+++ mutt_ssl_gnutls.c
@@ -99,7 +99,7 @@ static int tls_socket_read (CONNECTION* conn, char* buf, size_t len)
   }
 
   ret = gnutls_record_recv (data->state, buf, len);
-  if (gnutls_error_is_fatal(ret) == 1)
+  if (ret < 0 && gnutls_error_is_fatal(ret) == 1)
   {
     mutt_error ("tls_socket_read (%s)", gnutls_strerror (ret));
     mutt_sleep (4);
@@ -121,7 +121,7 @@ static int tls_socket_write (CONNECTION* conn, const char* buf, size_t len)
   }
 
   ret = gnutls_record_send (data->state, buf, len);
-  if (gnutls_error_is_fatal(ret) == 1)
+  if (ret < 0 && gnutls_error_is_fatal(ret) == 1)
   {
     mutt_error ("tls_socket_write (%s)", gnutls_strerror (ret));
     mutt_sleep (4);
