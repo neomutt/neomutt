581767649f998729300272284483630ad92e8f95
Alexey I. Froloff <raorn@altlinux.org>
2008-06-11 22:45:14 -0700

    Do not attempt to close invalid descriptors. Closes #3075

--- attach.c
+++ attach.c
@@ -561,10 +561,11 @@ int mutt_view_attachment (FILE *fp, BODY *a, int flag, HEADER *hdr,
       if ((mutt_wait_filter (thepid) || (entry->needsterminal &&
 	  option (OPTWAITKEY))) && !use_pager)
 	mutt_any_key_to_continue (NULL);
-      
-      close(tempfd);
-      close(pagerfd);
-      
+
+      if (tempfd != -1)
+	close (tempfd);
+      if (pagerfd != -1)
+	close (pagerfd);
     }
     else
     {
