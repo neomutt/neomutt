4812617d9f40120545e701f9e0fdb7d4141b7439
Brendan Cully <brendan@kublai.com>
2008-06-26 12:47:22 -0700

    Force a resize event after calling an external editor. Closes #2207

--- curs_lib.c
+++ curs_lib.c
@@ -176,6 +176,10 @@ void mutt_edit_file (const char *editor, const char *data)
     mutt_error (_("Error running \"%s\"!"), cmd);
     mutt_sleep (2);
   }
+#if defined (USE_SLANG_CURSES) || defined (HAVE_RESIZETERM)
+  /* the terminal may have been resized while the editor owned it */
+  mutt_resize_screen ();
+#endif
   keypad (stdscr, TRUE);
   clearok (stdscr, TRUE);
 }
