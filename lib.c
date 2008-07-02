2dccba0b15ea21e46d0dd69e9e0da60ed0dca024
Vladimir Marek <Vladimir.Marek@Sun.COM>
2008-07-02 18:26:17 +0200

    Use stat() instead of dirent->d_type to test for directory. Closes #3089.

--- lib.c
+++ lib.c
@@ -41,6 +41,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <pwd.h>
+#include <sys/types.h>
 #include <dirent.h>
 
 #ifdef HAVE_SYSEXITS_H
@@ -583,6 +584,7 @@ int mutt_rmtree (const char* path)
   DIR* dirp;
   struct dirent* de;
   char cur[_POSIX_PATH_MAX];
+  struct stat statbuf;
   int rc = 0;
 
   if (!(dirp = opendir (path)))
@@ -597,7 +599,14 @@ int mutt_rmtree (const char* path)
 
     snprintf (cur, sizeof (cur), "%s/%s", path, de->d_name);
     /* XXX make nonrecursive version */
-    if (de->d_type == DT_DIR)
+
+    if (stat(cur, &statbuf) == -1)
+    {
+      rc = 1;
+      continue;
+    }
+
+    if (S_ISDIR (statbuf.st_mode))
       rc |= mutt_rmtree (cur);
     else
       rc |= unlink (cur);
