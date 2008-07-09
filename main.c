08a139833e4de750bc0d7d3af8e9408da459d824
Rocco Rutte <pdmef@gmx.net>
2008-07-09 09:14:25 +0000

    Document redirection to trigger batch send mode in mutt(1) and mutt -h. Closes #2070.

--- main.c
+++ main.c
@@ -111,6 +111,7 @@ static void mutt_usage (void)
   puts _(
 "usage: mutt [<options>] [-z] [-f <file> | -yZ]\n\
        mutt [<options>] [-x] [-Hi <file>] [-s <subj>] [-bc <addr>] [-a <file> [...]] [--] <addr> [...]\n\
+       mutt [<options>] [-x] [-s <subj>] [-bc <addr>] [-a <file> [...]] [--] <addr> [...] < message\n\
        mutt [<options>] -p\n\
        mutt [<options>] -A <alias> [...]\n\
        mutt [<options>] -Q <query> [...]\n\
