69c463a4f6dcb9fb30cd5d0c5cdfc4a02a886385
Brendan Cully <brendan@kublai.com>
2008-08-19 13:17:18 -0700

    Silence an incorrect uninitialized variable warning.

--- init.c
+++ init.c
@@ -1686,7 +1686,7 @@ static void pretty_var (char *dst, size_t len, const char *option, const char *v
 
 static int check_charset (struct option_t *opt, const char *val)
 {
-  char *p, *q, *s = safe_strdup (val);
+  char *p, *q = NULL, *s = safe_strdup (val);
   int rc = 0, strict = strcmp (opt->option, "send_charset") == 0;
 
   for (p = strtok_r (s, ":", &q); p; p = strtok_r (NULL, ":", &q))
