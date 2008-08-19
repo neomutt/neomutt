593acbba1ef6ebb87f0a1efaa5a2674f022d8e6a
Brendan Cully <brendan@kublai.com>
2008-08-19 13:14:51 -0700

    Better documentation for how quote_regexp determines quote level. Closes #1463.

--- init.h
+++ init.h
@@ -2315,13 +2315,17 @@ struct option_t MuttVars[] = {
   { "quote_regexp",	DT_RX,	 R_PAGER, UL &QuoteRegexp, UL "^([ \t]*[|>:}#])+" },
   /*
   ** .pp
-  ** A regular expression used in the internal-pager to determine quoted
-  ** sections of text in the body of a message.
+  ** A regular expression used in the internal pager to determine quoted
+  ** sections of text in the body of a message. Quoted text may be filtered
+  ** out using the ``toggle-quoted'' command, or colored according to the
+  ** ``color quoted'' family of directives.
   ** .pp
-  ** \fBNote:\fP In order to use the \fIquoted\fP\fBx\fP patterns in the
-  ** internal pager, you need to set this to a regular expression that
-  ** matches \fIexactly\fP the quote characters at the beginning of quoted
-  ** lines.
+  ** Higher levels of quoting may be colored differently (``color quoted1'',
+  ** ``color quoted2'', etc.). The quoting level is determined by removing
+  ** the last character from the matched text and recursively reapplying
+  ** the regular expression until it fails to produce a match.
+  ** .pp
+  ** Match detection may be overridden by the ``$smileys'' regular expression.
   */
   { "read_inc",		DT_NUM,	 R_NONE, UL &ReadInc, 10 },
   /*
