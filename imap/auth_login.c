185594b22388b741e8063f58d5e08554d35f5cce
Brendan Cully <brendan@kublai.com>
2009-01-04 14:28:39 -0800

    IMAP copyright header updates

--- imap/auth_login.c
+++ imap/auth_login.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1999-2001 Brendan Cully <brendan@kublai.com>
+ * Copyright (C) 1999-2001,2005 Brendan Cully <brendan@kublai.com>
  * 
  *     This program is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
