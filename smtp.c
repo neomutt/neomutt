d8a0051463c36e55bc2fb0bc3a551b0aa82b8868
Brendan Cully <brendan@kublai.com>
2009-01-04 13:46:47 -0800

    SMTP: complain when authentication fails. Closes #2117. Now each failed attempt emits an error before mutt tries the next method. Possibly we should just fail immediately?

--- smtp.c
+++ smtp.c
@@ -454,7 +454,14 @@ static int smtp_auth (CONNECTION* conn)
 
       dprint (2, (debugfile, "smtp_authenticate: Trying method %s\n", method));
 
-      if ((r = smtp_auth_sasl (conn, method)) != SMTP_AUTH_UNAVAIL)
+      r = smtp_auth_sasl (conn, method);
+      
+      if (r == SMTP_AUTH_FAIL && delim)
+      {
+        mutt_error (_("%s authentication failed, trying next method"), method);
+        mutt_sleep (1);
+      }
+      else if (r != SMTP_AUTH_UNAVAIL)
         break;
     }
 
@@ -466,7 +473,12 @@ static int smtp_auth (CONNECTION* conn)
   if (r != SMTP_AUTH_SUCCESS)
     mutt_account_unsetpass (&conn->account);
 
-  if (r == SMTP_AUTH_UNAVAIL)
+  if (r == SMTP_AUTH_FAIL)
+  {
+    mutt_error (_("SASL authentication failed"));
+    mutt_sleep (1);
+  }
+  else if (r == SMTP_AUTH_UNAVAIL)
   {
     mutt_error (_("No authenticators available"));
     mutt_sleep (1);
@@ -559,17 +571,8 @@ static int smtp_auth_sasl (CONNECTION* conn, const char* mechlist)
     mutt_sasl_setup_conn (conn, saslconn);
     return SMTP_AUTH_SUCCESS;
   }
-  else if (SmtpAuthenticators && *SmtpAuthenticators)
-  {
-    /* if we're given a mech list to attempt, failure means try the next */
-    dprint (2, (debugfile, "smtp_auth_sasl: %s failed\n", mech));
-    sasl_dispose (&saslconn);
-    return SMTP_AUTH_UNAVAIL;
-  }
 
 fail:
-  mutt_error (_("SASL authentication failed"));
-  mutt_sleep (1);
   sasl_dispose (&saslconn);
   return SMTP_AUTH_FAIL;
 }
