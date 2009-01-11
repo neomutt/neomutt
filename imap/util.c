48e2fae9bae655df27face15145826d5c3c8bd17
Brendan Cully <brendan@kublai.com>
2009-01-10 21:44:41 -0800

    Use known connection delimiter in imap_expand_path

--- imap/util.c
+++ imap/util.c
@@ -54,6 +54,7 @@
 int imap_expand_path (char* path, size_t len)
 {
   IMAP_MBOX mx;
+  IMAP_DATA* idata;
   ciss_url_t url;
   char fixedpath[LONG_STRING];
   int rc;
@@ -61,8 +62,9 @@ int imap_expand_path (char* path, size_t len)
   if (imap_parse_path (path, &mx) < 0)
     return -1;
 
+  idata = imap_conn_find (&mx.account, M_IMAP_CONN_NONEW);
   mutt_account_tourl (&mx.account, &url);
-  imap_fix_path(NULL, mx.mbox, fixedpath, sizeof (fixedpath));
+  imap_fix_path (idata, mx.mbox, fixedpath, sizeof (fixedpath));
   url.path = fixedpath;
 
   rc = url_ciss_tostring (&url, path, len, U_DECODE_PASSWD);
