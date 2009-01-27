caac1f532690ce932c653e647b5006cce80f4f9f
Brendan Cully <brendan@kublai.com>
2009-01-27 12:17:23 -0800

    IMAP: only close socket when not already disconnected.

--- imap/imap.c
+++ imap/imap.c
@@ -488,8 +488,11 @@ int imap_open_connection (IMAP_DATA* idata)
 
 void imap_close_connection(IMAP_DATA* idata)
 {
-  mutt_socket_close (idata->conn);
-  idata->state = IMAP_DISCONNECTED;
+  if (idata->state != IMAP_DISCONNECTED)
+  {
+    mutt_socket_close (idata->conn);
+    idata->state = IMAP_DISCONNECTED;
+  }
   idata->seqno = idata->nextcmd = idata->lastcmd = idata->status = 0;
   memset (idata->cmds, 0, sizeof (IMAP_COMMAND) * idata->cmdslots);
 }
