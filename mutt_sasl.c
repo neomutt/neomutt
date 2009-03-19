dffc5c8896d220d93775934ebbb0b88576030071
Roman Kraevskiy <rkraevskiy@gmail.com>
2009-03-19 10:36:04 +0100

    Restore connection polling callback when closing SASL connection. Closes #3206.

--- mutt_sasl.c
+++ mutt_sasl.c
@@ -481,6 +481,7 @@ static int mutt_sasl_conn_close (CONNECTION* conn)
   conn->conn_close = sasldata->msasl_close;
   conn->conn_read = sasldata->msasl_read;
   conn->conn_write = sasldata->msasl_write;
+  conn->conn_poll = sasldata->msasl_poll;
 
   /* release sasl resources */
   sasl_dispose (&sasldata->saslconn);
