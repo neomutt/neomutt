3c7fd0ead38d180d59298ad8065a6a6bec713044
Rocco Rutte <pdmef@gmx.net>
2009-04-08 16:25:38 +0200

    For %F, display Bcc recipient if no other recipients found. Closes #3157.

--- hdrline.c
+++ hdrline.c
@@ -121,6 +121,8 @@ static void make_from (ENVELOPE *hdr, char *buf, size_t len, int do_lists)
     snprintf (buf, len, "To %s", mutt_get_name (hdr->to));
   else if (me && hdr->cc)
     snprintf (buf, len, "Cc %s", mutt_get_name (hdr->cc));
+  else if (me && hdr->bcc)
+    snprintf (buf, len, "Bcc %s", mutt_get_name (hdr->bcc));
   else if (hdr->from)
     strfcpy (buf, mutt_get_name (hdr->from), len);
   else
