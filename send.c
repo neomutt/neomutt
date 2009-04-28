230fc37b7e39efb731250e1223a0457cc68762eb
Rocco Rutte <pdmef@gmx.net>
2009-04-28 13:05:40 +0200

    Don't generate References: for reply to multiple messages
    All parent Message-Ids are present in In-Reply-To already.
    Also, RfC2822, section 3.6.4 discourages its generation.

--- send.c
+++ send.c
@@ -675,6 +675,12 @@ mutt_make_reference_headers (ENVELOPE *curenv, ENVELOPE *env, CONTEXT *ctx)
   }
   else
     mutt_add_to_reference_headers (env, curenv, NULL, NULL);
+
+  /* if there's more than entry in In-Reply-To (i.e. message has
+     multiple parents), don't generate a References: header as it's
+     discouraged by RfC2822, sect. 3.6.4 */
+  if (ctx->tagged > 0 && env->in_reply_to && env->in_reply_to->next)
+    mutt_free_list (&env->references);
 }
 
 static int
