51e08617e6ee58124e9875574b688228eff6a908
Rocco Rutte <pdmef@gmx.net>
2009-06-15 16:35:01 +0200

    Clarify what $wrap=0 does

--- init.h
+++ init.h
@@ -3303,7 +3303,8 @@ struct option_t MuttVars[] = {
   ** .pp
   ** When set to a positive value, mutt will wrap text at $$wrap characters.
   ** When set to a negative value, mutt will wrap text so that there are $$wrap
-  ** characters of empty space on the right side of the terminal.
+  ** characters of empty space on the right side of the terminal. Setting it
+  ** to zero makes mutt wrap at the terminal width.
   */
   { "wrap_search",	DT_BOOL, R_NONE, OPTWRAPSEARCH, 1 },
   /*
