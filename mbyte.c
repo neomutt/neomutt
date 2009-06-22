8be387ec6bd16faf875d41dbdd770f2ed91f556b
Rocco Rutte <pdmef@gmx.net>
2009-06-22 17:17:27 +0200

    Fix included iswupper(). Closes #3276.

--- mbyte.c
+++ mbyte.c
@@ -289,13 +289,15 @@ static int iswupper_ucs (wint_t x)
 {
   /* Only works for x < 0x130 */
   if ((0x60 < x && x < 0x7b) || (0xe0 <= x && x < 0xff && x != 0xf7))
+    return 0;
+  else if ((0x40 < x && x < 0x5b) || 0xbf < x && x < 0xde)
     return 1;
   else if (0x100 <= x && x < 0x130)
     return 1;
   else if (x == 0xb5)
     return 1;
   else if (x == 0xff)
-    return 1;
+    return 0;
   else
     return 0;
 }
