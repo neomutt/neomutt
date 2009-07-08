dea06ce71cceb41826313e82c09a565064b3afd6
Rocco Rutte <pdmef@gmx.net>
2009-07-08 13:37:27 +0200

    Fix compiler warning

--- mbyte.c
+++ mbyte.c
@@ -290,7 +290,7 @@ static int iswupper_ucs (wint_t x)
   /* Only works for x < 0x130 */
   if ((0x60 < x && x < 0x7b) || (0xe0 <= x && x < 0xff && x != 0xf7))
     return 0;
-  else if ((0x40 < x && x < 0x5b) || 0xbf < x && x < 0xde)
+  else if ((0x40 < x && x < 0x5b) || (0xbf < x && x < 0xde))
     return 1;
   else if (0x100 <= x && x < 0x130)
     return 1;
