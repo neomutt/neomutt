6137ff366b8cbd00aef68c4424fe93b52e45a841
Rocco Rutte <pdmef@gmx.net>
2009-08-17 17:07:26 +0200

    Doc: fix more typos

--- init.h
+++ init.h
@@ -917,7 +917,7 @@ struct option_t MuttVars[] = {
   ** When mutt is compiled with qdbm or tokyocabinet as header cache backend,
   ** this option determines whether the database will be compressed.
   ** Compression results in database files roughly being one fifth
-  ** of the usual diskspace, but the uncompression can result in a
+  ** of the usual diskspace, but the decompression can result in a
   ** slower opening of cached folder(s) which in general is still
   ** much faster than opening non header cached folders.
   */
