17fcd67e834659d029fc919c2041c2aca9514a3f
Michael Elkins <me@mutt.org>
2010-08-06 10:05:43 -0700

    fix compiler warning about wrong type for SslSessionCerts

--- mutt_ssl.c
+++ mutt_ssl.c
@@ -817,7 +817,7 @@ static int ssl_cache_trusted_cert (X509 *c)
 {
   dprint (1, (debugfile, "trusted: %s\n", c->name));
   if (!SslSessionCerts)
-    SslSessionCerts = sk_new_null();
+    SslSessionCerts = sk_X509_new_null();
   return (sk_X509_push (SslSessionCerts, X509_dup(c)));
 }
 
