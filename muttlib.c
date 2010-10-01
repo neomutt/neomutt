4007d3e2b7c3593ff9ddf993f40a1c8453db2508
Michael Elkins <me@mutt.org>
2010-10-01 07:14:24 -0700

    fix segfault when $message_cachedir is set and opening a POP3 mailbox.
    fixes #3457

--- muttlib.c
+++ muttlib.c
@@ -1960,6 +1960,7 @@ void mutt_encode_path (char *dest, size_t dlen, const char *src)
 {
   char *p = safe_strdup (src);
   int rc = mutt_convert_string (&p, Charset, "utf-8", 0);
-  strfcpy (dest, rc == 0 ? p : src, dlen);
+  /* `src' may be NULL, such as when called from the pop3 driver. */
+  strfcpy (dest, (rc == 0) ? NONULL(p) : NONULL(src), dlen);
   FREE (&p);
 }
