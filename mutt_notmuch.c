d835b6f6ff9ed5ab9791a7d4c770e3e79a532dd7
Karel Zak <kzak@redhat.com>
2011-12-14 11:58:47 +0100

    add basic files and build-sys stuff
    Signed-off-by: Karel Zak <kzak@redhat.com>

---
+++ mutt_notmuch.c
@@ -0,0 +1,5 @@
+/*
+ * Notmuch support for mutt
+ *
+ * Copyright (C) 2011 Karel Zak <kzak@redhat.com>
+ */
---
+++ mutt_notmuch.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright (C) 2011 Karel Zak <kzak@redhat.com>
+ */
+#ifndef _MUTT_NOTMUCH_H_
+#define _MUTT_NOTMUCH_H_ 1
+
+#endif /* _MUTT_NOTMUCH_H_ */
