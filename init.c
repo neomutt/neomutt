15efae443f99f20f56d6dcd2c0daa5577ba06e6b
Antonio Radici <antonio@dyne.org>
2012-07-07 21:57:01 -0700

    check_charset: check for NULL before calling strtok_r (closes #3326)
    Otherwise, if strtok_r cannot handle NULL an empty *charset setting will segfault

--- init.c
+++ init.c
@@ -1672,6 +1672,9 @@ static int check_charset (struct option_t *opt, const char *val)
   char *p, *q = NULL, *s = safe_strdup (val);
   int rc = 0, strict = strcmp (opt->option, "send_charset") == 0;
 
+  if (!s)
+    return rc;
+
   for (p = strtok_r (s, ":", &q); p; p = strtok_r (NULL, ":", &q))
   {
     if (!*p)
