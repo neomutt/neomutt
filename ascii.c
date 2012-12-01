df1b2415138c4cd8f0cbc4b44a9471921779a4be
Michael Elkins <me@mutt.org>
2012-12-01 14:31:42 -0800

    make ascii_strcasecmp properly handle unequal length strings with the same prefix
    closes #3601

--- ascii.c
+++ ascii.c
@@ -71,10 +71,15 @@ int ascii_strcasecmp (const char *a, const char *b)
   if (b == NULL && a)
     return 1;
   
-  for (; *a || *b; a++, b++)
+  for (;; a++, b++)
   {
     if ((i = ascii_tolower (*a) - ascii_tolower (*b)))
       return i;
+    /* test for NUL here rather that in the for loop in order to detect unqual
+     * length strings (see http://dev.mutt.org/trac/ticket/3601)
+     */
+    if (!*a)
+      break;
   }
   
   return 0;
