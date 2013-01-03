81a0f5a8d97c6af82212989b043d4378938e0698
Michael Elkins <me@sigpipe.org>
2013-01-03 08:03:49 -0800

    Backed out changeset bf84f24e512d

--- main.c
+++ main.c
@@ -192,13 +192,7 @@ static void show_version (void)
   printf (" (%s)", uts.machine);
 
 #ifdef NCURSES_VERSION
-  printf ("\nncurses: %s (compiled with %s) %cNCURSES_WIDECHAR", curses_version(), NCURSES_VERSION,
-#ifdef NCURSES_WIDECHAR
-		  '+'
-#else
-		  '-'
-#endif
-	 );
+  printf ("\nncurses: %s (compiled with %s)", curses_version(), NCURSES_VERSION);
 #elif defined(USE_SLANG_CURSES)
   printf ("\nslang: %d", SLANG_VERSION);
 #endif
