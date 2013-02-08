12358afdbfbf69190ee657d4e145d8ae2b9c681d
Michael Elkins <me@sigpipe.org>
2013-02-08 17:06:31 +0000

    explicitly call mutt_refresh() prior to endwin() to force a screen redraw.
    [4903e80356d3] removed the call, but some environments (screen+xterm under
    SuSE11/12) don't properly flush without it prior to the endwin() call

--- curs_lib.c
+++ curs_lib.c
@@ -477,7 +477,13 @@ void mutt_endwin (const char *msg)
   int e = errno;
 
   if (!option (OPTNOCURSES))
+  {
+    /* at least in some situations (screen + xterm under SuSE11/12) endwin()
+     * doesn't properly flush the screen without an explicit call.
+     */
+    mutt_refresh();
     endwin ();
+  }
   
   if (msg && *msg)
   {
