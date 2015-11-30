a5fc9204545d674903da942f17359cc31bebaed5
Richard Russon <rich@flatcap.org>
2015-11-30 04:25:05 +0000

    indent: our functions
    NOTE: this patch does NOT make any code changes.
    If only adds/removes whitespace.
    
    Use whitespace uniformly across all of our code.
    
    Delete trailing whitespace
    Indent with tabs
    Function {} to beginning of line
    Other {} in compact style
    Tidy whitespace around ()'s and maths operators

--- sidebar.h
+++ sidebar.h
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) ????-2004 Justin Hibbits <jrh29@po.cwru.edu>
  * Copyright (C) 2004 Thomer M. Gil <mutt@thomer.com>
- * 
+ *
  *     This program is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation; either version 2 of the License, or
  *     (at your option) any later version.
- * 
+ *
  *     This program is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
- * 
+ *
  *     You should have received a copy of the GNU General Public License
  *     along with this program; if not, write to the Free Software
  *     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
- */ 
+ */
 
 #ifndef SIDEBAR_H
 #define SIDEBAR_H
@@ -28,9 +28,10 @@ struct MBOX_LIST {
 
 /* parameter is whether or not to go to the status line */
 /* used for omitting the last | that covers up the status bar in the index */
-int draw_sidebar(int);
-void scroll_sidebar(int, int);
-void set_buffystats(CONTEXT*);
+int draw_sidebar (int);
+void scroll_sidebar (int, int);
+void set_buffystats (CONTEXT*);
+void set_curbuffy (char*);
 int sidebar_should_refresh();
 void sidebar_updated();
 
