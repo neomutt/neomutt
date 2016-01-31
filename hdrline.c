153b22466a5fc25497056195f02699cc75f3e5c9
Vsevolod Volkov <vvv@mutt.org.ua>
2016-01-31 17:17:10 +0000

    feature: initials
    Expando for author's initials

--- hdrline.c
+++ hdrline.c
@@ -212,6 +212,7 @@ int mutt_user_is_recipient (HEADER *h)
  * %f = entire from line
  * %F = like %n, unless from self
  * %i = message-id
+ * %I = initials of author
  * %l = number of lines in the message
  * %L = like %F, except `lists' are displayed first
  * %m = number of messages in the mailbox
@@ -461,6 +462,31 @@ hdr_format_str (char *dest,
 
     case 'i':
       mutt_format_s (dest, destlen, prefix, hdr->env->message_id ? hdr->env->message_id : "<no.id>");
+      break;
+
+    case 'I':
+      {
+	int iflag = FALSE;
+	int j = 0;
+
+	for (i = 0; hdr->env->from && hdr->env->from->personal &&
+		    hdr->env->from->personal[i] && j < SHORT_STRING - 1; i++)
+	{
+	  if (isalpha ((int)hdr->env->from->personal[i]))
+	  {
+	    if (!iflag)
+	    {
+	      buf2[j++] = hdr->env->from->personal[i];
+	      iflag = TRUE;
+	    }
+	  }
+	  else
+	    iflag = FALSE;
+	}
+
+	buf2[j] = '\0';
+      }
+      mutt_format_s (dest, destlen, prefix, buf2);
       break;
 
     case 'l':
