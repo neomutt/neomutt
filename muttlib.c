2074234052a61d1bb9abc237ce52dd71b99bf093
David Champion <dgc@uchicago.edu>
2016-02-01 00:41:48 +0000

    feature: nested-if
    Allow complex nested conditions in the index_format

--- muttlib.c
+++ muttlib.c
@@ -1248,6 +1248,12 @@ void mutt_FormatString (char *dest,		/* output buffer */
 	count = 0;
         while (count < sizeof (ifstring) && *src && *src != '?' && *src != '&')
 	{
+	  if (*src == '\\')
+	  {
+	    src++;
+	    if (!*src)
+	      break;
+	  }
           *cp++ = *src++;
 	  count++;
 	}
@@ -1260,7 +1266,13 @@ void mutt_FormatString (char *dest,		/* output buffer */
 	count = 0;
 	while (count < sizeof (elsestring) && *src && *src != '?')
 	{
-	  *cp++ = *src++;
+	  if (*src == '\\')
+	  {
+	    src++;
+	    if (!*src)
+	      break;
+	  }
+          *cp++ = *src++;
 	  count++;
 	}
 	*cp = 0;
