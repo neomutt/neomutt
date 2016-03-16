2ad5f4b689aaa9e854b5497362adfaad665d1ee6
Kevin McCarthy <kevin@8t8.us>
2016-03-16 14:15:14 -0700

    Fix tcbdb error reporting to use ecode, not errno.
    After committing aff8d62ebddb and taking a closer look at the tcbdb
    documentation, I realized those functions don't set errno.
    
    Change the error handling messages to instead get the ecode and
    message using tcbdbecode() and tcbdberrmsg().

--- hcache.c
+++ hcache.c
@@ -931,7 +931,8 @@ hcache_open_tc (struct header_cache* h, const char* path)
     return 0;
   else
   {
-    dprint(2, (debugfile, "tcbdbopen failed for %s: %s (errno %d)\n", path, strerror (errno), errno));
+    int ecode = tcbdbecode (h->db);
+    dprint (2, (debugfile, "tcbdbopen failed for %s: %s (ecode %d)\n", path, tcbdberrmsg (ecode), ecode));
     tcbdbdel(h->db);
     return -1;
   }
@@ -944,7 +945,10 @@ mutt_hcache_close(header_cache_t *h)
     return;
 
   if (!tcbdbclose(h->db))
-    dprint (2, (debugfile, "tcbdbclose failed for %s: %s (errno %d)\n", h->folder, strerror (errno), errno));
+  {
+    int ecode = tcbdbecode (h->db);
+    dprint (2, (debugfile, "tcbdbclose failed for %s: %s (ecode %d)\n", h->folder, tcbdberrmsg (ecode), ecode));
+  }
   tcbdbdel(h->db);
   FREE(&h->folder);
   FREE(&h);
