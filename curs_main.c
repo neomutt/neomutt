ba9cf41b2fc30aa7284bdd65a906fc72bbcfb395
Richard Russon <rich@flatcap.org>
2016-03-26 03:44:41 +0000

    tidy code

--- curs_main.c
+++ curs_main.c
@@ -915,8 +915,8 @@ int mutt_index_menu (void)
 	CHECK_IN_MAILBOX;
 	menu->oldcurrent = (Context->vcount && menu->current >= 0 && menu->current < Context->vcount) ?
 		CURHDR->index : -1;
-	if ((op == OP_LIMIT_CURRENT_THREAD && mutt_limit_current_thread(CURHDR))
-	    || (op == OP_MAIN_LIMIT && mutt_pattern_func (M_LIMIT, _("Limit to messages matching: ")) == 0))
+	if (((op == OP_LIMIT_CURRENT_THREAD) && mutt_limit_current_thread(CURHDR))
+	    || ((op == OP_MAIN_LIMIT) && (mutt_pattern_func (M_LIMIT, _("Limit to messages matching: ")) == 0)))
 	{
 	  if (menu->oldcurrent >= 0)
 	  {
