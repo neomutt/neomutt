2d258d237dc6d79b145be3eb49520308ec3c5553
Olaf Lessenich <xai@linux.com>
2016-04-14 23:23:28 +0100

    allow the user to change mailboxes again
    "unmailboxes *" used to cause Sidebar to crash.  The bug-fix for this
    introduced another bug that prevented users from changing to a
    non-Sidebar folder.
    
    Signed-off-by: Richard Russon <rich@flatcap.org>

--- curs_main.c
+++ curs_main.c
@@ -1248,8 +1248,7 @@ int mutt_index_menu (void)
 
 	mutt_expand_path (buf, sizeof (buf));
 #ifdef USE_SIDEBAR
-	if (sb_set_open_buffy (buf) == NULL)
-		break;
+	sb_set_open_buffy (buf);
 #endif
 	if (mx_get_magic (buf) <= 0)
 	{
