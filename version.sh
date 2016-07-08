df5eea36674c837ac852b7169ccb19425430b267
Faidon Liambotis <paravoid@debian.org>
2016-07-08 14:04:46 +0100

    fix version.sh regression

--- version.sh
+++ version.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Switch to directory where this script lives so that further commands are run
+# from the root directory of the source.  The script path and srcdir are double
+# quoted to allow the space character to appear in the path.
+srcdir=`dirname "$0"` && cd "$srcdir" || exit 1
+
 cat VERSION* | tr -d \\n
 
