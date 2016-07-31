a085af50059772a029714dd12d53fea9f199e5a9
Joshua Jordi <JakkinStewart@users.noreply.github.com>
2016-07-31 18:52:51 -0500

    added install.sh and updated readme

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # KeybaseMutt
 
-A work in progress to make Keybase work with mutt.
+A work in progress. KeybaseMutt exists to let its users utilize Keybase within mutt. (Unfortunately, not natively (yet).)
+
+## Quick start
+
+Run the install script. It will set up the directory's and paths for you. Do what it says or suffer the consequences!
+
+Then just throw these into your .muttrc
+
+`set editor = 'echo %s > ~/.mutt/keybaseMutt/.tmp; vim %s'`
+`macro compose K "<enter-command>unset wait_key<enter><shell-escape>python ~/.mutt/keybaseMutt/keybase.py<enter><enter-command>set wait_key<enter>`
+
+Done!
+
+## How to use KeybaseMutt
+
+Write an email. In the screen right before you send your email (called the pager mode), press "K".
+
+You can now use four Keybase commands in this "shell". (I thought it best to limit the user to four commands for security reasons. Running unusual commands would overwrite the email, forcing the user to rewrite the email again.)
+
+The commands are:
+- `keybase encrypt [user]`
+- `keybase pgp encrypt [user]`
+- `keybase sign`
+- `keybase pgp sign`
---
+++ install.sh
@@ -0,0 +1,59 @@
+#! /usr/bin/bash
+
+# If no directory exists, make it exist.
+if [ -d "$HOME/.mutt/keybaseMutt" ]; then
+
+	# If someone already has a backup, complain.
+	if [ -d "$HOME/.mutt/keybaseMuttBACKUP" ]; then
+		#echo "$HOME/.mutt/keybaseMuttBACKUP exists"
+		echo "You are going to overwrite your backup. Are you sure you want to do this? [y|n]"
+		read overwrite
+
+		# If they want to delete their backup.
+		if [ $overwrite == 'y' ]; then
+			cp -R $HOME/.mutt/keybaseMutt $HOME/.mutt/keybaseMuttBACKUP
+			rm -r $HOME/.mutt/keybaseMutt
+			mkdir -p $HOME/.mutt/keybaseMutt/scripts
+
+		# Otherwise, abort mission.
+		else
+			echo "ABORT! ABORT! ABORT!"
+			exit 1
+		fi
+
+	elif [ ! -d "$HOME/.mutt/keybaseMuttBACKUP" ]; then
+		echo "Backing up previous install."
+		cp -R $HOME/.mutt/keybaseMutt $HOME/.mutt/keybaseMuttBACKUP
+		rm -r $HOME/.mutt/keybaseMutt
+		mkdir -p $HOME/.mutt/keybaseMutt/scripts
+	fi
+# Otherwise, make a backup
+elif [ ! -d "$HOME/.mutt/keybaseMutt" ]; then
+	echo "Installing your program..."
+	mkdir -p $HOME/.mutt/keybaseMutt/scripts
+
+fi
+
+# Copy my directory to your directory.
+cp ./keybase.py  $HOME/.mutt/keybaseMutt
+cp ./pgpdecrypt.sh $HOME/.mutt/keybaseMutt/scripts
+cp ./decrypt.sh $HOME/.mutt/keybaseMutt/scripts
+cp ./verify.sh $HOME/.mutt/keybaseMutt/scripts
+cp ./pgpverify.sh $HOME/.mutt/keybaseMutt/scripts
+
+# Yay! Stuff's installed!
+echo "You'll need to include a path to '~/.mutt/keybase/scripts' in your shell's rc file. If you've done this previously on your computer, press 'n'."
+echo "Do you use [b]ash, [k]sh, or [z]sh? [n]"
+echo "(You use $SHELL)"
+read shellInput
+if [ $shellInput == 'b' ]; then
+	echo 'export PATH="$PATH:~/.mutt/keybaseMutt/scripts"' >> $HOME/.bashrc
+elif [ $shellInput == 'k' ]; then
+	echo 'export PATH="$PATH:~/.mutt/keybaseMutt/scripts"' >> $HOME/.kshrc
+elif [ $shellInput == 'z' ]; then
+	echo 'export PATH="$PATH:~/.mutt/keybaseMutt/scripts"' >> $HOME/.zshrc
+else 
+	echo "If you use something another shell, you'll need to add the path manually."
+fi
+
+echo "Please restart your shell to be able to use the scripts (closing and reopening the terminal is easiest)."
