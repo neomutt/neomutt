2f4417e898ac719dee6b0ccb53668533410869e1
Vincent Lefevre <vincent@vinc17.net>
2016-08-23 13:32:31 +0200

    Filter out zero width no-break space (U+FEFF).

--- mbyte.c
+++ mbyte.c
@@ -530,6 +530,7 @@ int is_display_corrupting_utf8 (wchar_t wc)
   if (wc == (wchar_t)0x200f ||   /* bidi markers: #3827 */
       wc == (wchar_t)0x200e ||
       wc == (wchar_t)0x00ad ||   /* soft hyphen: #3848 */
+      wc == (wchar_t)0xfeff ||   /* zero width no-break space */
       (wc >= (wchar_t)0x202a &&  /* misc directional markers: #3854 */
        wc <= (wchar_t)0x202e))
     return 1;
