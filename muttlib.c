1fc8d47149cb5b71a0082858b0040b8d8971a7fa
David Champion <dgc@bikeshed.us>
2016-08-30 16:30:06 -0700

    Moves mutt_copy_list to muttlib.c, where it belongs.

--- muttlib.c
+++ muttlib.c
@@ -311,6 +311,26 @@ void mutt_free_list (LIST **list)
   }
 }
 
+LIST *mutt_copy_list (LIST *p)
+{
+  LIST *t, *r=NULL, *l=NULL;
+
+  for (; p; p = p->next)
+  {
+    t = (LIST *) safe_malloc (sizeof (LIST));
+    t->data = safe_strdup (p->data);
+    t->next = NULL;
+    if (l)
+    { 
+      r->next = t;
+      r = r->next;
+    }
+    else
+      l = r = t;
+  }
+  return (l);
+}
+
 HEADER *mutt_dup_header(HEADER *h)
 {
   HEADER *hnew;
