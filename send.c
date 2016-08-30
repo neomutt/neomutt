8d2cbc47be4123709e86177c470cbe1822669448
David Champion <dgc@bikeshed.us>
2016-08-30 16:30:06 -0700

    Moves mutt_copy_list to muttlib.c, where it belongs.

--- send.c
+++ send.c
@@ -392,26 +392,6 @@ static void process_user_header (ENVELOPE *env)
   }
 }
 
-LIST *mutt_copy_list (LIST *p)
-{
-  LIST *t, *r=NULL, *l=NULL;
-
-  for (; p; p = p->next)
-  {
-    t = (LIST *) safe_malloc (sizeof (LIST));
-    t->data = safe_strdup (p->data);
-    t->next = NULL;
-    if (l)
-    {
-      r->next = t;
-      r = r->next;
-    }
-    else
-      l = r = t;
-  }
-  return (l);
-}
-
 void mutt_forward_intro (FILE *fp, HEADER *cur)
 {
   char buffer[STRING];
