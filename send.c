neomutt/neomutt

commit 395911ea4c20e57f121cb169e0f2083ff5e1ae33

tidy code to mutt standards

send.c
@@ -1233,30 +1233,40 @@ static int has_recips (ADDRESS *a)
 }
 
 int
-mutt_search_attach_keyword(char* filename)
+mutt_search_attach_keyword (char *filename)
 {
-  /* searches for the magic keyword "attach" within a file */
-  int found = 0;
-  char* inputline = malloc(1024);
-  char* lowerKeyword = malloc(strlen(AttachKeyword)+1);
-  FILE *attf = fopen(filename, "r");
+  /* Search for the keyword in AttachKeyword within a file */
+  int klen = mutt_strlen (AttachKeyword) + 1;
+  if (klen == 1)
+    return 0;
+
+  FILE *attf = safe_fopen (filename, "r");
+  if (!attf)
+    return 0;
+
+  char *lowerKeyword = safe_malloc (klen);
+  char *inputline = safe_malloc (LONG_STRING);
   int i;
-  for (i=0; i <= strlen(AttachKeyword); i++) {
-    lowerKeyword[i] = tolower(AttachKeyword[i]);
-  }
-  while (!feof(attf)) {
-    fgets(inputline, 1024, attf);
-    for (i=0; i < strlen(inputline); i++) {
-      inputline[i] = tolower(inputline[i]);
+  for (i = 0; i <= klen; i++)
+    lowerKeyword[i] = tolower (AttachKeyword[i]);
+
+  int found = 0;
+  while (!feof (attf))
+  {
+    fgets (inputline, LONG_STRING, attf);
+    int ilen = strlen (inputline);
+    for (i = 0; i < ilen; i++)
+      inputline[i] = tolower (inputline[i]);
+
+    if (strstr (inputline, lowerKeyword))
+    {
+      found = 1;
+      break;
     }
-    if (strstr(inputline, lowerKeyword)) {
-        found = 1;
-        break;
-    }
   }
-  free(inputline);
-  free(lowerKeyword);
-  fclose(attf);
+  FREE (&inputline);
+  FREE (&lowerKeyword);
+  safe_fclose (&attf);
   return found;
 }
 
@@ -1892,17 +1902,20 @@ main_loop:
   }
 #endif
 
-  if (mutt_search_attach_keyword(msg->content->filename) &&
+  if (mutt_search_attach_keyword (msg->content->filename) &&
          !msg->content->next &&
-         query_quadoption(OPT_ATTACH, _("No attachments, cancel sending?")) != MUTT_NO)
+         query_quadoption (OPT_ATTACH, _("No attachments, cancel sending?")) != MUTT_NO)
   {
     /* if the abort is automatic, print an error message */
-    if (quadoption (OPT_ATTACH) == MUTT_YES) {
-         char errorstr[512];
-         if (snprintf(errorstr, 512,
-                       "Message contains magic keyword \"%s\", but no attachments. Not sending.", AttachKeyword)==-1)
-               errorstr[511] = 0; // terminate if need be. our string shouldnt be this long.
-      mutt_error _(errorstr);
+    if (quadoption (OPT_ATTACH) == MUTT_YES)
+    {
+      char errorstr[STRING];
+      if (snprintf (errorstr, STRING,
+        _("Message contains magic keyword \"%s\", but no attachments. Not sending."), AttachKeyword) == -1)
+      {
+        errorstr[STRING] = 0; // terminate if need be. our string shouldn't be this long.
+      }
+      mutt_error (errorstr);
     }
     goto main_loop;
   }

mutt.h
@@ -321,9 +321,7 @@ enum
   OPT_CATCHUP,
   OPT_FOLLOWUPTOPOSTER,
 #endif
-
-OPT_ATTACH, /* forgotten attachment detector */
-
+  OPT_ATTACH, /* forgotten attachment detector */
   /* THIS MUST BE THE LAST VALUE. */
   OPT_MAX
 };

commit 9cf241ce2cafae3591e0cc965e27fca9ac2f6b34

Fix buffer overrun in search for attach keyword
Regression introduced by 395911ea4c20e57f121cb169e0f2083ff5e1ae33.

In the process of tidying

    char* lowerKeyword = malloc(strlen(AttachKeyword)+1);
    ...
    for (i=0; i <= strlen(AttachKeyword); i++)

was changed to

    int klen = mutt_strlen (AttachKeyword) + 1;
    ...
    char *lowerKeyword = safe_malloc (klen);
    ...
    for (i = 0; i <= klen; i++)

Now that klen is the actual allocation length, instead of the string
length, the loop comparison needs to be adjusted accordingly.

send.c
@@ -1247,7 +1247,7 @@ mutt_search_attach_keyword (char *filename)
   char *lowerKeyword = safe_malloc (klen);
   char *inputline = safe_malloc (LONG_STRING);
   int i;
-  for (i = 0; i <= klen; i++)
+  for (i = 0; i < klen; i++)
     lowerKeyword[i] = tolower (AttachKeyword[i]);
 
   int found = 0;