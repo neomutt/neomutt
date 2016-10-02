81041f8c22f05be2865dec514d23de16c3893e3f
Richard Russon <rich@flatcap.org>
2016-10-02 13:01:06 +0100

    build fix for strndup / malloc
    Some systems don't have strndup() and rely on our version.
    I changed it to use Mutt's safe_malloc(), but unfortunately it gets
    compiled into some tools which don't have that function.

--- strndup.c
+++ strndup.c
@@ -11,7 +11,7 @@
 char *strndup(const char *s, size_t n)
 {
 	size_t len = strnlen(s, n);
-	char *new = safe_malloc((len + 1) * sizeof(char));
+	char *new = malloc((len + 1) * sizeof(char));
 	if (!new)
 		return NULL;
 	new[len] = '\0';
