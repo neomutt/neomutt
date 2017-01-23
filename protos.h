d89f5d025fe889f6a5df5b21a154874aaa53d093
Pietro Cerutti <gahr@gahr.ch>
2017-01-23 14:45:25 +0000

    Remove in-house implementation of setenv
    Issue: #325

--- protos.h
+++ protos.h
@@ -560,10 +560,6 @@ int ci_send_message (int, HEADER *, char *, CONTEXT *, HEADER *);
 
 /* prototypes for compatibility functions */
 
-#ifndef HAVE_SETENV
-int setenv (const char *, const char *, int);
-#endif
-
 #ifndef HAVE_STRSEP
 char *strsep (char **, const char *);
 #endif
