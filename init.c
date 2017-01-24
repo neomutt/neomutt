931bd2bd3334e275265da1b63454b0878a6706ca
Kevin McCarthy <kevin@8t8.us>
2017-01-24 15:33:23 -0800

    Allow "unsubjectrc *" to remove all patterns.
    Thanks to Aaron Schrab for the original patch.

--- init.c
+++ init.c
@@ -928,6 +928,14 @@ static int parse_unreplace_list (BUFFER *buf, BUFFER *s, unsigned long data, BUF
   }
 
   mutt_extract_token(buf, s, 0);
+
+  /* "*" is a special case. */
+  if (!mutt_strcmp (buf->data, "*"))
+  {
+    mutt_free_replace_list (list);
+    return 0;
+  }
+
   remove_from_replace_list(list, buf->data);
   return 0;
 }
