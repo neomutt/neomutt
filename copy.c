1fba72028cd27ef1f59b434aed08b6ab4096b982
Kevin McCarthy <kevin@8t8.us>
2017-01-29 11:02:50 -0800

    Fix the x-label update code check location.
    The x-label comparison was outside the "beginning of header" block.
    
    This meant that it could theoretically match a continuation line.
    Additionally, the continuation lines of x-labels would not be
    stripped, because the comparison was after the ignore variable was
    reset.
    
    Move the comparison inside the block and before the ignore reset.

--- copy.c
+++ copy.c
@@ -112,6 +112,10 @@ mutt_copy_hdr (FILE *in, FILE *out, LOFF_T off_start, LOFF_T off_end, int flags,
 	if ((flags & CH_UPDATE_IRT) &&
 	    ascii_strncasecmp ("In-Reply-To:", buf, 12) == 0)
 	  continue;
+	if (flags & CH_UPDATE_LABEL &&
+	    ascii_strncasecmp ("X-Label:", buf, 8) == 0)
+	  continue;
+
 	ignore = 0;
       }
 
@@ -123,10 +127,6 @@ mutt_copy_hdr (FILE *in, FILE *out, LOFF_T off_start, LOFF_T off_end, int flags,
 	    (mutt_strncasecmp ("Keywords:", buf, 9) == 0))
 	  continue;
       }
-
-      if (flags & CH_UPDATE_LABEL &&
-	  ascii_strncasecmp ("X-Label:", buf, 8) == 0)
-	continue;
 
       if (!ignore && fputs (buf, out) == EOF)
 	return (-1);
