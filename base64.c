673d36b7ba92dbfdad61a7a43ceaa50c993feeb2
Damien Riegel <damien.riegel@savoirfairelinux.com>
2017-02-04 21:13:57 -0500

    make rfc2047 use generic base64 encode function
    This was another source of a custom base64 encode function. Let's keep
    the same logic as before: encode 3 bytes (or less) at a time. Also,
    switch from a `for` to a `while` loop as `dlen` can simply be used as a
    stop condition.
    
    B64Chars can now be made static to base64.c.

--- base64.c
+++ base64.c
@@ -48,7 +48,7 @@
 
 #define BAD     -1
 
-const char B64Chars[64] = {
+static const char B64Chars[64] = {
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
   'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
   'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
