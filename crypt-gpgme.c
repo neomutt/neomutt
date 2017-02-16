34a5c8177d57824b425cde673f5502fc93f24992
Richard Russon <rich@flatcap.org>
2017-02-16 12:27:14 +0000

    fix: resource leak - CID 83478

--- crypt-gpgme.c
+++ crypt-gpgme.c
@@ -2511,7 +2511,7 @@ int pgp_gpgme_application_handler (BODY *m, STATE *s)
                   tmpfname = data_object_to_tempfile (plaintext, NULL, &pgpout);
                   if (!tmpfname)
                     {
-                      pgpout = NULL;
+                      safe_fclose (&pgpout);
                       state_puts (_("Error: copy data failed\n"), s);
                     }
                   else
