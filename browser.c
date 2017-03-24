3ce92e27de864dc7594ec9c55971df20586f7371
Richard Russon <rich@flatcap.org>
2017-03-24 21:48:24 +0000

    fix: typo in browser_compare_count{_new}
    A typo meant that sorting by "count" or "count new" could not be done in
    reverse.

--- browser.c
+++ browser.c
@@ -145,9 +145,9 @@ static int browser_compare_count (const void *a, const void *b)
   if (pa->has_buffy && pb->has_buffy)
     r = pa->msg_count - pb->msg_count;
   else if (pa->has_buffy)
-    return r = -1;
+    r = -1;
   else
-    return r = 1;
+    r = 1;
 
   return ((BrowserSort & SORT_REVERSE) ? -r : r);
 }
@@ -161,9 +161,9 @@ static int browser_compare_count_new (const void *a, const void *b)
   if (pa->has_buffy && pb->has_buffy)
     r = pa->msg_unread - pb->msg_unread;
   else if (pa->has_buffy)
-    return r = -1;
+    r = -1;
   else
-    return r = 1;
+    r = 1;
 
   return ((BrowserSort & SORT_REVERSE) ? -r : r);
 }
