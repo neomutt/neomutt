1a86f6e00193623e6ff8e4b4ce49ce77a0dda5c3
Kevin McCarthy <kevin@8t8.us>
2017-03-26 12:27:15 -0700

    Fix mutt_refresh() pausing during macro events.
    Changeset a07e8215a0ef split input buffering into two pools.
    Unfortunately, the mutt_refresh() was not changed to check the correct
    buffer count, resulting in unnecessary refreshes during macros.
    
    The SSL interactive certificate prompts set OPTIGNOREMACROEVENTS and
    then put up a confirmation menu.  Perhaps we've just been lucky, but
    it seems we should refresh the screen in those cases if we're in the
    middle of a macro.  Add a check for this option in mutt_refresh() too.

--- curs_lib.c
+++ curs_lib.c
@@ -78,7 +78,8 @@ void mutt_refresh (void)
     return;
 
   /* don't refresh in the middle of macros unless necessary */
-  if (UngetCount && !option (OPTFORCEREFRESH))
+  if (MacroBufferCount && !option (OPTFORCEREFRESH) &&
+      !option (OPTIGNOREMACROEVENTS))
     return;
 
   /* else */
