02effb6e7f30186b4a2c105e2036d56383e51277
Vincent Lefevre <vincent@vinc17.net>
2017-03-31 17:29:35 +0200

    Filter other directional markers that corrupt the screen.

--- mbyte.c
+++ mbyte.c
@@ -531,6 +531,8 @@ int is_display_corrupting_utf8 (wchar_t wc)
       wc == (wchar_t)0x200e ||
       wc == (wchar_t)0x00ad ||   /* soft hyphen: #3848 */
       wc == (wchar_t)0xfeff ||   /* zero width no-break space */
+      (wc >= (wchar_t)0x2066 &&  /* misc directional markers */
+       wc <= (wchar_t)0x2069) ||
       (wc >= (wchar_t)0x202a &&  /* misc directional markers: #3854 */
        wc <= (wchar_t)0x202e))
     return 1;
