417636d100441cc2939dc98e31a20166eee72b94
Richard Russon <rich@flatcap.org>
2017-04-11 13:16:56 +0100

    string_is_address

--- alias.c
+++ alias.c
@@ -598,15 +598,15 @@ int mutt_alias_complete (char *s, size_t buflen)
   return 0;
 }
 
-static int string_is_address(const char *str, const char *u, const char *d)
+static bool string_is_address(const char *str, const char *u, const char *d)
 {
   char buf[LONG_STRING];
 
   snprintf(buf, sizeof(buf), "%s@%s", NONULL(u), NONULL(d));
   if (ascii_strcasecmp(str, buf) == 0)
-    return 1;
+    return true;
 
-  return 0;
+  return false;
 }
 
 /* returns true if the given address belongs to the user. */
