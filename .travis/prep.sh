5b11f2fc9dbe080a8fcfcfd49ea6f11a11fccbc2
Richard Russon <rich@flatcap.org>
2017-05-18 15:40:00 +0100

    travis: add deploy script
    [ci skip]

---
+++ .travis/prep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git config --global user.email "rich@flatcap.org"
+git config --global user.name "Richard Russon (DEPLOY)"
+
+mkdir -p ~/.ssh
+echo "Host github.com" >> ~/.ssh/config
+echo "        StrictHostKeyChecking no" >> ~/.ssh/config
+chmod 600 ~/.ssh/config
+
+cd .travis || exit
+openssl aes-256-cbc -K $encrypted_ff1b3f8609ac_key -iv $encrypted_ff1b3f8609ac_iv -in travis-deploy-github.enc -out travis-deploy-github.pem -d
+chmod 0400 travis-deploy-github.pem
+
