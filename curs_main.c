5b3afb5d7a91281d287ded5a1c8d69651c7b4f15
Kevin McCarthy <kevin@8t8.us>
2017-06-01 15:17:05 -0700

    Fix tag-prefix to not abort on $timeout.
    If $timeout is set very low, then it can cancel the tag-prefix before
    the user has time to press the desired command.
    
    Change the code to set the tag flag, and then recontinue with normal
    event processing instead.  Cancel on an abort, but continue with a timeout.
    
    Thanks to Lauri Tirkkonen for reporting the issue.

--- curs_main.c
+++ curs_main.c
@@ -916,7 +916,11 @@ int mutt_index_menu(void)
 
   while (true)
   {
-    tag = 0; /* clear the tag-prefix */
+    /* Clear the tag prefix unless we just started it.  Don't clear
+     * the prefix on a timeout (op==-2), but do clear on an abort (op==-1)
+     */
+    if (tag && op != OP_TAG_PREFIX && op != OP_TAG_PREFIX_COND && op != -2)
+      tag = 0;
 
     /* check if we need to resort the index because just about
      * any 'op' below could do mutt_enter_command(), either here or
@@ -1037,6 +1041,13 @@ int mutt_index_menu(void)
     {
       index_menu_redraw(menu);
 
+      /* give visual indication that the next command is a tag- command */
+      if (tag)
+      {
+        mutt_window_mvaddstr(MuttMessageWindow, 0, 0, "tag-");
+        mutt_window_clrtoeol(MuttMessageWindow);
+      }
+
       if (menu->current < menu->max)
         menu->oldcurrent = menu->current;
       else
@@ -1071,17 +1082,28 @@ int mutt_index_menu(void)
 
       mutt_debug(4, "mutt_index_menu[%d]: Got op %d\n", __LINE__, op);
 
+      /* either user abort or timeout */
       if (op < 0)
       {
         mutt_timeout_hook();
-        continue; /* either user abort or timeout */
+        if (tag)
+          mutt_window_clearline(MuttMessageWindow, 0);
+        continue;
       }
 
       mutt_curs_set(1);
 
       /* special handling for the tag-prefix function */
-      if (op == OP_TAG_PREFIX)
+      if (op == OP_TAG_PREFIX || op == OP_TAG_PREFIX_COND)
       {
+        /* A second tag-prefix command aborts */
+        if (tag)
+        {
+          tag = 0;
+          mutt_window_clearline(MuttMessageWindow, 0);
+          continue;
+        }
+
         if (!Context)
         {
           mutt_error(_("No mailbox is open."));
@@ -1090,54 +1112,22 @@ int mutt_index_menu(void)
 
         if (!Context->tagged)
         {
-          mutt_error(_("No tagged messages."));
+          if (op == OP_TAG_PREFIX)
+            mutt_error(_("No tagged messages."));
+          else if (op == OP_TAG_PREFIX_COND)
+          {
+            mutt_flush_macro_to_endcond();
+            mutt_message(_("Nothing to do."));
+          }
           continue;
         }
-        tag = 1;
-
-        /* give visual indication that the next command is a tag- command */
-        mutt_window_mvaddstr(MuttMessageWindow, 0, 0, "tag-");
-        mutt_window_clrtoeol(MuttMessageWindow);
 
         /* get the real command */
-        if ((op = km_dokey(MENU_MAIN)) == OP_TAG_PREFIX)
-        {
-          /* abort tag sequence */
-          mutt_window_clearline(MuttMessageWindow, 0);
-          continue;
-        }
+        tag = 1;
+        continue;
       }
       else if (option(OPTAUTOTAG) && Context && Context->tagged)
         tag = 1;
-
-      if (op == OP_TAG_PREFIX_COND)
-      {
-        if (!Context)
-        {
-          mutt_error(_("No mailbox is open."));
-          continue;
-        }
-
-        if (!Context->tagged)
-        {
-          mutt_flush_macro_to_endcond();
-          mutt_message(_("Nothing to do."));
-          continue;
-        }
-        tag = 1;
-
-        /* give visual indication that the next command is a tag- command */
-        mutt_window_mvaddstr(MuttMessageWindow, 0, 0, "tag-");
-        mutt_window_clrtoeol(MuttMessageWindow);
-
-        /* get the real command */
-        if ((op = km_dokey(MENU_MAIN)) == OP_TAG_PREFIX)
-        {
-          /* abort tag sequence */
-          mutt_window_clearline(MuttMessageWindow, 0);
-          continue;
-        }
-      }
 
       mutt_clear_error();
     }
