6163bbe87c489ac58c9c53d4b6c8df442b5c567b
Richard Russon <rich@flatcap.org>
2017-06-08 22:29:30 +0100

    Revert "Move setting of GPG_TTY to mutt_init(). (see #3948)"
    This reverts commit 89ddfa61c180de00769f6e1798579ec80349a4eb.
    
    This commit was created by upstream and then reverted citing possible
    security problems leaking the GPG_TTY env var.

--- ncrypt/pgp.c
+++ ncrypt/pgp.c
@@ -31,6 +31,7 @@
 #include <regex.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
@@ -96,13 +97,21 @@ int pgp_valid_passphrase(void)
   return 0;
 }
 
-/* This function used to do more: check GPG_AGENT_INFO,
- * set GPG_TTY.  GPG_AGENT_INFO is no longer exported, and GPG_TTY
- * is now set in mutt_init().
- */
 bool pgp_use_gpg_agent(void)
 {
-  return option (OPTUSEGPGAGENT);
+  char *tty = NULL;
+
+  /* GnuPG 2.1 no longer exports GPG_AGENT_INFO */
+  if (!option(OPTUSEGPGAGENT))
+    return false;
+
+  if ((tty = ttyname(0)))
+  {
+    setenv("GPG_TTY", tty, 0);
+    mutt_envlist_set("GPG_TTY", tty, false);
+  }
+
+  return true;
 }
 
 static struct PgpKeyInfo *_pgp_parent(struct PgpKeyInfo *k)
