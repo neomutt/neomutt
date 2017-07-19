1b53b9602e1b4788278ff128fbbbd7d6a98f7703
Kevin McCarthy <kevin@8t8.us>
2017-07-19 14:04:32 -0700

    Fix menu color calls to occur before positioning the cursor. (see #3956)
    It is possible for menu->color() to end up fetching an imap message,
    and therefore generating a status message.
    
    Because of this, we need to make those calls before we position the cursor.

--- menu.c
+++ menu.c
@@ -389,6 +389,7 @@ void menu_redraw_index(struct Menu *menu)
 void menu_redraw_motion(struct Menu *menu)
 {
   char buf[LONG_STRING];
+  int old_color, cur_color;
 
   if (menu->dialog)
   {
@@ -396,8 +397,13 @@ void menu_redraw_motion(struct Menu *menu)
     return;
   }
 
+  /* Note: menu->color() for the index can end up retrieving a message
+   * over imap (if matching against ~h for instance).  This can
+   * generate status messages.  So we want to call it *before* we
+   * position the cursor for drawing. */
+  old_color = menu->color(menu->oldcurrent);
   mutt_window_move(menu->indexwin, menu->oldcurrent + menu->offset - menu->top, 0);
-  ATTRSET(menu->color(menu->oldcurrent));
+  ATTRSET(old_color);
 
   if (option(OPT_ARROW_CURSOR))
   {
@@ -409,8 +415,7 @@ void menu_redraw_motion(struct Menu *menu)
       menu_make_entry(buf, sizeof(buf), menu, menu->oldcurrent);
       menu_pad_string(menu, buf, sizeof(buf));
       mutt_window_move(menu->indexwin, menu->oldcurrent + menu->offset - menu->top, 3);
-      print_enriched_string(menu->oldcurrent, menu->color(menu->oldcurrent),
-                            (unsigned char *) buf, 1);
+      print_enriched_string(menu->oldcurrent, old_color, (unsigned char *) buf, 1);
     }
 
     /* now draw it in the new location */
@@ -422,16 +427,15 @@ void menu_redraw_motion(struct Menu *menu)
     /* erase the current indicator */
     menu_make_entry(buf, sizeof(buf), menu, menu->oldcurrent);
     menu_pad_string(menu, buf, sizeof(buf));
-    print_enriched_string(menu->oldcurrent, menu->color(menu->oldcurrent),
-                          (unsigned char *) buf, 1);
+    print_enriched_string(menu->oldcurrent, old_color, (unsigned char *) buf, 1);
 
     /* now draw the new one to reflect the change */
+    cur_color = menu->color (menu->current);
     menu_make_entry(buf, sizeof(buf), menu, menu->current);
     menu_pad_string(menu, buf, sizeof(buf));
     SETCOLOR(MT_COLOR_INDICATOR);
     mutt_window_move(menu->indexwin, menu->current + menu->offset - menu->top, 0);
-    print_enriched_string(menu->current, menu->color(menu->current),
-                          (unsigned char *) buf, 0);
+    print_enriched_string(menu->current, cur_color, (unsigned char *) buf, 0);
   }
   menu->redraw &= REDRAW_STATUS;
   NORMAL_COLOR;
