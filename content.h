23211b3a022aaa6a4d3c999d3fc44235f992b4a7
Kevin McCarthy <kevin@8t8.us>
2017-07-20 17:30:05 -0700

    When guessing an attachment type, don't allow text/plain if there is a null character. (see #2933)
    Type text/plain should not contain any null characters.  Slightly
    improve the type guesser by forcing an attachment with any null
    characters to be application/octet-stream.
    
    Note the type guesser could use much more improvement, but this is an
    easy and obvious fix.

--- content.h
+++ content.h
@@ -34,6 +34,7 @@ struct Content
 {
   long hibin;      /**< 8-bit characters */
   long lobin;      /**< unprintable 7-bit chars (eg., control chars) */
+  long nulbin;     /**< null characters (0x0) */
   long crlf;       /**< `\r` and `\n` characters */
   long ascii;      /**< number of ascii chars */
   long linemax;    /**< length of the longest line in the file */
