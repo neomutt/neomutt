aa5c071f1e8499a4840bf94a76f6df5264e92ebf
Anton Rieger <a_rieger15@cs.uni-kl.de>
2017-08-24 00:41:00 +0200

    Unsubscribe after deleting an imap folder
    Only added two line to `browser.c` in `_mutt_select_file` case `OP_DELETE_MAILBOX`.

--- browser.c
+++ browser.c
@@ -1577,6 +1577,8 @@ void _mutt_select_file(char *f, size_t flen, int flags, char ***files, int *numf
           {
             if (!imap_delete_mailbox(Context, &mx))
             {
+              /* Unsubsribe to prevent dangling subscriptions */
+              imap_subscribe(state.entry[menu->current].name, 0);
               /* free the mailbox from the browser */
               FREE(&((state.entry)[nentry].name));
               FREE(&((state.entry)[nentry].desc));
