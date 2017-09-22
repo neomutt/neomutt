c9dcfc485393f86e3604b8bf84af891aa614cc62
Kevin McCarthy <kevin@8t8.us>
2017-09-22 11:14:27 -0700

    Add missing IMAP_CMD_POLL flag in imap buffy check.
    I missed adding the flag to one of the imap_exec() calls inside the
    loop, when a different server is encountered in the mailboxes list.

--- imap/imap.c
+++ imap/imap.c
@@ -1671,7 +1671,7 @@ int imap_buffy_check(int force, int check_stats)
     {
       /* Send commands to previous server. Sorting the buffy list
        * may prevent some infelicitous interleavings */
-      if (imap_exec(lastdata, NULL, IMAP_CMD_FAIL_OK) == -1)
+      if (imap_exec(lastdata, NULL, IMAP_CMD_FAIL_OK | IMAP_CMD_POLL) == -1)
         mutt_debug(1, "Error polling mailboxes\n");
 
       lastdata = NULL;
