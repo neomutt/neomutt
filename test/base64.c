74e563ce48786c95b5ca1b61078a8d95e5ab7e44
Pietro Cerutti <gahr@gahr.ch>
2018-01-22 15:26:43 +0000

    Unit test framework (#1031)
    Minimal unit-test framework to test C library APIs

---
+++ test/base64.c
@@ -0,0 +1,92 @@
+#define TEST_NO_MAIN
+#include "acutest.h"
+
+#include "mutt/base64.h"
+
+#include <string.h>
+
+static const char clear[] = "Hello";
+static const char encoded[] = "SGVsbG8=";
+
+void test_base64_encode(void)
+{
+  char buffer[16];
+  size_t len = mutt_b64_encode(buffer, clear, sizeof(clear) - 1, sizeof(buffer));
+  if (!TEST_CHECK(len == sizeof(encoded) - 1))
+  {
+    TEST_MSG("Expected: %zu", sizeof(encoded) - 1);
+    TEST_MSG("Actual  : %zu", len);
+  }
+  if (!TEST_CHECK(strcmp(buffer, encoded) == 0))
+  {
+    TEST_MSG("Expected: %zu", encoded);
+    TEST_MSG("Actual  : %zu", buffer);
+  }
+}
+
+void test_base64_decode(void)
+{
+  char buffer[16];
+  int len = mutt_b64_decode(buffer, encoded);
+  if (!TEST_CHECK(len == sizeof(clear) - 1))
+  {
+    TEST_MSG("Expected: %zu", sizeof(clear) - 1);
+    TEST_MSG("Actual  : %zu", len);
+  }
+  buffer[len] = '\0';
+  if (!TEST_CHECK(strcmp(buffer, clear) == 0))
+  {
+    TEST_MSG("Expected: %s", clear);
+    TEST_MSG("Actual  : %s", buffer);
+  }
+}
+
+void test_base64_lengths(void)
+{
+  const char *in = "FuseMuse";
+  char out1[32];
+  char out2[32];
+  size_t enclen;
+  int declen;
+
+  /* Encoding a zero-length string should fail */
+  enclen = mutt_b64_encode(out1, in, 0, 32);
+  if (!TEST_CHECK(enclen == 0))
+  {
+    TEST_MSG("Expected: %zu", 0);
+    TEST_MSG("Actual  : %zu", enclen);
+  }
+
+  /* Decoding a zero-length string should fail, too */
+  out1[0] = '\0';
+  declen = mutt_b64_decode(out2, out1);
+  if (!TEST_CHECK(declen == -1))
+  {
+    TEST_MSG("Expected: %zu", -1);
+    TEST_MSG("Actual  : %zu", declen);
+  }
+
+  /* Encode one to eight bytes, check the lengths of the returned string */
+  for (size_t i = 1; i <= 8; ++i)
+  {
+    enclen = mutt_b64_encode(out1, in, i, 32);
+    size_t exp = ((i + 2) / 3) << 2;
+    if (!TEST_CHECK(enclen == exp))
+    {
+      TEST_MSG("Expected: %zu", exp);
+      TEST_MSG("Actual  : %zu", enclen);
+    }
+    declen = mutt_b64_decode(out2, out1);
+    if (!TEST_CHECK(declen == i))
+    {
+      TEST_MSG("Expected: %zu", i);
+      TEST_MSG("Actual  : %zu", declen);
+    }
+    out2[declen] = '\0';
+    if (!TEST_CHECK(strncmp(out2, in, i) == 0))
+    {
+      TEST_MSG("Expected: %s", in);
+      TEST_MSG("Actual  : %s", out2);
+    }
+  }
+}
---
+++ test/rfc2047.c
@@ -0,0 +1,94 @@
+#define TEST_NO_MAIN
+#include "acutest.h"
+
+#include "mutt/charset.h"
+#include "mutt/memory.h"
+#include "mutt/rfc2047.h"
+#include "mutt/string2.h"
+
+#include <locale.h>
+
+static const struct
+{
+  const char *original; /* the string as received in the original email */
+  const char *decoded;  /* the expected plain-text string               */
+  const char *encoded;  /* the string as it's encoded by NeoMutt        */
+} test_data[] =
+    /* clang-format off */
+{
+  {
+    /* The string is split in the middle of a multi-byte sequence */
+      "=?UTF-8?Q?Kvie=C4=8Diame=20drauge=20pildyti=20ESO=20pasi=C5=BEad=C4?="
+      "\n =?UTF-8?Q?=97jim=C5=B3=20girliand=C4=85!?="
+    , "Kviečiame drauge pildyti ESO pasižadėjimų girliandą!"
+    , "=?utf-8?Q?Kvie=C4=8Diame_drauge_pildyti_ESO_pasi=C5=BEad=C4=97jim=C5=B3_g?="
+      "\n\t=?utf-8?Q?irliand=C4=85!?="
+  },
+  {
+    /* Reduced test case for split multi-byte sequence */
+      "=?utf-8?Q?=C4?==?utf-8?Q?=97?="
+    , "ė"
+    , "=?utf-8?B?xJc=?="
+  },
+  {
+    /* Make sure spaces after an encoded word are kept */
+      "=?utf-8?B?6IGq5piO55qE?=    Hello"
+    , "聪明的    Hello"
+    , "=?utf-8?B?6IGq5piO55qE?=    Hello"
+  },
+  {
+    /* Make sure spaces before an encoded word are kept */
+      "=?UTF-8?Q?Hello____=E8=81=AA=E6=98=8E=E7=9A=84?=" /* Roundcube style */
+    , "Hello    聪明的"
+    , "Hello    =?utf-8?B?6IGq5piO55qE?="
+  },
+  {
+    /* Make sure spaces between encoded words are kept */
+      "=?utf-8?B?6IGq5piO55qEICAgIOiBquaYjueahA==?="
+    , "聪明的    聪明的"
+    , "=?utf-8?B?6IGq5piO55qEICAgIOiBquaYjueahA==?="
+  }
+};
+/* clang-format on */
+
+void test_rfc2047(void)
+{
+  setlocale(LC_ALL, "en_US.UTF-8");
+  Charset = "utf-8";
+
+  for (size_t i = 0; i < mutt_array_size(test_data); ++i)
+  {
+    /* decode the original string */
+    char *s = mutt_str_strdup(test_data[i].original);
+    mutt_rfc2047_decode(&s);
+    if (!TEST_CHECK(strcmp(s, test_data[i].decoded) == 0))
+    {
+      TEST_MSG("Iteration: %zu", i);
+      TEST_MSG("Expected : %s", test_data[i].decoded);
+      TEST_MSG("Actual   : %s", s);
+    }
+    FREE(&s);
+
+    /* encode the expected result */
+    s = mutt_str_strdup(test_data[i].decoded);
+    mutt_rfc2047_encode(&s, NULL, 0, "utf-8");
+    if (!TEST_CHECK(strcmp(s, test_data[i].encoded) == 0))
+    {
+      TEST_MSG("Iteration: %zu", i);
+      TEST_MSG("Expected : %s", test_data[i].encoded);
+      TEST_MSG("Actual   : %s", s);
+    }
+    FREE(&s);
+
+    /* decode the encoded result */
+    s = mutt_str_strdup(test_data[i].encoded);
+    mutt_rfc2047_decode(&s);
+    if (!TEST_CHECK(strcmp(s, test_data[i].decoded) == 0))
+    {
+      TEST_MSG("Iteration: %zu", i);
+      TEST_MSG("Expected : %s", test_data[i].decoded);
+      TEST_MSG("Actual   : %s", s);
+    }
+    FREE(&s);
+  }
+}
