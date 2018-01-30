e0e163daa37ba1aecc4c0b480c1c6d486c977317
Richard Russon <rich@flatcap.org>
2018-01-30 22:34:03 +0000

    fix rfc2047 field decoding

--- mutt/rfc2047.c
+++ mutt/rfc2047.c
@@ -658,6 +658,9 @@ void mutt_rfc2047_encode(char **pd, const char *specials, int col, const char *c
  */
 void mutt_rfc2047_decode(char **pd)
 {
+  if (!pd || !*pd)
+    return;
+
   struct Buffer buf = { 0 }; /* Output buffer                          */
   char *s = *pd;             /* Read pointer                           */
   char *beg;                 /* Begin of encoded word                  */
@@ -745,5 +748,4 @@ void mutt_rfc2047_decode(char **pd)
 
   mutt_buffer_addch(&buf, '\0');
   *pd = buf.data;
-  return;
 }
