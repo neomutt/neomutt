fc690bffa7bb94305b1a972cec01da523ee46af5
Richard Russon <rich@flatcap.org>
2018-02-25 12:28:23 +0000

    fix: add missing library externs

--- mutt/message.h
+++ mutt/message.h
@@ -36,8 +36,8 @@
 #define N_(a) a
 #endif
 
-void (*mutt_error)  (const char *format, ...);
-void (*mutt_message)(const char *format, ...);
-void (*mutt_perror) (const char *message);
+extern void (*mutt_error)  (const char *format, ...);
+extern void (*mutt_message)(const char *format, ...);
+extern void (*mutt_perror) (const char *message);
 
 #endif /* _MUTT_MESSAGE_H */
