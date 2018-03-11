c6b6a9d9e95d7c90cece215c5e68a3e30d80db1a
Yubin Ruan <ablacktshirt@gmail.com>
2018-03-11 16:57:01 +0800

    support for multipart/alternative and multipart/mixed
    This work is based on commit 5b4f7a1342 from @somini and was discussed
    at <https://github.com/neomutt/neomutt/issues/587>. As of now, one can
    tag entries in the compose menu and use <group-alternatives> to group
    those entries into one. If all the entries are tagged (including the
    fundamental part), then the email become a multipart/alternative one,
    otherwise it becomes a multipart/mixed one.
    
    There is some weird things to be noted, though: when postponing a
    multipart/alternative mail and then recall it, those entries get
    split up. This does not occurred for multipart/mixed mails.

--- compose.c
+++ compose.c
@@ -1099,29 +1099,24 @@ int mutt_compose_menu(struct Header *msg, char *fcc, size_t fcclen,
           break;
         }
 
-        /* need to redo using mutt_gen_attach_list() */
-
         struct Body *group = mutt_body_new();
         group->type = TYPEMULTIPART;
-        group->subtype = "alternative";
+        group->subtype = mutt_str_strdup("alternative");
 
         struct Body *alts = NULL;
-        struct Body *bptr = NULL;
-        for (i = 0, bptr = msg->content; bptr && bptr->next;)
+        /* group tagged message into a multipart/alternative */
+        struct Body *bptr = msg->content;
+        for (i = 0; bptr;)
         {
-          /* always look at bptr->next, not bptr itself */
-          if (bptr->next->tagged)
+          if (bptr->tagged)
           {
-            /* untag */
-            bptr->next->tagged = false;
+            bptr->tagged = false;
 
             /* for first match, set group desc according to match */
 #define ALTS_TAG "Alternatives for \"%s\""
             if (!group->description)
             {
-              char *p = bptr->next->description;
-              if (!p)
-                p = bptr->next->filename;
+              char *p = bptr->description ? bptr->description : bptr->filename;
               if (p)
               {
                 group->description =
@@ -1130,24 +1125,23 @@ int mutt_compose_menu(struct Header *msg, char *fcc, size_t fcclen,
               }
             }
 
-            /* append bptr->next to the alts list,
+            /* append bptr to the alts list,
              * and remove from the msg->content list */
             if (alts == NULL)
             {
-              group->parts = alts = bptr->next;
-              bptr->next = bptr->next->next;
+              group->parts = alts = bptr;
+              bptr = bptr->next;
               alts->next = NULL;
             }
             else
             {
-              alts->next = bptr->next;
-              bptr->next = bptr->next->next;
+              alts->next = bptr;
+              bptr = bptr->next;
               alts = alts->next;
               alts->next = NULL;
             }
 
-            /* now delink the idx entry */
-            for (int j = i + 1; j < actx->idxlen - 1; ++j)
+            for (int j = i; j < actx->idxlen - 1; j++)
             {
               actx->idx[j] = actx->idx[j + 1];
             }
@@ -1160,19 +1154,14 @@ int mutt_compose_menu(struct Header *msg, char *fcc, size_t fcclen,
           }
         }
 
-        /* add group to attachment list */
-        for (bptr = msg->content; bptr->next; bptr = bptr->next)
-          ;
-        bptr->next = group;
         group->next = NULL;
+        mutt_generate_boundary(&group->parameter);
+
+        /* msg->content = NULL; */
 
         struct AttachPtr *gptr = mutt_mem_calloc(1, sizeof(struct AttachPtr));
         gptr->content = group;
-        actx->idx[actx->idxlen] = gptr;
         update_idx(menu, actx, gptr);
-
-        /* add a boundary */
-        mutt_generate_boundary(&group->parameter);
 
         /* if no group desc yet, make one up */
         if (!group->description)
