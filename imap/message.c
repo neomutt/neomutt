a561a21bdc4e1a68ef279cece974adab1cd4c648
Joey Pabalinas <joeypabalinas@gmail.com>
2018-04-09 12:38:38 -1000

    Revert "message.c: add error checks to imap_read_headers() sprintf()"
    This reverts commit 7c4c764dce3ca4dcccc7e4387768e2c4d9c23062.

--- imap/message.c
+++ imap/message.c
@@ -29,7 +29,6 @@
 
 #include "config.h"
 #include <ctype.h>
-#include <errno.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -694,11 +693,7 @@ int imap_read_headers(struct ImapData *idata, unsigned int msn_begin, unsigned i
     mutt_progress_init(&progress, _("Evaluating cache..."), MUTT_PROGRESS_MSG,
                        ReadInc, msn_end);
 
-    if (snprintf(buf, sizeof(buf), "UID FETCH 1:%u (UID FLAGS)", uidnext - 1) < 0)
-    {
-      mutt_error(_("Output error during UID FETCH 1:%u %s"), uidnext - 1, strerror(errno));
-      goto error_out_1;
-    }
+    snprintf(buf, sizeof(buf), "UID FETCH 1:%u (UID FLAGS)", uidnext - 1);
 
     imap_cmd_start(idata, buf);
 
