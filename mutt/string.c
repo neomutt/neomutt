378db96ab59124061cc01f191282a8c05069b3c4
Joey Pabalinas <joeypabalinas@gmail.com>
2018-04-08 23:16:58 -1000

    string.c: fix incorrect mutt_str_is_email_wsp return types
    The huge table of prototypes ended up having no space between
    the pointer return types and the function identifier, so add
    one space to separate them.
    
    Signed-off-by: Joey Pabalinas <joeypabalinas@gmail.com>

--- mutt/string.c
+++ mutt/string.c
@@ -669,7 +669,7 @@ char *mutt_str_skip_email_wsp(const char *s)
  * @param c Character to test
  * @retval boolean
  */
-int mutt_str_is_email_wsp(char c)
+bool mutt_str_is_email_wsp(char c)
 {
   return c && (strchr(EMAIL_WSP, c) != NULL);
 }
