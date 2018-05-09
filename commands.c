muttmua/mutt

Change prompt string for $crypt_verify_sig
Thanks to Michael Tatge for pointing out that the prompt is used for
both PGP and S/MIME.

commands.c
@@ -87,7 +87,8 @@ int mutt_display_message (HEADER *cur)
     else if (cur->security & SIGN)
     {
       /* find out whether or not the verify signature */
-      if (query_quadoption (OPT_VERIFYSIG, _("Verify PGP signature?")) == MUTT_YES)
+      /* L10N: Used for the $crypt_verify_sig prompt */
+      if (query_quadoption (OPT_VERIFYSIG, _("Verify signature?")) == MUTT_YES)
       {
 	cmflags |= MUTT_CM_VERIFY;
       }

neomutt/neomutt

commands.c
@@ -97,7 +97,8 @@ int mutt_display_message(struct Header *cur)
     else if (cur->security & SIGN)
     {
       /* find out whether or not the verify signature */
-      if (query_quadoption(CryptVerifySig, _("Verify PGP signature?")) == MUTT_YES)
+      /* L10N: Used for the $crypt_verify_sig prompt */
+      if (query_quadoption(CryptVerifySig, _("Verify signature?")) == MUTT_YES)
       {
         cmflags |= MUTT_CM_VERIFY;
       }