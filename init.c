b4c9f1f7d6311736fbf6de5ee5f3e0017a6fdafa
Marius Gedminas <marius@gedmin.as>
2018-05-23 12:59:18 +0300

    Apply coding style with clang-format

--- init.c
+++ init.c
@@ -3553,9 +3553,10 @@ int mutt_extract_token(struct Buffer *dest, struct Buffer *tok, int flags)
       *pc = '\0';
       if (flags & MUTT_TOKEN_BACKTICK_VARS)
       {
-          /* recursively extract tokens to interpolate variables */
-          mutt_extract_token(&cmd, tok, MUTT_TOKEN_QUOTE | MUTT_TOKEN_SPACE |
-            MUTT_TOKEN_COMMENT | MUTT_TOKEN_SEMICOLON);
+        /* recursively extract tokens to interpolate variables */
+        mutt_extract_token(&cmd, tok,
+                           MUTT_TOKEN_QUOTE | MUTT_TOKEN_SPACE |
+                               MUTT_TOKEN_COMMENT | MUTT_TOKEN_SEMICOLON);
       }
       else
       {
