6296f7153f0c9d5e5cd3aaf08f9731e56621bdd3
JerikoOne <jeriko.one@gmx.us>
2018-07-03 17:08:41 -0500

    Set length modifiers for group and desc
    nntp_add_group parses a line controlled by the connected nntp server.
    Restrict the maximum lengths read into the stack buffers group, and
    desc.

--- newsrc.c
+++ newsrc.c
@@ -592,7 +592,7 @@ int nntp_add_group(char *line, void *data)
 {
   struct NntpServer *nserv = data;
   struct NntpData *nntp_data = NULL;
-  char group[LONG_STRING];
+  char group[LONG_STRING] = "";
   char desc[HUGE_STRING] = "";
   char mod;
   anum_t first, last;
@@ -600,8 +600,12 @@ int nntp_add_group(char *line, void *data)
   if (!nserv || !line)
     return 0;
 
-  if (sscanf(line, "%s " ANUM " " ANUM " %c %[^\n]", group, &last, &first, &mod, desc) < 4)
+  /* These sscanf limits must match the sizes of the group and desc arrays */
+  if (sscanf(line, "%1023s " ANUM " " ANUM " %c %8191[^\n]", group, &last, &first, &mod, desc) < 4)
+  {
+    mutt_debug(4, "Cannot parse server line: %s\n", line);
     return 0;
+  }
 
   nntp_data = nntp_data_find(nserv, group);
   nntp_data->deleted = false;
