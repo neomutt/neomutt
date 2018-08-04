58e00ee4abd9ff8b28274ee522de46a37c01b27c
Richard Russon <rich@flatcap.org>
2018-08-04 20:36:10 +0100

    doxy: drop missing images

--- email/email.h
+++ email/email.h
@@ -43,8 +43,6 @@
  * | email/tags.c           | @subpage email_tags      |
  * | email/thread.c         | @subpage email_thread    |
  * | email/url.c            | @subpage email_url       |
- *
- * @image html libemail.svg
  */
 
 #ifndef _EMAIL_EMAIL_H
