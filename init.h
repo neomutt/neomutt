2eb486cd23fef57916771b74d3f3704cba41fbf6
Cody Brownstein <cbrownstein@liquidityllc.com>
2018-09-01 09:47:07 -0700

    Fix typo

--- init.h
+++ init.h
@@ -1492,7 +1492,7 @@ struct ConfigDef MuttVars[] = {
   ** .pp
   ** When \fIset\fP, NeoMutt will not open new IMAP connections to check for new
   ** mail.  NeoMutt will only check for new mail over existing IMAP
-  ** connections.  This is useful if you don't want to be prompted to
+  ** connections.  This is useful if you don't want to be prompted for
   ** user/password pairs on NeoMutt invocation, or if opening the connection
   ** is slow.
   */
