c949b77b5ec60b2f6e5f5cc515de4eea8b6cb9e0
Ian Zimmerman <itz@no-use.mooo.com>
2018-11-12 17:17:30 -0800

    mutt_list_find doesn't modify the list, so it should take a const*

--- mutt/list.h
+++ mutt/list.h
@@ -52,7 +52,7 @@ typedef void (*list_free_t)(void **ptr);
 
 void             mutt_list_clear(struct ListHead *h);
 int              mutt_list_compare(const struct ListHead *ah, const struct ListHead *bh);
-struct ListNode *mutt_list_find(struct ListHead *h, const char *data);
+struct ListNode *mutt_list_find(const struct ListHead *h, const char *data);
 void             mutt_list_free(struct ListHead *h);
 void             mutt_list_free_type(struct ListHead *h, list_free_t fn);
 struct ListNode *mutt_list_insert_after(struct ListHead *h, struct ListNode *n, char *s);
