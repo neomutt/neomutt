0b6043bc0dac191472f88cbad090b402bf693611
Pietro Cerutti <gahr@gahr.ch>
2018-11-20 15:10:52 +0000

    Simplify imap_logout, set imap state to disconnected

--- imap/imap.c
+++ imap/imap.c
@@ -691,17 +691,20 @@ static void imap_logout(struct ImapAccountData *adata)
 {
   /* we set status here to let imap_handle_untagged know we _expect_ to
    * receive a bye response (so it doesn't freak out and close the conn) */
-  if (adata->state != IMAP_DISCONNECTED)
+  if (adata->state == IMAP_DISCONNECTED)
   {
-    adata->status = IMAP_BYE;
-    imap_cmd_start(adata, "LOGOUT");
-    if (ImapPollTimeout <= 0 || mutt_socket_poll(adata->conn, ImapPollTimeout) != 0)
-    {
-      while (imap_cmd_step(adata) == IMAP_CMD_CONTINUE)
-        ;
-    }
-    mutt_socket_close(adata->conn);
+    return;
   }
+
+  adata->status = IMAP_BYE;
+  imap_cmd_start(adata, "LOGOUT");
+  if (ImapPollTimeout <= 0 || mutt_socket_poll(adata->conn, ImapPollTimeout) != 0)
+  {
+    while (imap_cmd_step(adata) == IMAP_CMD_CONTINUE)
+      ;
+  }
+  mutt_socket_close(adata->conn);
+  adata->state = IMAP_DISCONNECTED;
 }
 
 /**
