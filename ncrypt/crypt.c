c0d62bd40ff0bb115bf700d88076a2d71779191d
Kevin McCarthy <kevin@8t8.us>
2018-12-29 13:50:00 -0800

    Respect ignore for the protected subject display
    Co-authored-by: Richard Russon <rich@flatcap.org>

--- ncrypt/crypt.c
+++ ncrypt/crypt.c
@@ -1103,6 +1103,9 @@ int mutt_protected_headers_handler(struct Body *a, struct State *s)
   {
     if (a->mime_headers->subject)
     {
+      if ((s->flags & MUTT_DISPLAY) && Weed && mutt_matches_ignore("subject"))
+        return 0;
+
       state_mark_protected_header(s);
       mutt_write_one_header(s->fpout, "Subject", a->mime_headers->subject,
                             s->prefix, mutt_window_wrap_cols(MuttIndexWindow, Wrap),
