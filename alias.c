8a8577be230bddc01d387380a9f129f91a290646
Richard Russon <rich@flatcap.org>
2019-02-08 19:15:00 +0000

    boolify variables

--- alias.c
+++ alias.c
@@ -213,7 +213,8 @@ static int check_alias_name(const char *s, char *dest, size_t destlen)
   wchar_t wc;
   mbstate_t mb;
   size_t l;
-  int rc = 0, dry = !dest || !destlen;
+  int rc = 0;
+  bool dry = !dest || !destlen;
 
   memset(&mb, 0, sizeof(mbstate_t));
 
@@ -222,7 +223,7 @@ static int check_alias_name(const char *s, char *dest, size_t destlen)
   for (; s && *s && (dry || destlen) && (l = mbrtowc(&wc, s, MB_CUR_MAX, &mb)) != 0;
        s += l, destlen -= l)
   {
-    int bad = l == (size_t)(-1) || l == (size_t)(-2); /* conversion error */
+    bool bad = (l == (size_t)(-1)) || (l == (size_t)(-2)); /* conversion error */
     bad = bad || (!dry && l > destlen); /* too few room for mb char */
     if (l == 1)
       bad = bad || (!strchr("-_+=.", *s) && !iswalnum(wc));
