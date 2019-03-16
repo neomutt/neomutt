b50df91ef48b6ea3aee11f5548eeb03254f879b1
Richard Russon <rich@flatcap.org>
2019-03-16 12:42:57 +0000

    add checks to imap_account_match()

--- imap/util.c
+++ imap/util.c
@@ -1156,6 +1156,8 @@ void imap_disallow_reopen(struct Mailbox *m)
  */
 bool imap_account_match(const struct ConnAccount *a1, const struct ConnAccount *a2)
 {
+  if (!a1 || !a2)
+    return false;
   if (a1->type != a2->type)
     return false;
   if (mutt_str_strcasecmp(a1->host, a2->host) != 0)
