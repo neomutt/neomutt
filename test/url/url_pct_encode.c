b3bb6f8d2697fb72c98bcdb403fffee16c5f06f6
Richard Russon <rich@flatcap.org>
2019-04-28 14:13:47 +0100

    test: templates for url functions

---
+++ test/url/url_pct_encode.c
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * Test code for url_pct_encode()
+ *
+ * @authors
+ * Copyright (C) 2019 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define TEST_NO_MAIN
+#include "acutest.h"
+#include "config.h"
+#include "mutt/mutt.h"
+#include "email/lib.h"
+#include "address/lib.h"
+
+void test_url_pct_encode(void)
+{
+  // void url_pct_encode(char *buf, size_t buflen, const char *src);
+}
