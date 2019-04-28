315d16694210ef99924f193def03284475d579a9
Richard Russon <rich@flatcap.org>
2019-04-28 12:37:15 +0100

    test: templates for regex functions

---
+++ test/regex/main.c
@@ -0,0 +1,56 @@
+/**
+ * @file
+ * Test code for regex Operations
+ *
+ * @authors
+ * Copyright (C) 2019 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "acutest.h"
+
+/******************************************************************************
+ * Add your test cases to this list.
+ *****************************************************************************/
+#define NEOMUTT_TEST_LIST                                                      \
+  NEOMUTT_TEST_ITEM(test_mutt_regex_compile)                                   \
+  NEOMUTT_TEST_ITEM(test_mutt_regex_free)                                      \
+  NEOMUTT_TEST_ITEM(test_mutt_regexlist_add)                                   \
+  NEOMUTT_TEST_ITEM(test_mutt_regexlist_free)                                  \
+  NEOMUTT_TEST_ITEM(test_mutt_regexlist_match)                                 \
+  NEOMUTT_TEST_ITEM(test_mutt_regexlist_new)                                   \
+  NEOMUTT_TEST_ITEM(test_mutt_regexlist_remove)                                \
+  NEOMUTT_TEST_ITEM(test_mutt_regex_new)                                       \
+  NEOMUTT_TEST_ITEM(test_mutt_replacelist_add)                                 \
+  NEOMUTT_TEST_ITEM(test_mutt_replacelist_apply)                               \
+  NEOMUTT_TEST_ITEM(test_mutt_replacelist_free)                                \
+  NEOMUTT_TEST_ITEM(test_mutt_replacelist_match)                               \
+  NEOMUTT_TEST_ITEM(test_mutt_replacelist_new)                                 \
+  NEOMUTT_TEST_ITEM(test_mutt_replacelist_remove)
+
+/******************************************************************************
+ * You probably don't need to touch what follows.
+ *****************************************************************************/
+#define NEOMUTT_TEST_ITEM(x) void x(void);
+NEOMUTT_TEST_LIST
+#undef NEOMUTT_TEST_ITEM
+
+TEST_LIST = {
+#define NEOMUTT_TEST_ITEM(x) { #x, x },
+  NEOMUTT_TEST_LIST
+#undef NEOMUTT_TEST_ITEM
+  { 0 }
+};
