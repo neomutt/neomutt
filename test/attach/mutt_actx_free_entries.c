625606c1046dc3f4f169de9cc704faae11f48a78
Richard Russon <rich@flatcap.org>
2019-04-28 14:13:46 +0100

    test: templates for attach functions

---
+++ test/attach/mutt_actx_free_entries.c
@@ -0,0 +1,33 @@
+/**
+ * @file
+ * Test code for mutt_actx_free_entries()
+ *
+ * @authors
+ * Copyright (C) 2019 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define TEST_NO_MAIN
+#include "acutest.h"
+#include "config.h"
+#include "mutt/mutt.h"
+#include "email/lib.h"
+#include "address/lib.h"
+
+void test_mutt_actx_free_entries(void)
+{
+  // void mutt_actx_free_entries(struct AttachCtx *actx);
+}
