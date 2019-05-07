42bb68f211b2f43159773fea3810316b39e33844
Richard Russon <rich@flatcap.org>
2019-05-07 16:37:40 +0100

    fix: folder missing from status bar
    Fix a typo introduced with the Buffer changes.
    It lead to '%f' showing '(no mailbox)' all the time.

--- status.c
+++ status.c
@@ -149,7 +149,7 @@ static const char *status_format_str(char *buf, size_t buflen, size_t col, int c
       {
         mutt_str_strfcpy(tmp, m->desc, sizeof(tmp));
       }
-      else if (m && mutt_buffer_is_empty(m->pathbuf))
+      else if (m && !mutt_buffer_is_empty(m->pathbuf))
       {
         mutt_str_strfcpy(tmp, mutt_b2s(m->pathbuf), sizeof(tmp));
         mutt_pretty_mailbox(tmp, sizeof(tmp));
