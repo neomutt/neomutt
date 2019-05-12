neomutt/neomutt

commit e4133e5cd21c7391d2dabe0461f05dc40b7b3165

Convert struct Buffer->path to a struct Buffer
Rename to struct Buffer->pathbuf to make it clear the field is a struct Buffer, and
to make sure to catch and review all usages.

There are still uses of pathbuf that are truncating and need to be
fixed, for example in browser.c and buffy.c.

Fix up sidebar usage in one place, that was pointing inside the
struct Mailbox->path with a char *.  At the same time, change their "short
folder" computation to also use a struct Buffer.

Co-authored-by: Richard Russon <rich@flatcap.org>

index.c
@@ -627,7 +627,7 @@ static int main_change_folder(struct Menu *menu, int op, struct Mailbox *m,
     if (m)
     {
       magic = m->magic;
-      mutt_str_strfcpy(buf, m->path, buflen);
+      mutt_str_strfcpy(buf, mutt_b2s(m->pathbuf), buflen);
     }
     else
     {
@@ -638,7 +638,7 @@ static int main_change_folder(struct Menu *menu, int op, struct Mailbox *m,
   }
 
   /* keepalive failure in mutt_enter_fname may kill connection. #3028 */
-  if (Context && (Context->mailbox->path[0] == '\0'))
+  if (Context && (mutt_buffer_is_empty(Context->mailbox->pathbuf)))
     ctx_free(&Context);
 
   if (Context)
@@ -652,7 +652,7 @@ static int main_change_folder(struct Menu *menu, int op, struct Mailbox *m,
       new_last_folder = mutt_str_strdup(Context->mailbox->realpath);
     else
 #endif
-      new_last_folder = mutt_str_strdup(Context->mailbox->path);
+      new_last_folder = mutt_str_strdup(mutt_b2s(Context->mailbox->pathbuf));
     *oldcount = Context ? Context->mailbox->msg_count : 0;
 
     int check = mx_mbox_close(&Context);
@@ -1103,7 +1103,7 @@ int mutt_index_menu(void)
       check = mx_mbox_check(Context->mailbox, &index_hint);
       if (check < 0)
       {
-        if (!Context->mailbox || (Context->mailbox->path[0] == '\0'))
+        if (!Context->mailbox || (mutt_buffer_is_empty(Context->mailbox->pathbuf)))
         {
           /* fatal error occurred */
           ctx_free(&Context);
@@ -1922,7 +1922,7 @@ int mutt_index_menu(void)
         }
 
         /* check for a fatal error, or all messages deleted */
-        if (Context->mailbox->path[0] == '\0')
+        if (mutt_buffer_is_empty(Context->mailbox->pathbuf))
           ctx_free(&Context);
 
         /* if we were in the pager, redisplay the message */
@@ -2220,9 +2220,9 @@ int mutt_index_menu(void)
 
         buf[0] = '\0';
         if ((op == OP_MAIN_NEXT_UNREAD_MAILBOX) && Context &&
-            (Context->mailbox->path[0] != '\0'))
+            mutt_buffer_is_empty(Context->mailbox->pathbuf))
         {
-          mutt_str_strfcpy(buf, Context->mailbox->path, sizeof(buf));
+          mutt_str_strfcpy(buf, mutt_b2s(Context->mailbox->pathbuf), sizeof(buf));
           mutt_pretty_mailbox(buf, sizeof(buf));
           mutt_mailbox(Context ? Context->mailbox : NULL, buf, sizeof(buf));
           if (buf[0] == '\0')
@@ -2237,17 +2237,18 @@ int mutt_index_menu(void)
           m = mutt_sb_get_highlight();
           if (!m)
             break;
-          mutt_str_strfcpy(buf, m->path, sizeof(buf));
+          mutt_str_strfcpy(buf, mutt_b2s(m->pathbuf), sizeof(buf));
 
           /* Mark the selected dir for the neomutt browser */
-          mutt_browser_select_dir(m->path);
+          mutt_browser_select_dir(mutt_b2s(m->pathbuf));
         }
 #endif
         else
         {
-          if (C_ChangeFolderNext && Context && (Context->mailbox->path[0] != '\0'))
+          if (C_ChangeFolderNext && Context &&
+              mutt_buffer_is_empty(Context->mailbox->pathbuf))
           {
-            mutt_str_strfcpy(buf, Context->mailbox->path, sizeof(buf));
+            mutt_str_strfcpy(buf, mutt_b2s(Context->mailbox->pathbuf), sizeof(buf));
             mutt_pretty_mailbox(buf, sizeof(buf));
           }
 #ifdef USE_NNTP

browser.c
@@ -781,7 +781,7 @@ static int examine_directory(struct Menu *menu, struct BrowserState *state,
       struct MailboxNode *np = NULL;
       STAILQ_FOREACH(np, &AllMailboxes, entries)
       {
-        if (mutt_str_strcmp(mutt_b2s(buf), np->mailbox->path) != 0)
+        if (mutt_str_strcmp(mutt_b2s(buf), mutt_b2s(np->mailbox->pathbuf)) != 0)
           break;
       }
 
@@ -850,7 +850,7 @@ static int examine_mailboxes(struct Menu *menu, struct BrowserState *state)
       }
 
       char buf[PATH_MAX];
-      mutt_str_strfcpy(buf, np->mailbox->path, sizeof(buf));
+      mutt_str_strfcpy(buf, mutt_b2s(np->mailbox->pathbuf), sizeof(buf));
       if (C_BrowserAbbreviateMailboxes)
         mutt_pretty_mailbox(buf, sizeof(buf));
 
@@ -862,14 +862,14 @@ static int examine_mailboxes(struct Menu *menu, struct BrowserState *state)
           continue;
         case MUTT_NOTMUCH:
         case MUTT_NNTP:
-          add_folder(menu, state, np->mailbox->path, np->mailbox->desc, NULL,
-                     np->mailbox, NULL);
+          add_folder(menu, state, mutt_b2s(np->mailbox->pathbuf),
+                     np->mailbox->desc, NULL, np->mailbox, NULL);
           continue;
         default: /* Continue */
           break;
       }
 
-      if (lstat(np->mailbox->path, &s) == -1)
+      if (lstat(mutt_b2s(np->mailbox->pathbuf), &s) == -1)
         continue;
 
       if ((!S_ISREG(s.st_mode)) && (!S_ISDIR(s.st_mode)) && (!S_ISLNK(s.st_mode)))
@@ -879,10 +879,10 @@ static int examine_mailboxes(struct Menu *menu, struct BrowserState *state)
       {
         struct stat st2;
 
-        mutt_buffer_printf(md, "%s/new", np->mailbox->path);
+        mutt_buffer_printf(md, "%s/new", mutt_b2s(np->mailbox->pathbuf));
         if (stat(mutt_b2s(md), &s) < 0)
           s.st_mtime = 0;
-        mutt_buffer_printf(md, "%s/cur", np->mailbox->path);
+        mutt_buffer_printf(md, "%s/cur", mutt_b2s(np->mailbox->pathbuf));
         if (stat(mutt_b2s(md), &st2) < 0)
           st2.st_mtime = 0;
         if (st2.st_mtime > s.st_mtime)
@@ -1612,7 +1612,8 @@ void mutt_buffer_select_file(struct Buffer *file, SelectFileFlags flags,
           // TODO(sileht): It could be better to select INBOX instead. But I
           // don't want to manipulate Context/AllMailboxes/mailbox->account here for now.
           // Let's just protect neomutt against crash for now. #1417
-          if (mutt_str_strcmp(Context->mailbox->path, state.entry[nentry].name) == 0)
+          if (mutt_str_strcmp(mutt_b2s(Context->mailbox->pathbuf),
+                              state.entry[nentry].name) == 0)
           {
             mutt_error(_("Can't delete currently selected mailbox"));
             break;

compose.c
@@ -1414,7 +1414,7 @@ int mutt_compose_menu(struct Email *msg, char *fcc, size_t fcclen, struct Email 
           if ((op == OP_COMPOSE_ATTACH_MESSAGE) ^ (Context->mailbox->magic == MUTT_NNTP))
 #endif
           {
-            mutt_str_strfcpy(buf, Context->mailbox->path, sizeof(buf));
+            mutt_str_strfcpy(buf, mutt_b2s(Context->mailbox->pathbuf), sizeof(buf));
             mutt_pretty_mailbox(buf, sizeof(buf));
           }
 
@@ -1910,7 +1910,7 @@ int mutt_compose_menu(struct Email *msg, char *fcc, size_t fcclen, struct Email 
         buf[0] = '\0';
         if (Context)
         {
-          mutt_str_strfcpy(buf, Context->mailbox->path, sizeof(buf));
+          mutt_str_strfcpy(buf, mutt_b2s(Context->mailbox->pathbuf), sizeof(buf));
           mutt_pretty_mailbox(buf, sizeof(buf));
         }
         if (actx->idxlen)

compress.c
@@ -140,13 +140,13 @@ static int setup_paths(struct Mailbox *m)
   char tmp[PATH_MAX];
 
   /* Setup the right paths */
-  mutt_str_replace(&m->realpath, m->path);
+  mutt_str_replace(&m->realpath, mutt_b2s(m->pathbuf));
 
   /* We will uncompress to /tmp */
   mutt_mktemp(tmp, sizeof(tmp));
-  mutt_str_strfcpy(m->path, tmp, sizeof(m->path));
+  mutt_buffer_strcpy(m->pathbuf, tmp);
 
-  FILE *fp = mutt_file_fopen(m->path, "w");
+  FILE *fp = mutt_file_fopen(mutt_b2s(m->pathbuf), "w");
   if (!fp)
     return -1;
 
@@ -215,12 +215,12 @@ static struct CompressInfo *set_compress_info(struct Mailbox *m)
     return m->compress_info;
 
   /* Open is compulsory */
-  const char *o = find_hook(MUTT_OPEN_HOOK, m->path);
+  const char *o = find_hook(MUTT_OPEN_HOOK, mutt_b2s(m->pathbuf));
   if (!o)
     return NULL;
 
-  const char *c = find_hook(MUTT_CLOSE_HOOK, m->path);
-  const char *a = find_hook(MUTT_APPEND_HOOK, m->path);
+  const char *c = find_hook(MUTT_CLOSE_HOOK, mutt_b2s(m->pathbuf));
+  const char *a = find_hook(MUTT_APPEND_HOOK, mutt_b2s(m->pathbuf));
 
   struct CompressInfo *ci = mutt_mem_calloc(1, sizeof(struct CompressInfo));
   m->compress_info = ci;
@@ -278,7 +278,7 @@ static const char *compress_format_str(char *buf, size_t buflen, size_t col, int
       break;
     case 't':
       /* Plaintext, temporary file */
-      stuffing = mutt_path_escape(m->path);
+      stuffing = mutt_path_escape(mutt_b2s(m->pathbuf));
       break;
   }
   snprintf(buf, buflen, "%s", NONULL(stuffing));
@@ -377,7 +377,8 @@ bool mutt_comp_can_append(struct Mailbox *m)
   if (ci->cmd_append || ci->cmd_close)
     return true;
 
-  mutt_error(_("Can't append without an append-hook or close-hook : %s"), m->path);
+  mutt_error(_("Can't append without an append-hook or close-hook : %s"),
+             mutt_b2s(m->pathbuf));
   return false;
 }
 
@@ -464,7 +465,7 @@ static int comp_mbox_open(struct Mailbox *m)
     return -1;
 
   /* If there's no close-hook, or the file isn't writable */
-  if (!ci->cmd_close || (access(m->path, W_OK) != 0))
+  if (!ci->cmd_close || (access(mutt_b2s(m->pathbuf), W_OK) != 0))
     m->readonly = true;
 
   if (setup_paths(m) != 0)
@@ -483,7 +484,7 @@ static int comp_mbox_open(struct Mailbox *m)
 
   unlock_realpath(m);
 
-  m->magic = mx_path_probe(m->path, NULL);
+  m->magic = mx_path_probe(mutt_b2s(m->pathbuf), NULL);
   if (m->magic == MUTT_UNKNOWN)
   {
     mutt_error(_("Can't identify the contents of the compressed file"));
@@ -502,7 +503,7 @@ static int comp_mbox_open(struct Mailbox *m)
 
 cmo_fail:
   /* remove the partial uncompressed file */
-  remove(m->path);
+  remove(mutt_b2s(m->pathbuf));
   free_compress_info(m);
   return -1;
 }
@@ -528,7 +529,8 @@ static int comp_mbox_open_append(struct Mailbox *m, OpenMailboxFlags flags)
   /* To append we need an append-hook or a close-hook */
   if (!ci->cmd_append && !ci->cmd_close)
   {
-    mutt_error(_("Can't append without an append-hook or close-hook : %s"), m->path);
+    mutt_error(_("Can't append without an append-hook or close-hook : %s"),
+               mutt_b2s(m->pathbuf));
     goto cmoa_fail1;
   }
 
@@ -552,7 +554,7 @@ static int comp_mbox_open_append(struct Mailbox *m, OpenMailboxFlags flags)
       mutt_error(_("Compress command failed: %s"), ci->cmd_open);
       goto cmoa_fail2;
     }
-    m->magic = mx_path_probe(m->path, NULL);
+    m->magic = mx_path_probe(mutt_b2s(m->pathbuf), NULL);
   }
   else
     m->magic = C_MboxType;
@@ -578,7 +580,7 @@ static int comp_mbox_open_append(struct Mailbox *m, OpenMailboxFlags flags)
 
 cmoa_fail2:
   /* remove the partial uncompressed file */
-  remove(m->path);
+  remove(mutt_b2s(m->pathbuf));
 cmoa_fail1:
   /* Free the compress_info to prevent close from trying to recompress */
   free_compress_info(m);
@@ -727,23 +729,23 @@ static int comp_mbox_close(struct Mailbox *m)
     if (rc == 0)
     {
       mutt_any_key_to_continue(NULL);
-      mutt_error(_("Error. Preserving temporary file: %s"), m->path);
+      mutt_error(_("Error. Preserving temporary file: %s"), mutt_b2s(m->pathbuf));
     }
     else
-      remove(m->path);
+      remove(mutt_b2s(m->pathbuf));
 
     unlock_realpath(m);
   }
   else
   {
     /* If the file was removed, remove the compressed folder too */
-    if ((access(m->path, F_OK) != 0) && !C_SaveEmpty)
+    if ((access(mutt_b2s(m->pathbuf), F_OK) != 0) && !C_SaveEmpty)
     {
       remove(m->realpath);
     }
     else
     {
-      remove(m->path);
+      remove(mutt_b2s(m->pathbuf));
     }
   }
 

hdrline.c
@@ -631,11 +631,11 @@ static const char *index_format_str(char *buf, size_t buflen, size_t col, int co
     case 'b':
       if (m)
       {
-        p = strrchr(m->path, '/');
+        p = strrchr(mutt_b2s(m->pathbuf), '/');
         if (p)
           mutt_str_strfcpy(buf, p + 1, buflen);
         else
-          mutt_str_strfcpy(buf, m->path, buflen);
+          mutt_str_strfcpy(buf, mutt_b2s(m->pathbuf), buflen);
       }
       else
         mutt_str_strfcpy(buf, "(null)", buflen);

imap/browse.c
@@ -118,7 +118,7 @@ static void add_folder(char delim, char *folder, bool noselect, bool noinferiors
   struct MailboxNode *np = NULL;
   STAILQ_FOREACH(np, &AllMailboxes, entries)
   {
-    if (mutt_str_strcmp(tmp, np->mailbox->path) == 0)
+    if (mutt_str_strcmp(tmp, mutt_b2s(np->mailbox->pathbuf)) == 0)
       break;
   }
 

imap/imap.c
@@ -520,16 +520,16 @@ static int complete_hosts(char *buf, size_t buflen)
   struct MailboxNode *np = NULL;
   STAILQ_FOREACH(np, &AllMailboxes, entries)
   {
-    if (!mutt_str_startswith(np->mailbox->path, buf, CASE_MATCH))
+    if (!mutt_str_startswith(mutt_b2s(np->mailbox->pathbuf), buf, CASE_MATCH))
       continue;
 
     if (rc)
     {
-      mutt_str_strfcpy(buf, np->mailbox->path, buflen);
+      mutt_str_strfcpy(buf, mutt_b2s(np->mailbox->pathbuf), buflen);
       rc = 0;
     }
     else
-      longest_common_prefix(buf, np->mailbox->path, matchlen, buflen);
+      longest_common_prefix(buf, mutt_b2s(np->mailbox->pathbuf), matchlen, buflen);
   }
 
 #if 0
@@ -1541,7 +1541,7 @@ int imap_fast_trash(struct Mailbox *m, char *dest)
   /* check that the save-to folder is in the same account */
   if (!mutt_account_match(&(adata->conn->account), &(dest_adata->conn->account)))
   {
-    mutt_debug(LL_DEBUG3, "%s not same server as %s\n", dest, m->path);
+    mutt_debug(LL_DEBUG3, "%s not same server as %s\n", dest, mutt_b2s(m->pathbuf));
     goto out;
   }
 
@@ -1856,7 +1856,7 @@ int imap_ac_add(struct Account *a, struct Mailbox *m)
     struct ConnAccount conn_account;
     char mailbox[PATH_MAX];
 
-    if (imap_parse_path(m->path, &conn_account, mailbox, sizeof(mailbox)) < 0)
+    if (imap_parse_path(mutt_b2s(m->pathbuf), &conn_account, mailbox, sizeof(mailbox)) < 0)
       return -1;
 
     adata = imap_adata_new();
@@ -1876,14 +1876,14 @@ int imap_ac_add(struct Account *a, struct Mailbox *m)
 
   if (!m->mdata)
   {
-    struct Url *url = url_parse(m->path);
+    struct Url *url = url_parse(mutt_b2s(m->pathbuf));
     struct ImapMboxData *mdata = imap_mdata_new(adata, url->path);
 
     /* fixup path and realpath, mainly to replace / by /INBOX */
     char buf[1024];
     imap_qualify_path(buf, sizeof(buf), &adata->conn_account, mdata->name);
-    mutt_str_strfcpy(m->path, buf, sizeof(m->path));
-    mutt_str_replace(&m->realpath, m->path);
+    mutt_buffer_strcpy(m->pathbuf, buf);
+    mutt_str_replace(&m->realpath, mutt_b2s(m->pathbuf));
 
     m->mdata = mdata;
     m->free_mdata = imap_mdata_free;

imap/message.c
@@ -1563,7 +1563,7 @@ int imap_copy_messages(struct Mailbox *m, struct EmailList *el, char *dest, bool
   /* check that the save-to folder is in the same account */
   if (!mutt_account_match(&adata->conn->account, &conn_account))
   {
-    mutt_debug(LL_DEBUG3, "%s not same server as %s\n", dest, m->path);
+    mutt_debug(LL_DEBUG3, "%s not same server as %s\n", dest, mutt_b2s(m->pathbuf));
     return 1;
   }
 

commit 692b10450c4db1de042a6432155f15cd865fbdc6

Fix next-unread-mailbox
e4133e5cd2 have introduced a small regression.

next-unread-mailbox does not automatically open the mailbox.

This change fixes it.

index.c
@@ -2223,7 +2223,7 @@ int mutt_index_menu(void)
           cp = _("Open mailbox");
 
         if ((op == OP_MAIN_NEXT_UNREAD_MAILBOX) && Context &&
-            mutt_buffer_is_empty(Context->mailbox->pathbuf))
+            !mutt_buffer_is_empty(Context->mailbox->pathbuf))
         {
           mutt_buffer_strcpy(folderbuf, mutt_b2s(Context->mailbox->pathbuf));
           mutt_buffer_pretty_mailbox(folderbuf);