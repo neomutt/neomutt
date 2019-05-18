24071929875b21f8b6c4f715b8ee345eade161ac
Pietro Cerutti <gahr@gahr.ch>
2019-05-18 16:51:50 +0000

    Add test for mutt_addrlist_append and mutt_addrlist_prepend

--- test/address/mutt_addrlist_prepend.c
+++ test/address/mutt_addrlist_prepend.c
@@ -22,6 +22,7 @@
 
 #define TEST_NO_MAIN
 #include "acutest.h"
+#include "common.h"
 #include "config.h"
 #include "mutt/mutt.h"
 #include "address/lib.h"
@@ -40,5 +41,17 @@ void test_mutt_addrlist_prepend(void)
     struct AddressList al = { 0 };
     mutt_addrlist_prepend(&al, NULL);
     TEST_CHECK_(1, "mutt_addrlist_prepend(&al, NULL)");
+  }
+
+  {
+    struct Address a1 = { .mailbox = "test@example.com" };
+    struct Address a2 = { .mailbox = "john@doe.org" };
+    struct AddressList al = TAILQ_HEAD_INITIALIZER(al);
+    mutt_addrlist_prepend(&al, &a1);
+    mutt_addrlist_prepend(&al, &a2);
+    struct Address *a = TAILQ_FIRST(&al);
+    TEST_CHECK_STR_EQ("john@doe.org", a->mailbox);
+    a = TAILQ_NEXT(a, entries);
+    TEST_CHECK_STR_EQ("test@example.com", a->mailbox);
   }
 }
