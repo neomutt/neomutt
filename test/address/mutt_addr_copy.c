e930eebbc52ef6a02a8551c592cdd6e198fd3057
Pietro Cerutti <gahr@gahr.ch>
2019-05-19 11:20:25 +0000

    Kill duplicate copyright line

--- test/address/mutt_addr_copy.c
+++ test/address/mutt_addr_copy.c
@@ -5,7 +5,6 @@
  * @authors
  * Copyright (C) 2019 Richard Russon <rich@flatcap.org>
  * Copyright (C) 2019 Pietro Cerutti <gahr@gahr.ch>
- * Copyright (C) 2019 Pietro Cerutti <gahr@gahr.ch>
  *
  * @copyright
  * This program is free software: you can redistribute it and/or modify it under
