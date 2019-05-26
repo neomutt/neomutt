3b22aa4b8100a3ace9682bd2255804e6bb2a7bdc
Richard Russon <rich@flatcap.org>
2019-05-26 16:02:23 +0100

    test: improve test_mutt_str_lws_rlen()

--- test/string/mutt_str_lws_rlen.c
+++ test/string/mutt_str_lws_rlen.c
@@ -25,11 +25,45 @@
 #include "config.h"
 #include "mutt/mutt.h"
 
+struct LwsLenTest
+{
+  const char *str;
+  size_t len;
+  size_t result;
+};
+
 void test_mutt_str_lws_rlen(void)
 {
   // size_t mutt_str_lws_rlen(const char *s, size_t n);
 
+  // clang-format off
+  struct LwsLenTest lws_tests[] =
   {
-    TEST_CHECK(mutt_str_lws_rlen(NULL, 10) == 0);
+    { NULL,           10, 0 },
+    { "",              1, 0 },
+    { "apple",         5, 0 },
+    { "apple",         0, 0 },
+
+    { "apple ",        6, 1 },
+    { "apple\t",       6, 1 },
+    { "apple\n",       6, 0 },
+    { "apple\r",       6, 0 },
+
+    { "apple \t\n\r", 10, 0 },
+    { " \t\n\r",       5, 0 },
+
+    { "apple    ",     8, 3 },
+    { "apple    ",     7, 2 },
+  };
+  // clang-format on
+
+  {
+    for (size_t i = 0; i < mutt_array_size(lws_tests); i++)
+    {
+      struct LwsLenTest *t = &lws_tests[i];
+      TEST_CASE_("%ld, '%s'", i, NONULL(t->str));
+      size_t result = mutt_str_lws_rlen(t->str, t->len);
+      TEST_CHECK(result == t->result);
+    }
   }
 }
