9033ab8f074c7b3142f96778ee5d85eee197e7ab
Richard Russon <rich@flatcap.org>
2019-09-21 16:22:34 +0100

    fix checks in progress_choose_increment()

--- progress.c
+++ progress.c
@@ -116,7 +116,7 @@ static void message_bar(int percent, const char *fmt, ...)
 static size_t progress_choose_increment(enum ProgressType type)
 {
   static short *incs[] = { &C_ReadInc, &C_WriteInc, &C_NetInc };
-  return (type < 0 || type >= mutt_array_size(incs)) ? 0 : *incs[type];
+  return (type >= mutt_array_size(incs)) ? 0 : *incs[type];
 }
 
 /**
