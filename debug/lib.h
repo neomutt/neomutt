3fbc7a48953bb63a95f615406fcbde155cfc2ad3
Richard Russon <rich@flatcap.org>
2019-12-21 16:48:44 +0000

    build: add libdebug

---
+++ debug/lib.h
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * Convenience wrapper for the debug headers
+ *
+ * @authors
+ * Copyright (C) 2019 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MUTT_DEBUG_LIB_H
+#define MUTT_DEBUG_LIB_H
+
+
+#endif /* MUTT_DEBUG_LIB_H */
