3b3ab978a378754ec5b4b1fbfb024c306185c310
Richard Russon <rich@flatcap.org>
2020-02-10 13:15:18 +0000

    conn: add range check for oauth token

--- conn/connaccount.c
+++ conn/connaccount.c
@@ -27,6 +27,7 @@
  */
 
 #include "config.h"
+#include <assert.h>
 #include <string.h>
 #include "mutt/lib.h"
 #include "gui/lib.h"
@@ -185,15 +186,23 @@ char *mutt_account_getoauthbearer(struct ConnAccount *cac)
     return NULL;
   }
 
-  char *oauthbearer = NULL;
-  int oalen = mutt_str_asprintf(&oauthbearer, "n,a=%s,\001host=%s\001port=%d\001auth=Bearer %s\001\001",
-                                cac->login, cac->host, cac->port, token);
+  if (token_size > 512)
+  {
+    mutt_error(_("OAUTH token is too big: %ld"), token_size);
+    FREE(&token);
+    return NULL;
+  }
+
+  char oauthbearer[1024];
+  int oalen = snprintf(oauthbearer, sizeof(oauthbearer), "n,a=%s,\001host=%s\001port=%d\001auth=Bearer %s\001\001",
+                       cac->login, cac->host, cac->port, token);
+  FREE(&token);
 
   size_t encoded_len = oalen * 4 / 3 + 10;
+  assert(encoded_len < 1400); // Assure LGTM that we won't overflow
+
   char *encoded_token = mutt_mem_malloc(encoded_len);
   mutt_b64_encode(oauthbearer, oalen, encoded_token, encoded_len);
 
-  FREE(&token);
-  FREE(&oauthbearer);
   return encoded_token;
 }
