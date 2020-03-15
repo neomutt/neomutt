47709f321569d410d2c1edb51c3c72c30d21a7b3
Richard Russon <rich@flatcap.org>
2020-03-15 01:06:24 +0000

    tidy serialize
    - reorder params of serial_dump_char_size() for consistency
    - tidy headers

--- hcache/serialize.h
+++ hcache/serialize.h
@@ -29,7 +29,6 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include <sys/types.h>
-#include "lib.h"
 
 struct AddressList;
 struct Body;
@@ -38,27 +37,27 @@ struct Envelope;
 struct ListHead;
 struct ParameterList;
 
-unsigned char *serial_dump_address(struct AddressList *al, unsigned char *d, int *off, bool convert);
-unsigned char *serial_dump_body(struct Body *c, unsigned char *d, int *off, bool convert);
-unsigned char *serial_dump_buffer(struct Buffer *buf, unsigned char *d, int *off, bool convert);
-unsigned char *serial_dump_char(char *c, unsigned char *d, int *off, bool convert);
-unsigned char *serial_dump_char_size(char *c, unsigned char *d, int *off, ssize_t size, bool convert);
-unsigned char *serial_dump_envelope(struct Envelope *e, unsigned char *d, int *off, bool convert);
-unsigned char *serial_dump_int(unsigned int i, unsigned char *d, int *off);
-unsigned char *serial_dump_size_t(size_t s, unsigned char *d, int *off);
+unsigned char *serial_dump_address  (struct AddressList *al,   unsigned char *d, int *off, bool convert);
+unsigned char *serial_dump_body     (struct Body *c,           unsigned char *d, int *off, bool convert);
+unsigned char *serial_dump_buffer   (struct Buffer *buf,       unsigned char *d, int *off, bool convert);
+unsigned char *serial_dump_char     (char *c,                  unsigned char *d, int *off, bool convert);
+unsigned char *serial_dump_char_size(char *c, ssize_t size,    unsigned char *d, int *off, bool convert);
+unsigned char *serial_dump_envelope (struct Envelope *e,       unsigned char *d, int *off, bool convert);
+unsigned char *serial_dump_int      (unsigned int i,           unsigned char *d, int *off);
+unsigned char *serial_dump_size_t   (size_t s,                 unsigned char *d, int *off);
 unsigned char *serial_dump_parameter(struct ParameterList *pl, unsigned char *d, int *off, bool convert);
-unsigned char *serial_dump_stailq(struct ListHead *l, unsigned char *d, int *off, bool convert);
+unsigned char *serial_dump_stailq   (struct ListHead *l,       unsigned char *d, int *off, bool convert);
 
-void           serial_restore_address(struct AddressList *al, const unsigned char *d, int *off, bool convert);
-void           serial_restore_body(struct Body *c, const unsigned char *d, int *off, bool convert);
-void           serial_restore_buffer(struct Buffer *buf, const unsigned char *d, int *off, bool convert);
-void           serial_restore_char(char **c, const unsigned char *d, int *off, bool convert);
-void           serial_restore_envelope(struct Envelope *e, const unsigned char *d, int *off, bool convert);
-void           serial_restore_int(unsigned int *i, const unsigned char *d, int *off);
-void           serial_restore_size_t(size_t *s, const unsigned char *d, int *off);
-void           serial_restore_parameter(struct ParameterList *pl, const unsigned char *d, int *off, bool convert);
-void           serial_restore_stailq(struct ListHead *l, const unsigned char *d, int *off, bool convert);
+void serial_restore_address  (struct AddressList *al,   const unsigned char *d, int *off, bool convert);
+void serial_restore_body     (struct Body *c,           const unsigned char *d, int *off, bool convert);
+void serial_restore_buffer   (struct Buffer *buf,       const unsigned char *d, int *off, bool convert);
+void serial_restore_char     (char **c,                 const unsigned char *d, int *off, bool convert);
+void serial_restore_envelope (struct Envelope *e,       const unsigned char *d, int *off, bool convert);
+void serial_restore_int      (unsigned int *i,          const unsigned char *d, int *off);
+void serial_restore_size_t   (size_t *s,                const unsigned char *d, int *off);
+void serial_restore_parameter(struct ParameterList *pl, const unsigned char *d, int *off, bool convert);
+void serial_restore_stailq   (struct ListHead *l,       const unsigned char *d, int *off, bool convert);
 
-void           lazy_realloc(void *ptr, size_t size);
+void lazy_realloc(void *ptr, size_t size);
 
 #endif /* MUTT_HCACHE_SERIALIZE_H */
