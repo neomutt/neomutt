d786f568c0bada248cfa6edf321578cd2f54ea35
Pietro Cerutti <gahr@gahr.ch>
2020-03-17 12:40:49 +0000

    Resolve symlinks only when necessary
    This partially undoes ad00c1bf, which added symlink resolution to all
    path expansions. It turnes out that broke attaching files via symlinks
    because symlink resolution happens too early causing the attachment name
    to reflect the actual target instead of the symlink (see #2168).
    
    This commit adds a new API mutt_file_resolve_symlink and calls it where
    needed. Right now, the only place I am aware of is when selecting a
    target for saving a message.
    
    Fixes #2168

--- mutt/file.h
+++ mutt/file.h
@@ -121,6 +121,7 @@ void        mutt_file_touch_atime(int fd);
 void        mutt_file_unlink(const char *s);
 void        mutt_file_unlink_empty(const char *path);
 int         mutt_file_unlock(int fd);
+void        mutt_file_resolve_symlink(struct Buffer *buf);
 
 void        mutt_buffer_quote_filename(struct Buffer *buf, const char *filename, bool add_outer);
 void        mutt_buffer_file_expand_fmt_quote(struct Buffer *dest, const char *fmt, const char *src);
