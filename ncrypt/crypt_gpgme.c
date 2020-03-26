c2e11f9ce353870d3b615f3524f32ad3c9811b5f
Richard Russon <rich@flatcap.org>
2020-03-26 02:03:59 +0000

    doxy: fix @page location

--- ncrypt/crypt_gpgme.c
+++ ncrypt/crypt_gpgme.c
@@ -24,8 +24,6 @@
  *
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * crypt_gpgme.c - GPGME based crypto operations
  */
 
 /**
