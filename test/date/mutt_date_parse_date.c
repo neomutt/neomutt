cde85c86bc24e7a7bc89aa760ec8b75bb74491c2
Pietro Cerutti <gahr@gahr.ch>
2020-04-15 09:37:56 +0000

    Handle more non-conformant dates
    - No space after day of week: Wed,17 Jul 2002 15:41:00 +0800
    - Invalid day of week: Tuesday, 12 Feb 2002 13:08:10 -0500
    - Invalid or missing timezone: Tue, 19 Aug 2003 12:47:46 --0400
    - Single-digit time components: Fri, 5 Sep 2003 13:2:1 -0800

--- test/date/mutt_date_parse_date.c
+++ test/date/mutt_date_parse_date.c
@@ -48,6 +48,51 @@ void test_mutt_date_parse_date(void)
   // clang-format off
   struct ParseDateTest parse_tests[] = {
      // [ weekday , ] day-of-month month year hour:minute:second [ timezone ]
+
+     /* This should be India Standard Time, but we use IST for Israel */
+     { "Thu, 16 Aug 2001 16:01:08 IST+530",          997970468  },
+
+     /* We only parse the MET part, we don't do -1 */
+     { "Tue, 19 Dec 2000 19:33:09 MET-1",            977250789  },
+
+     /* Skip unknown week days */
+     { "Sunday, 21 Apr 2002 21:51:04 +0000",         1019425864 },
+     { "Tuesday, 12 Feb 2002 13:08:10 -0500",        1013537290 },
+
+     /* A partial TZ, assume UTC */
+     { "Sun, 21 Apr 2002 21:51:04 +000",             1019425864 },
+
+     /* A comment followed by the TZ */
+     { "Wed, 13 Jun 2007 12:34:56 (CET) +0100",      1181734496 },
+
+     /* No TZ, just a comment, assume UTC */
+     { "Wed, 13 Jun 2007 12:34:56 (CET)",            1181738096 },
+
+     /* No TZ, assume UTC */
+     { "13 Jun 2007 12:34:56",                       1181738096 },
+
+     /* A TZ we do not understand, assume UTC */
+     { "Wed, 13 Jun 2007 12:34:56 D",                1181738096 },
+     { "Tue, 19 Aug 2003 12:47:46 --0400",           1061297266 },
+     { "Tue, 30 Mar 2004 14:33:08 %z (PST)",         1080657188 },
+
+     /* Bad seconds, we skip the part we don't get  */
+     { "Thu, 26 Mar 2020 17:16:22.020 +0000 (UTC)",  1585242982 },
+     { "Wed, 13 Jun 2007 12:34:-1 +0100",            1181738040 },
+
+     /* Single-digit time components */
+     { "Fri, 6 Sep 2002 11:46:3 +0800",              1031283963 },
+     { "Tue, 27 Apr 2004 16:2:37 +0800",             1083052957 },
+     { "Fri, 5 Sep 2003 13:2:1 -0800",               1062795721 },
+     { "Wed, 2 Oct 2002 2:7:1 +0800",                1033495621 },
+
+     /* Missing space after the comma */
+     { "Wed,17 Jul 2002 15:41:00 +0800",             1026891660 },
+
+     /* Bad month case */
+     { "Fri, 30 sep 2005 02:39:28 +0200",            -1         },
+
+     /* Good stuff */
      { "Wed, 13 Jun 2007 12:34:56 +0100",            1181734496 },
      { "Wed, 13 Jun 2007 12:34:56 MET DST",          1181734496 },
      { "Wed, 13 Jun 20 12:34:56 +0100",              1592048096 },
@@ -62,29 +107,14 @@ void test_mutt_date_parse_date(void)
      { "Tue, 07 Apr 2020 15:06:31 GMT",              1586271991 },
      { "Tue,  7 Apr 2020 15:06:31 GMT",              1586271991 },
      { "Tue, 7 Apr 2020 15:06:31 GMT",               1586271991 },
-     { "13 Jun 2007 12:34:56",                       1181738096 },
-
-     /* Single-digit day / hour */
-     { "Fri, 10 Jul 2009 3:22:08 -0400",             1247210528 }, 
-     { "Fri, 3 Apr 2009 6:26:03 -0500",              1238757963 },
-
-     /* A comment followed by the timezone  */
-     { "Wed, 13 Jun 2007 12:34:56 (CET) +0100",      1181734496 },
-
-     /* A timezone we do not understand, assume UTC */
-     { "Wed, 13 Jun 2007 12:34:56 D",                1181738096 },
-
-     /* Missing TZ, assume UTC */
-     { "Wed, 13 Jun 2007 12:34:56 (CET)",            1181738096 },
-     { "Fri, 10 Oct 2008 12:36:15",                  1223642175 },
-     { "Sat, 16 Apr 2011 18:17:54 (GMT)",            1302977874 },
-     { "Sat, 19 Mar 2011 22:46:08 (GMT)",            1300574768 },
-     { "Sun, 24 Apr 2011 01:14:15 (GMT)",            1303607655 },
-     { "Thu, 05 Jan 2012 02:11:51 (GMT)",            1325729511 },
-     { "Thu, 21 Apr 2011 18:54:56 (GMT)",            1303412096 },
-     { "Tue, 10 Jan 2012 14:31:05 (GMT)",            1326205865 },
 
      /* Stuff we don't parse */
+     { "Wed, ab Jun 2007 12:34:56 +0100",            -1         },
+     { "Wed, -2 Jun 2007 12:34:56 +0100",            -1         },
+     { "Wed, 32 Jun 2007 12:34:56 +0100",            -1         },
+     { "Wed, 13 Bob 2007 12:34:56 +0100",            -1         },
+     { "Wed, 13 Jun -1 12:34:56 +0100",              -1         },
+     { "Wed, 13 Jun 10000 12:34:56 +0100",           -1         },
      { "Wed, 13 Jun 2007 12.34 +0100",               -1         },
      { "Wed, 13 Jun 2007 -1:34:56 +0100",            -1         },
      { "Wed, 13 Jun 2007 24:34:56 +0100",            -1         },
@@ -92,31 +122,8 @@ void test_mutt_date_parse_date(void)
      { "Wed, 13 Jun 2007 12:-1:56 +0100",            -1         },
      { "Wed, 13 Jun 2007 12:60:56 +0100",            -1         },
      { "Wed, 13 Jun 2007 (bar baz) 12:60:56 +0100",  -1         },
-     { "Wed, 13 Jun 2007 12:34:-1 +0100",            -1         },
      { "Wed, 13 Jun 2007 12:34:61 +0100",            -1         },
-     { "Wed, ab Jun 2007 12:34:56 +0100",            -1         },
-     { "Wed, -2 Jun 2007 12:34:56 +0100",            -1         },
-     { "Wed, 32 Jun 2007 12:34:56 +0100",            -1         },
-     { "Wed, 13 Bob 2007 12:34:56 +0100",            -1         },
-     { "Wed, 13 Jun -1 12:34:56 +0100",              -1         },
-     { "Wed, 13 Jun 10000 12:34:56 +0100",           -1         },
      { "13 Jun 2007",                                -1         },
-     { "2007 12 02",                                 -1         },
-     { "Mon Nov 17 23:47:44 CST 2008",               -1         },
-     { "Mon Nov 17 23:48:26 CST 2008",               -1         },
-     { "Mon Nov 17 23:48:27 CST 2008",               -1         },
-     { "Mon, 12 Mar 2012 17.58.37 +0100",            -1         },
-     { "Mon, 2 Sep 2019 09:13:05 0000",              -1         },
-     { "Mon, Jan 11 09:56:23 2016 +0100",            -1         },
-     { "Mon,02 apr 2012 20:36:16 +0800",             -1         },
-     { "Sun, Jan 10 07:54:56 CET 2016",              -1         },
-     { "Thu Oct 16 22:10:38 EST 2008",               -1         },
-     { "Thu Oct 23 11:55:16 EST 2008",               -1         },
-     { "Thu, 10 Jun 2010 20:36:17 2000",             -1         },
-     { "Thu, 22 Mar 2012 00:21:36 +00",              -1         },
-     { "Tue, 17 Sep 2019 14:29:06 0000",             -1         },
-     { "Vie, 16 Mar 2007 11:52:31 +0100",            -1         },
-     { "Sun=2C 9 Jun 2013 20:18:43 +0100",           -1         },
   };
   // clang-format on
 
