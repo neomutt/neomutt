511030c018822972132e7cd17153e15e2257f1cf
Thomas Sanchez <thomas.sanchz@gmail.com>
2020-07-07 16:56:25 +0200

    minimal window debugger

--- preview/preview.c
+++ preview/preview.c
@@ -1,7 +1,7 @@
-#include "lib.h"
-
+#include <string.h>
 #include "core/neomutt.h"
 #include "gui/mutt_window.h"
+#include "lib.h"
 #include "mutt_globals.h"
 
 #include "private.h"
@@ -39,6 +39,22 @@ static struct MuttWindow *find_index_container(struct MuttWindow *root)
   return TAILQ_FIRST(&container->children); // Index container is the first
 }
 
+static void debug_window_tree(struct MuttWindow *node, int indent)
+{
+  char *s_indent = malloc(4 * indent + 1);
+  memset(s_indent, ' ', 4 * indent);
+  s_indent[4 * indent] = 0;
+  {
+    struct MuttWindow *win;
+    TAILQ_FOREACH(win, &node->children, entries)
+    {
+      mutt_message("%sWindow: %d\n", s_indent, win->type);
+      debug_window_tree(win, indent + 1);
+    }
+  }
+  free(s_indent);
+}
+
 static int preview_recalc(struct MuttWindow *win)
 {
   mutt_debug(LL_DEBUG1, "PREVIEW RECALC\n");
@@ -72,6 +88,7 @@ void preview_win_init(struct MuttWindow *dlg)
   }
 
   TAILQ_INSERT_AFTER(&index_container->children, index_panel, preview_window, entries);
+  debug_window_tree(dlg, 0);
 }
 
 void preview_win_shutdown(struct MuttWindow *dlg)
