663abf412e6f05f286b11614a0535194dc2b0931
Richard Russon <rich@flatcap.org>
2020-07-08 14:23:43 +0100

    hello

--- preview/draw.c
+++ preview/draw.c
@@ -1,5 +1,12 @@
+#include "config.h"
 #include "private.h"
+#include "gui/lib.h"
 
 void preview_draw(struct MuttWindow *win)
 {
+  if (!win || !win->state.visible)
+    return;
+
+  mutt_window_move(win, 0, 0);
+  mutt_window_addstr("Hi Daedric!");
 }
