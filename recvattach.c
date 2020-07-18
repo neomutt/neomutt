9996357bba7938e6a15c0c7e1aeab58d7698b03a
Richard Russon <rich@flatcap.org>
2020-07-18 15:39:52 +0100

    dialog: refactor attach

--- recvattach.c
+++ recvattach.c
@@ -1427,36 +1427,8 @@ void mutt_view_attachments(struct Email *e)
   if (!msg)
     return;
 
-  struct MuttWindow *dlg =
-      mutt_window_new(WT_DLG_ATTACH, MUTT_WIN_ORIENT_VERTICAL, MUTT_WIN_SIZE_MAXIMISE,
-                      MUTT_WIN_SIZE_UNLIMITED, MUTT_WIN_SIZE_UNLIMITED);
-
-  struct MuttWindow *index =
-      mutt_window_new(WT_INDEX, MUTT_WIN_ORIENT_VERTICAL, MUTT_WIN_SIZE_MAXIMISE,
-                      MUTT_WIN_SIZE_UNLIMITED, MUTT_WIN_SIZE_UNLIMITED);
-
-  struct MuttWindow *ibar =
-      mutt_window_new(WT_INDEX_BAR, MUTT_WIN_ORIENT_VERTICAL,
-                      MUTT_WIN_SIZE_FIXED, MUTT_WIN_SIZE_UNLIMITED, 1);
-
-  if (C_StatusOnTop)
-  {
-    mutt_window_add_child(dlg, ibar);
-    mutt_window_add_child(dlg, index);
-  }
-  else
-  {
-    mutt_window_add_child(dlg, index);
-    mutt_window_add_child(dlg, ibar);
-  }
-
-  dialog_push(dlg);
-
   struct Menu *menu = mutt_menu_new(MENU_ATTACH);
-
-  menu->pagelen = index->state.rows;
-  menu->win_index = index;
-  menu->win_ibar = ibar;
+  struct MuttWindow *dlg = dialog_create_simple_index(menu);
 
   menu->title = _("Attachments");
   menu->make_entry = attach_make_entry;
@@ -1742,8 +1714,7 @@ void mutt_view_attachments(struct Email *e)
 
         mutt_menu_pop_current(menu);
         mutt_menu_free(&menu);
-        dialog_pop();
-        mutt_window_free(&dlg);
+        dialog_destroy_simple_index(&dlg);
         return;
     }
 
