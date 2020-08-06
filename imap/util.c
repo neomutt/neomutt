e7780febaf3e7d34853c0890a04571f32d48bc27
Pietro Cerutti <gahr@gahr.ch>
2020-08-06 08:48:13 +0000

    Use our own string APIs
    Fixes #2569

--- imap/util.c
+++ imap/util.c
@@ -594,7 +594,7 @@ char *imap_hcache_get_uid_seqset(struct ImapMboxData *mdata)
   char *hc_seqset = mutt_hcache_fetch_raw(mdata->hcache, "/UIDSEQSET", 10, &dlen);
   if (hc_seqset)
   {
-    seqset = strndup(hc_seqset, dlen);
+    seqset = mutt_strn_dup(hc_seqset, dlen);
     mutt_hcache_free_raw(mdata->hcache, (void **) &hc_seqset);
   }
   mutt_debug(LL_DEBUG3, "Retrieved /UIDSEQSET %s\n", NONULL(seqset));
