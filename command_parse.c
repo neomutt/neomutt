94708ab238e8543850653efe347226b1c55fe24a
Matthew Hughes <matthewhughes934@gmail.com>
2020-09-06 19:14:07 +0100

    Add header notifications for my_hdr
    Add notifications for when this command is parsed and adds or changes a
    header.

--- command_parse.c
+++ command_parse.c
@@ -1011,15 +1011,18 @@ enum CommandResult parse_my_hdr(struct Buffer *buf, struct Buffer *s,
     return MUTT_CMD_WARNING;
   }
 
+  struct EventHeader event = { buf->data };
   struct ListNode *n = header_find(&UserHeader, buf->data);
 
   if (n)
   {
     header_update(n, buf->data);
+    notify_send(NeoMutt->notify, NT_HEADER, NT_HEADER_CHANGE, &event);
   }
   else
   {
     header_add(&UserHeader, buf->data);
+    notify_send(NeoMutt->notify, NT_HEADER, NT_HEADER_ADD, &event);
   }
 
   return MUTT_CMD_SUCCESS;
