neomutt/neomutt

commit 978cf231a9d45e530f157e93e79f3549dc0fbf7f

sidebar: strip sidebar update functions
Remove the hard-coded sidebar update functions.
The sidebar now relies on notifications / observer functions.

index.c
@@ -777,11 +777,9 @@ static void change_folder_mailbox(struct Menu *menu, struct Mailbox *m, int *old
   if (((C_Sort & SORT_MASK) == SORT_THREADS) && C_CollapseAll)
     collapse_all(Context, menu, 0);
 
-#ifdef USE_SIDEBAR
   struct MuttWindow *dlg = dialog_find(menu->win_index);
-  struct MuttWindow *win_sidebar = mutt_window_find(dlg, WT_SIDEBAR);
-  sb_set_open_mailbox(win_sidebar, Context ? Context->mailbox : NULL);
-#endif
+  struct EventMailbox em = { Context ? Context->mailbox : NULL };
+  notify_send(dlg->notify, NT_MAILBOX, NT_MAILBOX_SWITCH, &em);
 
   mutt_clear_error();
   mutt_mailbox_check(Context ? Context->mailbox : NULL, MUTT_MAILBOX_CHECK_FORCE); /* force the mailbox check after we have changed the folder */
@@ -1878,8 +1876,8 @@ int mutt_index_menu(struct MuttWindow *dlg)
 
           oldcount = (Context && Context->mailbox) ? Context->mailbox->msg_count : 0;
 
-          mutt_startup_shutdown_hook(MUTT_SHUTDOWN_HOOK);
-          notify_send(NeoMutt->notify, NT_GLOBAL, NT_GLOBAL_SHUTDOWN, NULL);
+          struct EventMailbox em = { Context ? Context->mailbox : NULL };
+          notify_send(dlg->notify, NT_MAILBOX, NT_MAILBOX_SWITCH, &em);
 
           if (!Context || ((check = mx_mbox_close(&Context)) == 0))
             done = true;

command_parse.c
@@ -58,9 +58,6 @@
 #include "myvar.h"
 #include "options.h"
 #include "version.h"
-#ifdef USE_SIDEBAR
-#include "sidebar/lib.h"
-#endif
 #ifdef ENABLE_NLS
 #include <libintl.h>
 #endif
@@ -908,25 +905,6 @@ bail:
 }
 
 /**
- * notify_sidebar - Notify the sidebar that a mailbox has changed
- * @param m Mailbox that has changed
- * @param e Event that has happened
- */
-static void notify_sidebar(struct Mailbox *m, enum SidebarNotification e)
-{
-  struct MuttWindow *dlg = NULL;
-  TAILQ_FOREACH(dlg, &AllDialogsWindow->children, entries)
-  {
-    struct MuttWindow *win_sidebar = mutt_window_find(dlg, WT_SIDEBAR);
-    if (win_sidebar)
-    {
-      sb_notify_mailbox(win_sidebar, m, e);
-      break;
-    }
-  }
-}
-
-/**
  * parse_mailboxes - Parse the 'mailboxes' command - Implements Command::parse()
  *
  * This is also used by 'virtual-mailboxes'.
@@ -989,12 +967,6 @@ enum CommandResult parse_mailboxes(struct Buffer *buf, struct Buffer *s,
           mutt_str_replace(&m_old->name, m->name);
         }
 
-#ifdef USE_SIDEBAR
-        if (show || rename)
-        {
-          notify_sidebar(m_old, show ? SBN_CREATED : SBN_RENAMED);
-        }
-#endif
         mailbox_free(&m);
         continue;
       }
@@ -1018,9 +990,6 @@ enum CommandResult parse_mailboxes(struct Buffer *buf, struct Buffer *s,
       neomutt_account_add(NeoMutt, a);
     }
 
-#ifdef USE_SIDEBAR
-    notify_sidebar(m, SBN_CREATED);
-#endif
 #ifdef USE_INOTIFY
     mutt_monitor_add(m);
 #endif
@@ -1977,9 +1946,6 @@ enum CommandResult parse_unmailboxes(struct Buffer *buf, struct Buffer *s,
         continue;
       }
 
-#ifdef USE_SIDEBAR
-      notify_sidebar(np->mailbox, SBN_DELETED);
-#endif
 #ifdef USE_INOTIFY
       mutt_monitor_remove(np->mailbox);
 #endif

main.c
@@ -78,9 +78,6 @@
 #ifdef ENABLE_NLS
 #include <libintl.h>
 #endif
-#ifdef USE_SIDEBAR
-#include "sidebar/lib.h"
-#endif
 #ifdef USE_IMAP
 #include "imap/lib.h"
 #endif
@@ -1226,10 +1223,10 @@ int main(int argc, char *argv[], char *envp[])
     {
       struct MuttWindow *dlg = index_pager_init();
       dialog_push(dlg);
-#ifdef USE_SIDEBAR
-      struct MuttWindow *win_sidebar = mutt_window_find(dlg, WT_SIDEBAR);
-      sb_set_open_mailbox(win_sidebar, Context ? Context->mailbox : NULL);
-#endif
+
+      struct EventMailbox em = { Context->mailbox };
+      notify_send(dlg->notify, NT_MAILBOX, NT_MAILBOX_SWITCH, &em);
+
       mutt_index_menu(dlg);
       dialog_pop();
       index_pager_shutdown(dlg);

sidebar/lib.h
@@ -46,21 +46,10 @@
 
 struct Buffer;
 struct ConfigSet;
-struct Mailbox;
 struct MuttWindow;
 
 void sb_init    (void);
 void sb_shutdown(void);
-
-/**
- * enum SidebarNotification - what happened to a mailbox
- */
-enum SidebarNotification
-{
-  SBN_CREATED, ///< A new mailbox was created
-  SBN_DELETED, ///< An existing mailbox is about to be deleted
-  SBN_RENAMED  ///< An existing mailbox was renamed
-};
 
 void            sb_change_mailbox(struct MuttWindow *win, int op);
 struct Mailbox *sb_get_highlight (struct MuttWindow *win);
@@ -68,8 +57,6 @@ struct Mailbox *sb_get_highlight (struct MuttWindow *win);
 enum CommandResult sb_parse_unwhitelist(struct Buffer *buf, struct Buffer *s, intptr_t data, struct Buffer *err);
 enum CommandResult sb_parse_whitelist  (struct Buffer *buf, struct Buffer *s, intptr_t data, struct Buffer *err);
 
-void sb_notify_mailbox  (struct MuttWindow *win, struct Mailbox *m, enum SidebarNotification sbn);
-void sb_set_open_mailbox(struct MuttWindow *win, struct Mailbox *m);
 bool config_init_sidebar(struct ConfigSet *cs);
 
 #endif /* MUTT_SIDEBAR_LIB_H */

sidebar/observer.c
@@ -146,7 +146,7 @@ static void sb_init_data(struct MuttWindow *win)
   STAILQ_FOREACH(np, &ml, entries)
   {
     if (!(np->mailbox->flags & MB_HIDDEN))
-      sb_notify_mailbox(win, np->mailbox, SBN_CREATED);
+      sb_notify_mailbox(wdata, np->mailbox);
   }
   neomutt_mailboxlist_clear(&ml);
 }
@@ -308,6 +308,27 @@ static int sb_mailbox_observer(struct NotifyCallback *nc)
     return -1;
 
   struct MuttWindow *win = nc->global_data;
+
+  if (nc->event_subtype == NT_MAILBOX_SWITCH)
+  {
+    struct SidebarWindowData *wdata = sb_wdata_get(win);
+    struct EventMailbox *em = nc->event_data;
+
+    wdata->opn_index = -1;
+
+    if (em->mailbox)
+    {
+      struct SbEntry **sbep = NULL;
+      ARRAY_FOREACH(sbep, &wdata->entries)
+      {
+        if (mutt_str_equal((*sbep)->mailbox->realpath, em->mailbox->realpath))
+        {
+          wdata->opn_index = wdata->hil_index = ARRAY_FOREACH_IDX;
+          break;
+        }
+      }
+    }
+  }
 
   mutt_debug(LL_NOTIFY, "mailbox\n");
   win->actions |= WA_RECALC;

sidebar/private.h
@@ -27,6 +27,8 @@
 #include "mutt/lib.h"
 #include "gui/lib.h"
 
+struct Mailbox;
+
 extern struct ListHead SidebarWhitelist;
 
 /**
@@ -86,6 +88,7 @@ extern short C_SidebarWidth;
 // sidebar.c
 bool select_next        (struct SidebarWindowData *wdata);
 WindowActionFlags calc_divider(struct SidebarWindowData *wdata, bool ascii, const char *div_str);
+void sb_notify_mailbox  (struct SidebarWindowData *wdata, struct Mailbox *m);
 int sb_recalc(struct MuttWindow *win);
 int sb_repaint(struct MuttWindow *win);
 

sidebar/sidebar.c
@@ -859,105 +859,32 @@ struct Mailbox *sb_get_highlight(struct MuttWindow *win)
 }
 
 /**
- * sb_set_open_mailbox - Set the 'open' Mailbox
- * @param win Sidebar Window
- * @param m   Mailbox
- *
- * Search through the list of mailboxes.
- * If a Mailbox has a matching path, set OpnMailbox to it.
- */
-void sb_set_open_mailbox(struct MuttWindow *win, struct Mailbox *m)
-{
-  struct SidebarWindowData *wdata = sb_wdata_get(win);
-
-  wdata->opn_index = -1;
-
-  if (!m)
-    return;
-
-  struct SbEntry **sbep = NULL;
-  ARRAY_FOREACH(sbep, &wdata->entries)
-  {
-    if (mutt_str_equal((*sbep)->mailbox->realpath, m->realpath))
-    {
-      wdata->opn_index = wdata->hil_index = ARRAY_FOREACH_IDX;
-      break;
-    }
-  }
-}
-
-/**
  * sb_notify_mailbox - The state of a Mailbox is about to change
- * @param win Sidebar Window
- * @param m   Folder
- * @param sbn What happened to the mailbox
- *
- * We receive a notification:
- * - After a new Mailbox has been created
- * - Before a Mailbox is deleted
- * - After an existing Mailbox is renamed
- *
- * Before a deletion, check that our pointers won't be invalidated.
+ * @param wdata Sidebar data
+ * @param m     Mailbox
  */
-void sb_notify_mailbox(struct MuttWindow *win, struct Mailbox *m, enum SidebarNotification sbn)
+void sb_notify_mailbox(struct SidebarWindowData *wdata, struct Mailbox *m)
 {
   if (!m)
     return;
 
-  struct SidebarWindowData *wdata = sb_wdata_get(win);
   /* Any new/deleted mailboxes will cause a refresh.  As long as
    * they're valid, our pointers will be updated in prepare_sidebar() */
 
-  if (sbn == SBN_CREATED)
+  struct SbEntry *entry = mutt_mem_calloc(1, sizeof(struct SbEntry));
+  entry->mailbox = m;
+
+  if (wdata->top_index < 0)
+    wdata->top_index = ARRAY_SIZE(&wdata->entries);
+  if (wdata->bot_index < 0)
+    wdata->bot_index = ARRAY_SIZE(&wdata->entries);
+  if ((wdata->opn_index < 0) && Context &&
+      mutt_str_equal(m->realpath, Context->mailbox->realpath))
   {
-    struct SbEntry *entry = mutt_mem_calloc(1, sizeof(struct SbEntry));
-    entry->mailbox = m;
-
-    if (wdata->top_index < 0)
-      wdata->top_index = ARRAY_SIZE(&wdata->entries);
-    if (wdata->bot_index < 0)
-      wdata->bot_index = ARRAY_SIZE(&wdata->entries);
-    if ((wdata->opn_index < 0) && Context &&
-        mutt_str_equal(m->realpath, Context->mailbox->realpath))
-    {
-      wdata->opn_index = ARRAY_SIZE(&wdata->entries);
-    }
-
-    ARRAY_ADD(&wdata->entries, entry);
-  }
-  else if (sbn == SBN_DELETED)
-  {
-    struct SbEntry **sbep = NULL, **to_del = NULL;
-    ARRAY_FOREACH(sbep, &wdata->entries)
-    {
-      if ((*sbep)->mailbox == m)
-      {
-        to_del = sbep;
-        break;
-      }
-    }
-
-    if (!to_del)
-      return;
-
-    int del_index = ARRAY_IDX(&wdata->entries, to_del);
-
-    if ((wdata->top_index > del_index) || (wdata->top_index == ARRAY_SIZE(&wdata->entries)))
-      wdata->top_index--;
-    if (wdata->opn_index == del_index)
-      wdata->opn_index = -1;
-    else if (wdata->opn_index > del_index)
-      wdata->opn_index--;
-    if ((wdata->hil_index > del_index) || (wdata->hil_index == ARRAY_SIZE(&wdata->entries)))
-      wdata->hil_index--;
-    if ((wdata->bot_index > del_index) || (wdata->bot_index == ARRAY_SIZE(&wdata->entries)))
-      wdata->bot_index--;
-
-    FREE(to_del);
-    ARRAY_REMOVE(&wdata->entries, to_del);
+    wdata->opn_index = ARRAY_SIZE(&wdata->entries);
   }
 
-  // otherwise, we just need to redraw
+  ARRAY_ADD(&wdata->entries, entry);
 }
 
 /**
@@ -978,7 +905,7 @@ int sb_recalc(struct MuttWindow *win)
     STAILQ_FOREACH(np, &ml, entries)
     {
       if (!(np->mailbox->flags & MB_HIDDEN))
-        sb_notify_mailbox(win, np->mailbox, SBN_CREATED);
+        sb_notify_mailbox(wdata, np->mailbox);
     }
     neomutt_mailboxlist_clear(&ml);
   }

commit dd27f5cc1f9fc4a36b2daa823c279eba996d3817

fix: shutdown-hook
Commit 978cf231 accidentally trampled over the `shutdown-hook`.

Fixes: #2676

index.c
@@ -1855,6 +1855,9 @@ int mutt_index_menu(struct MuttWindow *dlg)
 
           oldcount = (Context && Context->mailbox) ? Context->mailbox->msg_count : 0;
 
+          mutt_startup_shutdown_hook(MUTT_SHUTDOWN_HOOK);
+          notify_send(NeoMutt->notify, NT_GLOBAL, NT_GLOBAL_SHUTDOWN, NULL);
+
           if (!Context || ((check = mx_mbox_close(&Context)) == 0))
           {
             done = true;