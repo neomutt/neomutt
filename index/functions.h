f6170aba37f7b2fe74ef6cfa01ebd01d59bd633e
Richard Russon <rich@flatcap.org>
2021-02-11 18:56:20 +0000

    index: refactor functions
    # Refactor mutt_index_menu() into separate functions
    
    ## `mutt_index_menu()` is way too big
    
    The function contains 151 `case` statements spread over 2939 lines.
    It's complicated by the fact that the Pager delegates work to the Index.
    
    Functionally, this PR makes no changes.
    
    ## Break Up
    
    The 151 `case` statements have been refactored into 89 new functions in `index/functions.c`
    
    Each Neomutt `<function>` is now listed in `IndexFunctions[]`.
    It maps the opcode to a C function and lists any pre-requisites:
    
    ```c
    { OP_DELETE, op_delete, CHECK_IN_MAILBOX | CHECK_MSGCOUNT | CHECK_READONLY | CHECK_VISIBLE },
    ```
    
    Each handler function has the prototype:
    
    ```c
    int op_XXX(struct IndexSharedData *shared, struct IndexPrivateData *priv, int op);
    ```
    
    Each function returns an `enum IndexRetval` or an opcode, e.g. OP_DELETE.
    The opcode return values are only used by the Pager (`op_display_message()`) and will soon be eliminated.
    
    The enum retvals aren't needed by `mutt_index_menu()` but they may be useful to other callers.
    
    ## Complications
    
    The Pager often delegates its work to the Index.
    This works by `op_display_message()` returning an opcode, meaning:
    - Perform this function
    - Return to the Pager
    
    The functions that allow this behaviour all contain the code:
    ```c
    if (priv->in_pager)
      return IR_CONTINUE;
    ```
    
    Soon, the Pager will be able to call these functions directly and these complications will be eliminated.
    
    ## Tests
    
    This PR should not change any behaviour of the Index or Pager.
    
    I've tested quite a lot of functions in the Index and Pager.
    In particular, I've checked:
    
    - Functions work in the Index
    - Functions work in the Pager
      - and stay in the Pager (e.g. OP_DELETE)
      - and leave the Pager (e.g. OP_QUIT)
    - OP_JUMP works in the Index and Pager
    - Unbound keys show an error message in the Index and Pager
    - Change folder works in the Index and Pager

---
+++ index/functions.h
@@ -0,0 +1,64 @@
+/**
+ * @file
+ * Index functions
+ *
+ * @authors
+ * Copyright (C) 2021 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MUTT_INDEX_FUNCTIONS_H
+#define MUTT_INDEX_FUNCTIONS_H
+
+struct IndexPrivateData;
+struct IndexSharedData;
+
+/**
+ * enum IndexRetval - Possible return values for Index functions
+ */
+enum IndexRetval
+{
+  IR_UNKNOWN   = -7, ///< Unknown key
+  IR_CONTINUE  = -6, ///< Return to the Pager
+  IR_DONE      = -5, ///< Exit the Index
+  IR_NOT_IMPL  = -4, ///< Invalid function - feature not enabled
+  IR_NO_ACTION = -3, ///< Valid function - no action performed
+  IR_ERROR     = -2, ///< Valid function - error occurred
+  IR_SUCCESS   = -1, ///< Valid function - successfully performed
+};
+
+/**
+ * typedef index_function_t - Perform an Index Function
+ * @param shared Shared Index data
+ * @param priv   Private Index data
+ * @param op     Operation to perform, e.g. OP_MAIN_LIMIT
+ * @retval num #IndexRetval or opcode, e.g. OP_JUMP
+ */
+typedef int (*index_function_t)(struct IndexSharedData *shared, struct IndexPrivateData *priv, int op);
+
+/**
+ * struct IndexFunction - A NeoMutt function
+ */
+struct IndexFunction
+{
+  int op;                    ///< Op code, e.g. OP_MAIN_LIMIT
+  index_function_t function; ///< Function to call
+  int flags;                 ///< Prerequisites for the function, e.g. #CHECK_IN_MAILBOX
+};
+
+extern struct IndexFunction IndexFunctions[];
+
+#endif /* MUTT_INDEX_FUNCTIONS_H */
--- index/lib.h
+++ index/lib.h
@@ -28,6 +28,7 @@
  * | File                 | Description                 |
  * | :------------------- | :-------------------------- |
  * | index/config.c       | @subpage index_config       |
+ * | index/functions.c    | @subpage index_functions    |
  * | index/dlg_index.c    | @subpage index_dialog       |
  * | index/ibar.c         | @subpage index_ibar         |
  * | index/index.c        | @subpage index_index        |
@@ -42,6 +43,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "mutt/lib.h"
 #include "core/lib.h"
 #include "shared_data.h" // IWYU pragma: keep
 
@@ -60,6 +62,14 @@ typedef uint8_t NotifyIndex;         ///< Flags, e.g. #NT_INDEX_ACCOUNT
 #define NT_INDEX_MAILBOX   (1 << 5)  ///< Mailbox has changed
 #define NT_INDEX_EMAIL     (1 << 6)  ///< Email has changed
 
+typedef uint8_t CheckFlags;       ///< Flags, e.g. #CHECK_IN_MAILBOX
+#define CHECK_NO_FLAGS         0  ///< No flags are set
+#define CHECK_IN_MAILBOX (1 << 0) ///< Is there a mailbox open?
+#define CHECK_MSGCOUNT   (1 << 1) ///< Are there any messages?
+#define CHECK_VISIBLE    (1 << 2) ///< Is the selected message visible in the index?
+#define CHECK_READONLY   (1 << 3) ///< Is the mailbox readonly?
+#define CHECK_ATTACH     (1 << 4) ///< Is the user in message-attach mode?
+
 int  index_color(struct Menu *menu, int line);
 void index_make_entry(struct Menu *menu, char *buf, size_t buflen, int line);
 void mutt_draw_statusline(struct MuttWindow *win, int cols, const char *buf, size_t buflen);
@@ -67,5 +77,18 @@ struct Mailbox *mutt_index_menu(struct MuttWindow *dlg, struct Mailbox *m);
 void mutt_set_header_color(struct Mailbox *m, struct Email *e);
 void mutt_update_index(struct Menu *menu, struct Context *ctx, enum MxStatus check, int oldcount, struct IndexSharedData *shared);
 struct MuttWindow *index_pager_init(void);
+int mutt_dlgindex_observer(struct NotifyCallback *nc);
+bool check_acl(struct Mailbox *m, AclFlags acl, const char *msg);
+int ci_next_undeleted(struct Mailbox *m, int msgno);
+void update_index(struct Menu *menu, struct Context *ctx, enum MxStatus check, int oldcount, const struct IndexSharedData *shared);
+void change_folder_mailbox(struct Menu *menu, struct Mailbox *m, int *oldcount, struct IndexSharedData *shared, bool read_only);
+void collapse_all(struct Context *ctx, struct Menu *menu, int toggle);
+void change_folder_string(struct Menu *menu, char *buf, size_t buflen, int *oldcount, struct IndexSharedData *shared, bool *pager_return, bool read_only);
+int ci_previous_undeleted(struct Mailbox *m, int msgno);
+int ci_first_message(struct Mailbox *m);
+void resort_index(struct Context *ctx, struct Menu *menu);
+int mx_toggle_write(struct Mailbox *m);
+extern const struct Mapping IndexNewsHelp[];
+struct Mailbox *change_folder_notmuch(struct Menu *menu, char *buf, int buflen, int *oldcount, struct IndexSharedData *shared, bool read_only);
 
 #endif /* MUTT_INDEX_LIB_H */
