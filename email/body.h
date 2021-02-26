287ff9dc600e7487b6fb9abde9302b2f8eb80fa6
Richard Russon <rich@flatcap.org>
2021-02-26 12:55:18 +0000

    Body: reorganise struct Body
    No functional changes

--- email/body.h
+++ email/body.h
@@ -33,55 +33,61 @@
  */
 struct Body
 {
-  char *xtype;                    ///< content-type if x-unknown
-  char *subtype;                  ///< content-type subtype
-  char *language;                 ///< content-language (RFC8255)
-  struct ParameterList parameter; ///< parameters of the content-type
-  char *description;              ///< content-description
-  char *form_name;                ///< Content-Disposition form-data name param
-  long hdr_offset;                ///< Offset in stream where the headers begin.
-                                  ///< This info is used when invoking metamail, where we need to send the headers of the attachment
+  // ---------------------------------------------------------------------------
+  // Data that gets stored in the Header Cache
+
+  unsigned int type        : 4;   ///< content-type primary type, #ContentType
+  unsigned int encoding    : 3;   ///< content-transfer-encoding, #ContentEncoding
+  unsigned int disposition : 2;   ///< content-disposition, #ContentDisposition
+  bool badsig              : 1;   ///< Bad cryptographic signature (needed to check encrypted s/mime-signatures)
+  bool force_charset       : 1;   ///< Send mode: don't adjust the character set when in send-mode.
+  bool goodsig             : 1;   ///< Good cryptographic signature
+  bool noconv              : 1;   ///< Don't do character set conversion
+  bool use_disp            : 1;   ///< Content-Disposition uses filename= ?
+  bool warnsig             : 1;   ///< Maybe good signature
+#ifdef USE_AUTOCRYPT
+  bool is_autocrypt        : 1;   ///< Flag autocrypt-decrypted messages for replying
+#endif
   LOFF_T offset;                  ///< offset where the actual data begins
   LOFF_T length;                  ///< length (in bytes) of attachment
-  char *filename;                 ///< when sending a message, this is the file to which this structure refers
-  char *d_filename;               ///< filename to be used for the content-disposition header.
+
+  char *description;              ///< content-description
+  char *d_filename;               ///< filename to be used for the content-disposition header
                                   ///< If NULL, filename is used instead.
-  char *charset;                  ///< Send mode: charset of attached file as stored on disk.
-                                  ///< The charset used in the generated message is stored in parameter.
+  char *filename;                 ///< When sending a message, this is the file to which this structure refers
+  char *form_name;                ///< Content-Disposition form-data name param
+  char *subtype;                  ///< content-type subtype
+  char *xtype;                    ///< content-type if x-unknown
+  struct ParameterList parameter; ///< Parameters of the content-type
+
+  // ---------------------------------------------------------------------------
+  // Management data - Runtime info and glue to hold the objects together
+
+  bool unlink           : 1;      ///< If true, `filename` should be unlink()ed before free()ing this structure
+
   struct Content *content;        ///< Detailed info about the content of the attachment.
                                   ///< Used to determine what content-transfer-encoding is required when sending mail.
   struct Body *next;              ///< next attachment in the list
   struct Body *parts;             ///< parts of a multipart or message/rfc822
   struct Email *email;            ///< header information for message/rfc822
-
   struct AttachPtr *aptr;         ///< Menu information, used in recvattach.c
-
-  signed short attach_count;      ///< Number of attachments
-
-  time_t stamp;                   ///< Time stamp of last encoding update
-
   struct Envelope *mime_headers;  ///< Memory hole protected headers
+  time_t stamp;                   ///< Time stamp of last encoding update
+  char *language;                 ///< content-language (RFC8255)
+  char *charset;                  ///< Send mode: charset of attached file as stored on disk.
+                                  ///< The charset used in the generated message is stored in parameter.
+  long hdr_offset;                ///< Offset in stream where the headers begin.
+                                  ///< This info is used when invoking metamail, where we need to send the headers of the attachment
 
-  unsigned int type : 4;          ///< content-type primary type, #ContentType
-  unsigned int encoding : 3;      ///< content-transfer-encoding, #ContentEncoding
-  unsigned int disposition : 2;   ///< content-disposition, #ContentDisposition
-  bool use_disp : 1;              ///< Content-Disposition uses filename= ?
-  bool unlink : 1;                ///< If true, `filename` should be unlink()ed before free()ing this structure
-  bool tagged : 1;                ///< This attachment is tagged
-  bool deleted : 1;               ///< Attachment marked for deletion
+  // ---------------------------------------------------------------------------
+  // View data - Used by the GUI
 
-  bool noconv : 1;                ///< Don't do character set conversion
-  bool nowrap : 1;                ///< Do not wrap the output in the pager
-  bool force_charset : 1;         ///< Send mode: don't adjust the character set when in send-mode.
-  bool goodsig : 1;               ///< Good cryptographic signature
-  bool warnsig : 1;               ///< Maybe good signature
-  bool badsig : 1;                ///< Bad cryptographic signature (needed to check encrypted s/mime-signatures)
-#ifdef USE_AUTOCRYPT
-  bool is_autocrypt : 1;          ///< Flag autocrypt-decrypted messages for replying
-#endif
-
-  bool collapsed : 1;             ///< Used by recvattach
   bool attach_qualifies : 1;      ///< This attachment should be counted
+  bool collapsed        : 1;      ///< Used by recvattach
+  bool deleted          : 1;      ///< Attachment marked for deletion
+  bool nowrap           : 1;      ///< Do not wrap the output in the pager
+  bool tagged           : 1;      ///< This attachment is tagged
+  signed short attach_count;      ///< Number of attachments
 };
 
 bool         mutt_body_cmp_strict(const struct Body *b1, const struct Body *b2);
