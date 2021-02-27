29a0539379f9863f0b35ee1d7fdc567c3019766c
Ihor Antonov <ihor@antonovs.family>
2021-02-27 08:26:26 -0800

    pager: add comment about macros

--- pager.c
+++ pager.c
@@ -273,6 +273,53 @@ static const struct Mapping PagerNewsHelp[] = {
 
 #define IS_HEADER(x) ((x) == MT_COLOR_HEADER || (x) == MT_COLOR_HDRDEFAULT)
 
+// The macros below appear to be used to determine what mode pagers is
+// operating in. In order to
+// - bring some clarity
+// - prepare for further refactoring
+// I decided to write this comment.
+//
+// There are 10 code paths that lead to mutt_pager() invocation:
+//
+// 1. mutt_index_menu -> mutt_display_message -> mutt_pager
+//
+//    This path always results in mailbox and email set,
+//    the rest is unset - Body, fp.
+//    This invocation can be identified by IsEmail macro.
+//    The intent is to display an email message
+//
+// 2. mutt_view_attachment -> mutt_do_pager -> mutt_pager
+//
+//    this path always results in email, body, ctx set
+//    this invocation can be identified by one of the two macros
+//    - IsAttach (the Body could be any old attachment)
+//    - IsMsgAttach (or a full Email message)
+//    The distinction between the two is set/unset fp and the following:
+//
+//    The intent is to display an attachment of the email message
+//
+// 3. The following 8 invocations are similar, because they all call
+//    mutt_do_page with info = NULL
+//
+//    And so it results in mailbox, body, fp set to NULL.
+//    The intent is to show user some text that is not
+//    directly related to viewing emails,
+//    e.g. help, log messages,gpg key selection etc.
+//
+//    No macro identifies these invocations
+//
+//    mutt_index_menu       -> mutt_do_pager -> mutt_pager
+//    mutt_help             -> mutt_do_pager -> mutt_pager
+//    icmd_bind             -> mutt_do_pager -> mutt_pager
+//    icmd_set              -> mutt_do_pager -> mutt_pager
+//    icmd_version          -> mutt_do_pager -> mutt_pager
+//    dlg_select_pgp_key    -> mutt_do_pager -> mutt_pager
+//    verify_key            -> mutt_do_pager -> mutt_pager
+//    mutt_invoke_sendmail  -> mutt_do_pager -> mutt_pager
+//
+// See nice infographic here:
+// https://gist.github.com/flatcap/044ecbd2498c65ea9a85099ef317509a
+
 #define IsAttach(pager) (pager && (pager)->body)
 #define IsMsgAttach(pager)                                                     \
   (pager && (pager)->fp && (pager)->body && (pager)->body->email)
