0da35d8ff3b943160b2e679f314ad8067f1586c2
Pietro Cerutti <gahr@gahr.ch>
2021-03-23 09:56:42 +0000

    Allow a -noregex param to folder and mbox hooks
    This is helpful if we want to match mailbox that comes from a variable
    exactly (or we simply can't be bothered to escape the whole thingie).
    As an example, in `folder-hook -noregex $trash set my_foo=bar` we don't
    want an expansion of `$trash` such as `imaps://user@gmail.com/[Gmail]/Trash`
    to be treated as a regex containing a character class `[Gmail]`.

--- hook.c
+++ hook.c
@@ -85,8 +85,10 @@ enum CommandResult mutt_parse_hook(struct Buffer *buf, struct Buffer *s,
   struct Hook *hook = NULL;
   int rc = MUTT_CMD_ERROR;
   bool pat_not = false;
+  bool use_regex = true;
   regex_t *rx = NULL;
   struct PatternList *pat = NULL;
+  const bool folder_or_mbox = (data & (MUTT_FOLDER_HOOK | MUTT_MBOX_HOOK));
 
   struct Buffer *cmd = mutt_buffer_pool_get();
   struct Buffer *pattern = mutt_buffer_pool_get();
@@ -101,6 +103,17 @@ enum CommandResult mutt_parse_hook(struct Buffer *buf, struct Buffer *s,
     }
 
     mutt_extract_token(pattern, s, MUTT_TOKEN_NO_FLAGS);
+    if (folder_or_mbox && mutt_str_equal(mutt_buffer_string(pattern), "-noregex"))
+    {
+      use_regex = false;
+      if (!MoreArgs(s))
+      {
+        mutt_buffer_printf(err, _("%s: too few arguments"), buf->data);
+        rc = MUTT_CMD_WARNING;
+        goto cleanup;
+      }
+      mutt_extract_token(pattern, s, MUTT_TOKEN_NO_FLAGS);
+    }
 
     if (!MoreArgs(s))
     {
@@ -132,7 +145,7 @@ enum CommandResult mutt_parse_hook(struct Buffer *buf, struct Buffer *s,
 
   const char *const c_default_hook =
       cs_subset_string(NeoMutt->sub, "default_hook");
-  if (data & (MUTT_FOLDER_HOOK | MUTT_MBOX_HOOK))
+  if (folder_or_mbox)
   {
     /* Accidentally using the ^ mailbox shortcut in the .neomuttrc is a
      * common mistake */
@@ -144,7 +157,7 @@ enum CommandResult mutt_parse_hook(struct Buffer *buf, struct Buffer *s,
 
     struct Buffer *tmp = mutt_buffer_pool_get();
     mutt_buffer_copy(tmp, pattern);
-    mutt_buffer_expand_path_regex(tmp, true);
+    mutt_buffer_expand_path_regex(tmp, use_regex);
 
     /* Check for other mailbox shortcuts that expand to the empty string.
      * This is likely a mistake too */
@@ -155,7 +168,14 @@ enum CommandResult mutt_parse_hook(struct Buffer *buf, struct Buffer *s,
       goto cleanup;
     }
 
-    mutt_buffer_copy(pattern, tmp);
+    if (use_regex)
+    {
+      mutt_buffer_copy(pattern, tmp);
+    }
+    else
+    {
+      mutt_file_sanitize_regex(pattern, mutt_buffer_string(tmp));
+    }
     mutt_buffer_pool_release(&tmp);
   }
 #ifdef USE_COMP_MBOX
