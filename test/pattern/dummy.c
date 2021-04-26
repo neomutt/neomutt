428ea487959b16632cbda780d04d7cbf35b4c7a1
Richard Russon <rich@flatcap.org>
2021-04-26 13:02:07 +0100

    compose: ensure focus is on Menu
    - Link dialog focus to Menu window
    - Ensure Menu window points to a Menu

--- test/pattern/dummy.c
+++ test/pattern/dummy.c
@@ -68,10 +68,10 @@ typedef uint16_t CompletionFlags;
 typedef uint16_t PagerFlags;
 typedef uint8_t SelectFileFlags;
 
-typedef const char *(format_t)(char *buf, size_t buflen, size_t col, int cols,
-                               char op, const char *src, const char *prec,
-                               const char *if_str, const char *else_str,
-                               intptr_t data, MuttFormatFlags flags);
+typedef const char *(format_t) (char *buf, size_t buflen, size_t col, int cols,
+                                char op, const char *src, const char *prec,
+                                const char *if_str, const char *else_str,
+                                intptr_t data, MuttFormatFlags flags);
 
 int crypt_valid_passphrase(int flags)
 {
