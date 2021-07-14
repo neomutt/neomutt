666c23e240b3ac7827fd8e201bceb695c58cfdb3
Richard Russon <rich@flatcap.org>
2021-07-14 15:13:21 +0100

    observers: postpone

--- postpone.c
+++ postpone.c
@@ -214,6 +214,60 @@ static void post_make_entry(struct Menu *menu, char *buf, size_t buflen, int lin
 }
 
 /**
+ * postponed_config_observer - Notification that a Config Variable has changed - Implements ::observer_t
+ *
+ * The Address Book Window is affected by changes to `$sort_postponed`.
+ */
+static int postponed_config_observer(struct NotifyCallback *nc)
+{
+  if ((nc->event_type != NT_CONFIG) || !nc->global_data || !nc->event_data)
+    return -1;
+
+  if (nc->event_subtype == NT_CONFIG_INITIAL_SET)
+    return 0;
+
+  struct EventConfig *ev_c = nc->event_data;
+
+  if (!mutt_str_equal(ev_c->name, "index_format") && !mutt_str_equal(ev_c->name, "sort"))
+    return 0;
+
+  struct Menu *menu = nc->global_data;
+  menu_queue_redraw(menu, MENU_REDRAW_FULL);
+  mutt_debug(LL_DEBUG5, "config done, request WA_RECALC, MENU_REDRAW_FULL\n");
+
+  return 0;
+}
+
+/**
+ * postponed_window_observer - Notification that a Window has changed - Implements ::observer_t
+ *
+ * This function is triggered by changes to the windows.
+ *
+ * - Delete (this window): clean up the resources held by the Help Bar
+ */
+static int postponed_window_observer(struct NotifyCallback *nc)
+{
+  if ((nc->event_type != NT_WINDOW) || !nc->global_data || !nc->event_data)
+    return -1;
+
+  if (nc->event_subtype != NT_WINDOW_DELETE)
+    return 0;
+
+  struct MuttWindow *win_menu = nc->global_data;
+  struct EventWindow *ev_w = nc->event_data;
+  if (ev_w->win != win_menu)
+    return 0;
+
+  struct Menu *menu = win_menu->wdata;
+
+  notify_observer_remove(NeoMutt->notify, postponed_config_observer, menu);
+  notify_observer_remove(win_menu->notify, postponed_window_observer, win_menu);
+
+  mutt_debug(LL_DEBUG5, "window delete done\n");
+  return 0;
+}
+
+/**
  * dlg_select_postponed_email - Create a Menu to select a postponed message
  * @param m Mailbox
  * @retval ptr Email
@@ -230,6 +284,12 @@ static struct Email *dlg_select_postponed_email(struct Mailbox *m)
   menu->max = m->msg_count;
   menu->mdata = m;
   menu->custom_search = true;
+
+  struct MuttWindow *win_menu = menu->win_index;
+
+  // NT_COLOR is handled by the SimpleDialog
+  notify_observer_add(NeoMutt->notify, NT_CONFIG, postponed_config_observer, menu);
+  notify_observer_add(win_menu->notify, NT_WINDOW, postponed_window_observer, win_menu);
 
   struct MuttWindow *sbar = window_find_child(dlg, WT_STATUS_BAR);
   sbar_set_title(sbar, _("Postponed Messages"));
