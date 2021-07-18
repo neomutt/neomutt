ed70fefc4253231c6a307fd23685d5d88ba7a1f5
Alexander Kuleshov <kuleshovmail@gmail.com>
2021-07-18 19:55:43 +0600

    Fix building of neomutt if HAVE_RAND_EGD is enabled
    This commit adds missed mutt_globals.h header to openssl.c as
    without it the build is broken with:
    
    In file included from ./mutt/lib.h:97,
                     from conn/openssl.c:52:
    conn/openssl.c: In function ‘ssl_init’:
    conn/openssl.c:568:52: error: ‘HomeDir’ undeclared (first use in this function)
      568 |     mutt_buffer_printf(path, "%s/.entropy", NONULL(HomeDir));
    
    error.

--- conn/openssl.c
+++ conn/openssl.c
@@ -56,6 +56,9 @@
 #include "lib.h"
 #include "mutt_logging.h"
 #include "ssl.h"
+#ifdef HAVE_RAND_EGD
+#include "mutt_globals.h"
+#endif
 
 /* LibreSSL defines OPENSSL_VERSION_NUMBER but sets it to 0x20000000L.
  * So technically we don't need the defined(OPENSSL_VERSION_NUMBER) check.  */
