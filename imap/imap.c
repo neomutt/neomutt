095eed7f8b04d555652069fbf8304f658b959bdb
Richard Russon <rich@flatcap.org>
2021-09-07 21:42:07 -0700

    Fix compare_uid() to work with large UID values
    The function was pulled from the other sort methods used in neomutt.  But
    those don't work properly for a 32-bit unsigned value.  If the
    difference between two UID values is greater than a signed int can
    represent, it will sort improperly.
    
    Some of the other sort functions need to be fixed too, but that can be
    done in master.
    
    Upstream-commit: https://gitlab.com/muttmua/mutt/commit/fa2fef4618ed45d9f4011ea76fd8bc3844d59c88
    
    Co-authored-by: Kevin McCarthy <kevin@8t8.us>

--- imap/imap.c
+++ imap/imap.c
@@ -904,8 +904,15 @@ static int compare_uid(const void *a, const void *b)
 {
   const struct Email *ea = *(struct Email const *const *) a;
   const struct Email *eb = *(struct Email const *const *) b;
-  return imap_edata_get((struct Email *) ea)->uid -
-         imap_edata_get((struct Email *) eb)->uid;
+
+  const unsigned int ua = imap_edata_get((struct Email *) ea)->uid;
+  const unsigned int ub = imap_edata_get((struct Email *) eb)->uid;
+
+  if (ua < ub)
+    return -1;
+  if (ua > ub)
+    return 1;
+  return 0;
 }
 
 /**
