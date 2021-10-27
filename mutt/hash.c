68b9d4d97fef28cec2a3d0bd1139e46dca9791b7
Richard Russon <rich@flatcap.org>
2021-10-27 11:43:45 +0100

    fix: protect hash table against empty keys

--- mutt/hash.c
+++ mutt/hash.c
@@ -38,11 +38,15 @@
 
 /**
  * gen_string_hash - Generate a hash from a string - Implements hash_gen_hash_t - @ingroup hash_gen_hash_api
+ *
+ * @note If the key is NULL or empty, the retval will be 0
  */
 static size_t gen_string_hash(union HashKey key, size_t num_elems)
 {
   size_t hash = 0;
   const unsigned char *s = (const unsigned char *) key.strkey;
+  if (!s)
+    return 0;
 
   while (*s != '\0')
     hash += ((hash << 7) + *s++);
@@ -309,7 +313,7 @@ void mutt_hash_set_destructor(struct HashTable *table, hash_hdata_free_t fn, int
 struct HashElem *mutt_hash_typed_insert(struct HashTable *table,
                                         const char *strkey, int type, void *data)
 {
-  if (!table || !strkey)
+  if (!table || !strkey || (strkey[0] == '\0'))
     return NULL;
 
   union HashKey key;
@@ -418,7 +422,7 @@ struct HashElem *mutt_hash_find_bucket(const struct HashTable *table, const char
  */
 void mutt_hash_delete(struct HashTable *table, const char *strkey, const void *data)
 {
-  if (!table || !strkey)
+  if (!table || !strkey || (strkey[0] == '\0'))
     return;
   union HashKey key;
   // Copy the key because union_hash_delete() may use it after the HashElem is freed.
