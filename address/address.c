4242a31313e0b600693215c01047bbda8a6dd25a
Richard Russon <rich@flatcap.org>
2021-10-29 17:20:27 +0100

    Revert "Iterate safely"
    This reverts commit 322e0dda487e2923ec36b109bd9d475b025689ce.

--- address/address.c
+++ address/address.c
@@ -1410,17 +1410,16 @@ void mutt_addrlist_dedupe(struct AddressList *al)
     return;
 
   struct Address *a = NULL;
-  struct Address *tmp = NULL;
-  TAILQ_FOREACH_SAFE(a, al, entries, tmp)
+  TAILQ_FOREACH(a, al, entries)
   {
     if (a->mailbox)
     {
       struct Address *a2 = TAILQ_NEXT(a, entries);
+      struct Address *tmp = NULL;
 
       if (a2)
       {
-        struct Address *tmp2 = NULL;
-        TAILQ_FOREACH_FROM_SAFE(a2, al, entries, tmp2)
+        TAILQ_FOREACH_FROM_SAFE(a2, al, entries, tmp)
         {
           if (a2->mailbox && mutt_istr_equal(a->mailbox, a2->mailbox))
           {
