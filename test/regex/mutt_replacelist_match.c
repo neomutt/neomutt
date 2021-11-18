f6989fdaeca9d44fd38310b594e28e0deb01632f
Richard Russon <rich@flatcap.org>
2021-11-18 18:32:58 +0000

    tests: fix leaks in test_mutt_replacelist_match()

--- test/regex/mutt_replacelist_match.c
+++ test/regex/mutt_replacelist_match.c
@@ -52,6 +52,7 @@ void test_mutt_replacelist_match(void)
     char buf[32] = { 0 };
     TEST_CHECK(mutt_replacelist_match(&replacelist, buf, sizeof(buf), "foo-1234-bar"));
     TEST_CHECK_STR_EQ("foo [foo-1234-bar] bar", buf);
+    mutt_replacelist_free(&replacelist);
   }
 
   {
@@ -60,6 +61,7 @@ void test_mutt_replacelist_match(void)
     char buf[32] = { 0 };
     TEST_CHECK(mutt_replacelist_match(&replacelist, buf, sizeof(buf), "foo-1234-bar"));
     TEST_CHECK_STR_EQ("foo [1234] bar", buf);
+    mutt_replacelist_free(&replacelist);
   }
 
   {
@@ -67,5 +69,6 @@ void test_mutt_replacelist_match(void)
     mutt_replacelist_add(&replacelist, "foo-([^-]+)-bar", "foo [%2] bar", NULL);
     char buf[32] = { 0 };
     TEST_CHECK(!mutt_replacelist_match(&replacelist, buf, sizeof(buf), "foo-1234-bar"));
+    mutt_replacelist_free(&replacelist);
   }
 }
