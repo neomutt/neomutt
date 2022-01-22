6849de687599a3a576a6b0e5ac1f503580754fb3
Richard Russon <rich@flatcap.org>
2022-01-22 17:44:45 +0000

    compose: tidy op_save()
    Create some locals to avoid long strings of pointers:
    
    - struct AttachCtx *actx = shared->adata->actx;
    - struct Menu *menu = shared->adata->menu;

--- compose/functions.c
+++ compose/functions.c
@@ -1583,17 +1583,19 @@ static int op_attachment_rename_attachment(struct ComposeSharedData *shared, int
  */
 static int op_attachment_save(struct ComposeSharedData *shared, int op)
 {
-  if (!check_count(shared->adata->actx))
+  struct AttachCtx *actx = shared->adata->actx;
+  if (!check_count(actx))
     return IR_NO_ACTION;
-  struct AttachPtr *cur_att =
-      current_attachment(shared->adata->actx, shared->adata->menu);
+
+  struct Menu *menu = shared->adata->menu;
+  struct AttachPtr *cur_att = current_attachment(actx, menu);
   if (cur_att->body->type == TYPE_MULTIPART)
   {
     mutt_error(_("Can't save multipart attachments"));
     return IR_ERROR;
   }
-  mutt_save_attachment_list(shared->adata->actx, NULL, shared->adata->menu->tagprefix,
-                            cur_att->body, NULL, shared->adata->menu);
+
+  mutt_save_attachment_list(actx, NULL, menu->tagprefix, cur_att->body, NULL, menu);
   /* no send2hook, since this doesn't modify the message */
   return IR_SUCCESS;
 }
