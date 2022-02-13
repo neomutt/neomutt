17c24bc26720bdb37751d4190c6d18880b42a566
Richard Russon <rich@flatcap.org>
2022-02-13 18:59:44 +0000

    create libenvelope
    Create a new library to hold the Envelope Window.
    
    This self-contained Window will manage the display of an Email's
    Envelope in the Compose Dialog.

---
+++ envelope/lib.h
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * Envelope-editing Window
+ *
+ * @authors
+ * Copyright (C) 2022 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @page lib_envelope Envelope-editing Window
+ *
+ * Envelope-editing Window
+ *
+ * | File                 | Description                 |
+ * | :------------------  | :-------------------------- |
+ */
+
+#ifndef MUTT_ENVELOPE_LIB_H
+#define MUTT_ENVELOPE_LIB_H
+
+extern int dummy;
+
+#endif /* MUTT_ENVELOPE_LIB_H */
