b02042d62a5fcd889464d275053727ae26f97e94
Pietro Cerutti <gahr@gahr.ch>
2022-03-05 23:41:09 +0000

    Revert "fix crash saving fcc to current mailbox"
    This reverts commit c6e293cbc57787b01c338ce2e54f76c68f3ea593.

--- send/send.c
+++ send/send.c
@@ -1880,8 +1880,6 @@ full_fcc:
      * the From_ line contains the current time instead of when the
      * message was first postponed.  */
     e->received = mutt_date_epoch();
-    const int old_flags = m->flags;
-    m->flags = MB_NORMAL;
     rc = mutt_write_multiple_fcc(mutt_buffer_string(fcc), e, NULL, false, NULL,
                                  finalpath, sub);
     while (rc && !(flags & SEND_BATCH))
@@ -1922,7 +1920,6 @@ full_fcc:
           break;
       }
     }
-    m->flags = old_flags;
   }
 
   if (!c_fcc_before_send)
