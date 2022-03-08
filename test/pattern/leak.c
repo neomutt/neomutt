4f649258890a873cc08b2e7ca379bfae8f29093c
Pietro Cerutti <gahr@gahr.ch>
2022-03-08 13:20:58 +0000

    Add tests

---
+++ test/pattern/leak.c
@@ -0,0 +1,16 @@
+#define TEST_NO_MAIN
+#define MAIN_C 1
+
+#include "pattern/lib.h"
+
+static void test_one_leak(const char *pattern)
+{
+  struct PatternList *p = mutt_pattern_comp(NULL, NULL, pattern, 0, NULL);
+  mutt_pattern_free(&p);
+}
+
+void test_mutt_pattern_leak(void)
+{
+  test_one_leak("~E ~F | ~D");
+  test_one_leak("~D | ~E ~F");
+}
