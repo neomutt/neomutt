35c84e8e1765105311eb67a5f1d7e563fb2f225e
Richard Russon <rich@flatcap.org>
2022-03-09 02:27:35 +0000

    menu: move dialog functions
    menu_dialog_dokey() and menu_dialog_translate_op() are only used by the
    Certificate Verification Dialog.

--- conn/dlg_verifycert.c
+++ conn/dlg_verifycert.c
@@ -70,6 +70,70 @@ static const struct Mapping VerifyHelp[] = {
 };
 
 /**
+ * menu_dialog_dokey - Check if there are any menu key events to process
+ * @param menu Current Menu
+ * @param id   KeyEvent ID
+ * @retval  0 An event occurred for the menu, or a timeout
+ * @retval -1 There was an event, but not for menu
+ */
+int menu_dialog_dokey(struct Menu *menu, int *id)
+{
+  struct KeyEvent ch = { OP_NULL, OP_NULL };
+  char *p = NULL;
+
+  enum MuttCursorState cursor = mutt_curses_set_cursor(MUTT_CURSOR_VISIBLE);
+  do
+  {
+    ch = mutt_getch();
+  } while (ch.ch == OP_TIMEOUT);
+  mutt_curses_set_cursor(cursor);
+
+  if (ch.ch < 0)
+  {
+    *id = -1;
+    return 0;
+  }
+
+  if ((ch.ch != 0) && (p = strchr(menu->keys, ch.ch)))
+  {
+    *id = OP_MAX + (p - menu->keys + 1);
+    return 0;
+  }
+  else
+  {
+    if (ch.op == OP_NULL)
+      mutt_unget_event(ch.ch, OP_NULL);
+    else
+      mutt_unget_event(0, ch.op);
+    return -1;
+  }
+}
+
+/**
+ * menu_dialog_translate_op - Convert menubar movement to scrolling
+ * @param op Action requested, e.g. OP_NEXT_ENTRY
+ * @retval num Action to perform, e.g. OP_NEXT_LINE
+ */
+int menu_dialog_translate_op(int op)
+{
+  switch (op)
+  {
+    case OP_NEXT_ENTRY:
+      return OP_NEXT_LINE;
+    case OP_PREV_ENTRY:
+      return OP_PREV_LINE;
+    case OP_CURRENT_TOP:
+      return OP_TOP_PAGE;
+    case OP_CURRENT_BOTTOM:
+      return OP_BOTTOM_PAGE;
+    case OP_CURRENT_MIDDLE:
+      return OP_MIDDLE_PAGE;
+  }
+
+  return op;
+}
+
+/**
  * cert_array_clear - Free all memory of a CertArray
  * @param carr Array of text to clear
  *
