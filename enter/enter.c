cb10fce0e763f6eacf44fcd4210461c5b061257f
Richard Russon <rich@flatcap.org>
2022-05-27 19:24:38 +0100

    enter: create enter buffer
    Create a non-gui enter buffer.

---
+++ enter/enter.c
@@ -0,0 +1,174 @@
+/**
+ * @file
+ * Enter buffer
+ *
+ * @authors
+ * Copyright (C) 2022 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @page enter_enter Enter buffer
+ *
+ * Enter buffer
+ */
+
+#include "config.h"
+#include "state.h"
+
+/**
+ * editor_backspace - Delete the char in front of the cursor
+ */
+int editor_backspace(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_backward_char - Move the cursor one character to the left
+ */
+int editor_backward_char(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_backward_word - Move the cursor to the beginning of the word
+ */
+int editor_backward_word(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_bol - Jump to the beginning of the line
+ */
+int editor_bol(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_capitalize_word - Capitalize the word
+ */
+int editor_capitalize_word(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_delete_char - Delete the char under the cursor
+ */
+int editor_delete_char(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_downcase_word - Convert the word to lower case
+ */
+int editor_downcase_word(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_eol - Jump to the end of the line
+ */
+int editor_eol(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_forward_char - Move the cursor one character to the right
+ */
+int editor_forward_char(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_forward_word - Move the cursor to the end of the word
+ */
+int editor_forward_word(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_kill_eol - Delete chars from cursor to end of line
+ */
+int editor_kill_eol(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_kill_eow - Delete chars from the cursor to the end of the word
+ */
+int editor_kill_eow(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_kill_line - Delete all chars on the line
+ */
+int editor_kill_line(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_kill_word - Delete the word in front of the cursor
+ */
+int editor_kill_word(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_mailbox_cycle - Cycle among incoming mailboxes
+ */
+int editor_mailbox_cycle(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_quote_char - Quote the next typed key
+ */
+int editor_quote_char(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_transpose_chars - Transpose character under cursor with previous
+ */
+int editor_transpose_chars(struct EnterState *es)
+{
+  return 0;
+}
+
+/**
+ * editor_upcase_word - Convert the word to upper case
+ */
+int editor_upcase_word(struct EnterState *es)
+{
+  return 0;
+}
--- enter/lib.h
+++ enter/lib.h
@@ -27,6 +27,7 @@
  *
  * | File                | Description                |
  * | :------------------ | :------------------------- |
+ * | enter/enter.c       | @subpage enter_enter       |
  * | enter/functions.c   | @subpage enter_functions   |
  * | enter/state.c       | @subpage enter_state       |
  * | enter/window.c      | @subpage enter_window      |
