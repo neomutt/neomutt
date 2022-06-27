e5909b7540edd00681ba89c0245778242f3f5bbd
Richard Russon <rich@flatcap.org>
2022-06-27 10:34:15 +0100

    fix stray debugging
    Remove a bit of debugging code left over from f341587c29.

--- convert/content_info.c
+++ convert/content_info.c
@@ -228,9 +228,7 @@ struct Content *mutt_get_content_info(const char *fname, struct Body *b,
                                    (c_attach_charset ? c_attach_charset : c_charset_slist) :
                                    c_charset_slist;
 
-    // struct Slist *chs = slist_parse(mutt_param_get(&b->parameter, "charset"), SLIST_SEP_COLON);
-    struct Slist *chs = slist_parse("charset", SLIST_SEP_COLON);
-    mutt_debug(LL_DEBUG1, "marker4 %p\n", chs);
+    struct Slist *chs = slist_parse(mutt_param_get(&b->parameter, "charset"), SLIST_SEP_COLON);
 
     if (c_charset && (chs || c_send_charset) &&
         (mutt_convert_file_from_to(fp, fchs, chs ? chs : c_send_charset,
