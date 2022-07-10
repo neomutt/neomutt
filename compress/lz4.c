d5e6e7e74a78ff97fb07dbefe0874674f7030c45
Tobias Stoeckmann <tobias@stoeckmann.org>
2022-07-10 17:11:03 +0200

    compress: check if lengths are stored
    The lz4 and zlib functions store length of uncompressed data in front of
    it. Make sure that it*s still there when reading cache file back into
    memory.

--- compress/lz4.c
+++ compress/lz4.c
@@ -112,6 +112,8 @@ static void *compr_lz4_decompress(void *cctx, const char *cbuf, size_t clen)
 
   /* first 4 bytes store the size */
   const unsigned char *cs = (const unsigned char *) cbuf;
+  if (clen < 4)
+    return NULL;
   size_t ulen = cs[0] + (cs[1] << 8) + (cs[2] << 16) + ((size_t) cs[3] << 24);
   if (ulen == 0)
     return (void *) cbuf;
