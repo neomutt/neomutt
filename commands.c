5f4d38544c044d24b2850379005a3d06a5abca0a
Richard Russon <rich@flatcap.org>
2022-07-11 12:19:39 +0100

    source: fix variable interpretation
    Change `source` to look for NeoMutt variables first.

--- commands.c
+++ commands.c
@@ -1054,7 +1054,7 @@ static enum CommandResult parse_source(struct Buffer *buf, struct Buffer *s,
 
   do
   {
-    if (parse_extract_token(buf, s, TOKEN_NO_FLAGS) != 0)
+    if (parse_extract_token(buf, s, TOKEN_BACKTICK_VARS) != 0)
     {
       buf_printf(err, _("source: error at %s"), s->dptr);
       return MUTT_CMD_ERROR;
