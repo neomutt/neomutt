neomutt/neomutt

commit d9ff90dced82e7328dff4abc0918ee0020af8d0a

Convert HeaderOrderList and (Un)?Ignore to STailQ
Issue #374

copy.c
@@ -68,7 +68,6 @@ int mutt_copy_hdr(FILE *in, FILE *out, LOFF_T off_start, LOFF_T off_end,
   bool ignore = false;
   char buf[LONG_STRING]; /* should be long enough to get most fields in one pass */
   char *nl = NULL;
-  struct List *t = NULL;
   char **headers = NULL;
   int hdr_count;
   int x;
@@ -157,9 +156,10 @@ int mutt_copy_hdr(FILE *in, FILE *out, LOFF_T off_start, LOFF_T off_end,
    */
   if (flags & CH_REORDER)
   {
-    for (t = HeaderOrderList; t; t = t->next)
+    struct STailQNode *np;
+    STAILQ_FOREACH(np, &HeaderOrderList, entries)
     {
-      mutt_debug(3, "Reorder list: %s\n", t->data);
+      mutt_debug(3, "Reorder list: %s\n", np->data);
       hdr_count++;
     }
   }
@@ -251,11 +251,14 @@ int mutt_copy_hdr(FILE *in, FILE *out, LOFF_T off_start, LOFF_T off_end,
       /* Find x -- the array entry where this header is to be saved */
       if (flags & CH_REORDER)
       {
-        for (t = HeaderOrderList, x = 0; (t); t = t->next, x++)
+        struct STailQNode *np;
+        x = 0;
+        STAILQ_FOREACH(np, &HeaderOrderList, entries)
         {
-          if (mutt_strncasecmp(buf, t->data, mutt_strlen(t->data)) == 0)
+          ++x;
+          if (mutt_strncasecmp(buf, np->data, mutt_strlen(np->data)) == 0)
           {
-            mutt_debug(2, "Reorder: %s matches %s\n", t->data, buf);
+            mutt_debug(2, "Reorder: %s matches %s\n", np->data, buf);
             break;
           }
         }

globals.h
@@ -208,11 +208,11 @@ WHERE struct STailQHead AttachAllow INITVAL(STAILQ_HEAD_INITIALIZER(AttachAllow)
 WHERE struct STailQHead AttachExclude INITVAL(STAILQ_HEAD_INITIALIZER(AttachExclude));
 WHERE struct STailQHead InlineAllow INITVAL(STAILQ_HEAD_INITIALIZER(InlineAllow));
 WHERE struct STailQHead InlineExclude INITVAL(STAILQ_HEAD_INITIALIZER(InlineExclude));
-WHERE struct List *HeaderOrderList;
-WHERE struct List *Ignore;
+WHERE struct STailQHead HeaderOrderList INITVAL(STAILQ_HEAD_INITIALIZER(HeaderOrderList));
+WHERE struct STailQHead Ignore INITVAL(STAILQ_HEAD_INITIALIZER(Ignore));
 WHERE struct List *MailToAllow;
 WHERE struct List *MimeLookupList;
-WHERE struct List *UnIgnore;
+WHERE struct STailQHead UnIgnore INITVAL(STAILQ_HEAD_INITIALIZER(UnIgnore));
 
 WHERE struct RxList *Alternates;
 WHERE struct RxList *UnAlternates;

init.c
@@ -1125,6 +1125,26 @@ static int parse_ifdef(struct Buffer *tmp, struct Buffer *s, unsigned long data,
   return 0;
 }
 
+static void remove_from_stailq(struct STailQHead *head, const char *str)
+{
+  if (mutt_strcmp("*", str) == 0)
+    mutt_stailq_free(head); /* ``unCMD *'' means delete all current entries */
+  else
+  {
+    struct STailQNode *np, *tmp;
+    STAILQ_FOREACH_SAFE(np, head, entries, tmp)
+    {
+      if (mutt_strcasecmp(str, np->data) == 0)
+      {
+        STAILQ_REMOVE(head, np, STailQNode, entries);
+        FREE(&np->data);
+        FREE(&np);
+        break;
+      }
+    }
+  }
+}
+
 static int parse_unignore(struct Buffer *buf, struct Buffer *s,
                           unsigned long data, struct Buffer *err)
 {
@@ -1134,9 +1154,9 @@ static int parse_unignore(struct Buffer *buf, struct Buffer *s,
 
     /* don't add "*" to the unignore list */
     if (strcmp(buf->data, "*") != 0)
-      add_to_list(&UnIgnore, buf->data);
+      add_to_stailq(&UnIgnore, buf->data);
 
-    remove_from_list(&Ignore, buf->data);
+    remove_from_stailq(&Ignore, buf->data);
   } while (MoreArgs(s));
 
   return 0;
@@ -1148,8 +1168,8 @@ static int parse_ignore(struct Buffer *buf, struct Buffer *s,
   do
   {
     mutt_extract_token(buf, s, 0);
-    remove_from_list(&UnIgnore, buf->data);
-    add_to_list(&Ignore, buf->data);
+    remove_from_stailq(&UnIgnore, buf->data);
+    add_to_stailq(&Ignore, buf->data);
   } while (MoreArgs(s));
 
   return 0;
@@ -1178,26 +1198,6 @@ static int parse_stailq(struct Buffer *buf, struct Buffer *s, unsigned long data
   } while (MoreArgs(s));
 
   return 0;
-}
-
-static void remove_from_stailq(struct STailQHead *head, const char *str)
-{
-  if (mutt_strcmp("*", str) == 0)
-    mutt_stailq_free(head); /* ``unCMD *'' means delete all current entries */
-  else
-  {
-    struct STailQNode *np, *tmp;
-    STAILQ_FOREACH_SAFE(np, head, entries, tmp)
-    {
-      if (mutt_strcasecmp(str, np->data) == 0)
-      {
-        STAILQ_REMOVE(head, np, STailQNode, entries);
-        FREE(&np->data);
-        FREE(&np);
-        break;
-      }
-    }
-  }
 }
 
 static int parse_unstailq(struct Buffer *buf, struct Buffer *s,

init.h
@@ -4599,7 +4599,7 @@ const struct Command Commands[] = {
 #endif
   { "group",            parse_group,            MUTT_GROUP },
   { "ungroup",          parse_group,            MUTT_UNGROUP },
-  { "hdr_order",        parse_list,             UL &HeaderOrderList },
+  { "hdr_order",        parse_stailq,           UL &HeaderOrderList },
   { "ifdef",            parse_ifdef,            0 },
   { "ifndef",           parse_ifdef,            1 },
   { "finish",           finish_source,          0 },
@@ -4655,7 +4655,7 @@ const struct Command Commands[] = {
   { "unalias",          parse_unalias,          0 },
   { "unalternative_order",parse_unstailq,       UL &AlternativeOrderList },
   { "unauto_view",      parse_unstailq,         UL &AutoViewList },
-  { "unhdr_order",      parse_unlist,           UL &HeaderOrderList },
+  { "unhdr_order",      parse_unstailq,         UL &HeaderOrderList },
   { "unhook",           mutt_parse_unhook,      0 },
   { "unignore",         parse_unignore,         0 },
   { "unlists",          parse_unlists,          0 },

list.h
@@ -23,6 +23,8 @@
 #ifndef _MUTT_LIST_H
 #define _MUTT_LIST_H
 
+#include <string.h>
+
 /**
  * struct List - Singly-linked List type
  */
@@ -90,6 +92,17 @@ static inline void mutt_stailq_free(struct STailQHead *h)
       np = next;
   }
   STAILQ_INIT(h);
+}
+
+static inline bool mutt_stailq_match(const char *s, struct STailQHead *h)
+{
+  struct STailQNode *np;
+  STAILQ_FOREACH(np, h, entries)
+  {
+    if (*np->data == '*' || mutt_strncasecmp(s, np->data, strlen(np->data)) == 0)
+      return true;
+  }
+  return false;
 }
 
 #endif /* _MUTT_LIST_H */

muttlib.c
@@ -379,7 +379,7 @@ bool mutt_matches_list(const char *s, struct List *t)
  */
 int mutt_matches_ignore(const char *s)
 {
-  return mutt_matches_list(s, Ignore) && !mutt_matches_list(s, UnIgnore);
+  return mutt_stailq_match(s, &Ignore) && !mutt_stailq_match(s, &UnIgnore);
 }
 
 char *mutt_expand_path(char *s, size_t slen)

commit f7858e5bc326dfca643187d0b04cbd8ac59e3ac5

Fix bug: hdr_order did not sort last header correctly
The algorithm to sort headers by the order given in hdr_order sorted the
last mentioned header in hdr_order at the same level as unmentioned
headers, i.e. not at all.

The bug was introduced in d9ff90dced82e7328dff4abc0918ee0020af8d0a.

The algorithm puts headers in buckets labeled by their priority (0
= highest, n = lowest).

The problem is that the "priority counter" (which starts at 0) `x` is
first incremented and then the header is assigned to priority `x`.
Thus, the buckets 0 to n become 1 to n+1 and the header mentioned last
in hdr_order (number n) was sorted in the bucket n+1 together with all
other headers.

The fix is simple: First sort the header in the bucket, then increase
the priority counter.

copy.c
@@ -310,7 +310,6 @@ int mutt_copy_hdr(FILE *fp_in, FILE *fp_out, LOFF_T off_start, LOFF_T off_end,
 
         STAILQ_FOREACH(np, &HeaderOrderList, entries)
         {
-          x++;
           size_t hdr_order_len = mutt_str_len(np->data);
           if (mutt_istrn_equal(buf, np->data, hdr_order_len))
           {
@@ -321,6 +320,7 @@ int mutt_copy_hdr(FILE *fp_in, FILE *fp_out, LOFF_T off_start, LOFF_T off_end,
             }
             mutt_debug(LL_DEBUG2, "Reorder: %s matches %s", np->data, buf);
           }
+          x++;
         }
         if (match != -1)
           x = match;