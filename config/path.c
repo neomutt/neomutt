594614230f51d816b9c3c2a22bb4d9580bfd83c0
Dennis Schön <mail@dennis-schoen.de>
2023-07-10 13:45:03 +0200

    change path_tidy() to use a buffer internally

--- config/path.c
+++ config/path.c
@@ -58,13 +58,16 @@ static char *path_tidy(const char *path, bool is_dir)
   if (!path || (*path == '\0'))
     return NULL;
 
-  char buf[PATH_MAX] = { 0 };
-  mutt_str_copy(buf, path, sizeof(buf));
+  struct Buffer *buf = buf_pool_get();
+  buf_strcpy(buf, path);
 
-  mutt_path_tilde(buf, sizeof(buf), HomeDir);
-  mutt_path_tidy(buf, is_dir);
+  mutt_path_tilde(buf->data, buf->dsize, HomeDir);
+  mutt_path_tidy(buf->data, is_dir);
 
-  return mutt_str_dup(buf);
+  char *tidy_path = buf_strdup(buf);
+  buf_pool_release(&buf);
+
+  return tidy_path;
 }
 
 /**
