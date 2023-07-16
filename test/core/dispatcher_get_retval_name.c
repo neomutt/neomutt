059c9363a1cd532da3e1526b290dcb9d7699f0a1
Richard Russon <rich@flatcap.org>
2023-07-16 20:45:52 +0100

    test: core coverage

---
+++ test/core/dispatcher_get_retval_name.c
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * Test code for dispatcher_get_retval_name()
+ *
+ * @authors
+ * Copyright (C) 2023 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define TEST_NO_MAIN
+#include "config.h"
+#include "acutest.h"
+#include <stdio.h>
+#include "mutt/lib.h"
+#include "config/lib.h"
+#include "core/lib.h"
+#include "test_common.h"
+
+void test_dispatcher_get_retval_name(void)
+{
+  // const char *dispatcher_get_retval_name(int rv);
+
+  {
+    const char *name = dispatcher_get_retval_name(-99);
+    TEST_CHECK_STR_EQ(name, "");
+  }
+
+  {
+    const char *name = dispatcher_get_retval_name(FR_ERROR);
+    TEST_CHECK_STR_EQ(name, "error");
+  }
+}
