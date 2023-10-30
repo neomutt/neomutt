c22d20382d73dd8900b2268740e6034f2ed06daf
Dennis Schön <mail@dennis-schoen.de>
2023-10-30 16:42:39 +0100

    remove unused mutt_path_pretty()

--- mutt/path.h
+++ mutt/path.h
@@ -36,7 +36,6 @@ char *      mutt_path_dirname(const char *path);
 char *      mutt_path_escape(const char *src);
 const char *mutt_path_getcwd(struct Buffer *cwd);
 bool        mutt_path_parent(struct Buffer *path);
-bool        mutt_path_pretty(struct Buffer *path, const char *homedir, bool is_dir);
 size_t      mutt_path_realpath(struct Buffer *path);
 bool        mutt_path_tidy(struct Buffer *path, bool is_dir);
 bool        mutt_path_tidy_dotdot(char *buf);
