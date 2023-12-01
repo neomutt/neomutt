05f6e10fa2f8fda1167224ba534863e08d0d2810
Dennis Schön <mail@dennis-schoen.de>
2023-12-01 12:39:54 +0100

    add github action to check for unused functions
    Use xunused to search the code for unused functions.

---
+++ .github/unused.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+EXIT_CODE=0
+IGNORELIST=("progress_set_size" "mutt_socket_buffer_readln_d")
+
+while IFS= read -r line; do
+    if ! [[ $line =~ "is unused" ]]; then
+        continue
+    fi
+    read -r func def <<<$(echo $line | sed -rn "s/^(.*:[[:digit:]]+):.*Function '(.*)'.*$/\2 \1/p")
+    for i in "${IGNORELIST[@]}"; do
+        if [[ $i == "$func" ]]; then
+            continue 2
+        fi
+    done
+    printf '%s (%s)\n' "$func" "$def"
+    EXIT_CODE=1
+done
+
+exit $EXIT_CODE
