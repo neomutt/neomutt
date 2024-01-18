46ee04b57597a06023c5fe1314f6f253eda7f7e3
Richard Russon <rich@flatcap.org>
2024-01-18 18:01:31 +0000

    bump up test coverage

--- test/buffer/buf_inline_replace.c
+++ test/buffer/buf_inline_replace.c
@@ -29,8 +29,8 @@
 struct InlineReplaceTest
 {
   const char *initial;
-  int pos;
-  int len;
+  size_t pos;
+  size_t len;
   const char *replace;
   const char *expected;
 };
@@ -40,26 +40,59 @@ void test_buf_inline_replace(void)
   // void buf_inline_replace(struct Buffer *buf, size_t pos, size_t len, const char *str);
 
   // clang-format off
-  struct InlineReplaceTest replace_tests[] =
+  static const struct InlineReplaceTest replace_tests[] =
   {
     { NULL,         0, 2, "apple",     ""                },
     { "apple",      0, 2, NULL,        "apple"           },
+
     { "XXXXbanana", 0, 4, "",          "banana"          },
     { "XXXXbanana", 0, 4, "OO",        "OObanana"        },
     { "XXXXbanana", 0, 4, "OOOO",      "OOOObanana"      },
     { "XXXXbanana", 0, 4, "OOOOOO",    "OOOOOObanana"    },
     { "XXXXbanana", 0, 4, "OOOOOOO",   "OOOOOOObanana"   },
     { "XXXXbanana", 0, 4, "OOOOOOOO",  "OOOOOOOObanana"  },
-    { "XXXXbanana", 0, 4, "OOOOOOOOO", "OOOOOOOOObanana" },
-    { "11XX222",    2, 2, "YYYY",      "11YYYY222"       },
-    { "11XX222",    2, 2, "",          "11222"           },
+
+    { "banXXXXana", 3, 4, "",          "banana"          },
+    { "banXXXXana", 3, 4, "OO",        "banOOana"        },
+    { "banXXXXana", 3, 4, "OOOO",      "banOOOOana"      },
+    { "banXXXXana", 3, 4, "OOOOOO",    "banOOOOOOana"    },
+    { "banXXXXana", 3, 4, "OOOOOOO",   "banOOOOOOOana"   },
+    { "banXXXXana", 3, 4, "OOOOOOOO",  "banOOOOOOOOana"  },
+
+    { "bananaXXXX", 6, 4, "",          "banana"          },
+    { "bananaXXXX", 6, 4, "OO",        "bananaOO"        },
+    { "bananaXXXX", 6, 4, "OOOO",      "bananaOOOO"      },
+    { "bananaXXXX", 6, 4, "OOOOOO",    "bananaOOOOOO"    },
+    { "bananaXXXX", 6, 4, "OOOOOOO",   "bananaOOOOOOO"   },
+    { "bananaXXXX", 6, 4, "OOOOOOOO",  "bananaOOOOOOOO"  },
+
+    { "banana",     0, 0, "",          "banana"          },
+    { "banana",     0, 0, "OO",        "OObanana"        },
+    { "banana",     0, 0, "OOOO",      "OOOObanana"      },
+    { "banana",     0, 0, "OOOOOO",    "OOOOOObanana"    },
+    { "banana",     0, 0, "OOOOOOO",   "OOOOOOObanana"   },
+    { "banana",     0, 0, "OOOOOOOO",  "OOOOOOOObanana"  },
+
+    { "banana",     3, 0, "",          "banana"          },
+    { "banana",     3, 0, "OO",        "banOOana"        },
+    { "banana",     3, 0, "OOOO",      "banOOOOana"      },
+    { "banana",     3, 0, "OOOOOO",    "banOOOOOOana"    },
+    { "banana",     3, 0, "OOOOOOO",   "banOOOOOOOana"   },
+    { "banana",     3, 0, "OOOOOOOO",  "banOOOOOOOOana"  },
+
+    { "banana",     6, 0, "",          "banana"          },
+    { "banana",     6, 0, "OO",        "bananaOO"        },
+    { "banana",     6, 0, "OOOO",      "bananaOOOO"      },
+    { "banana",     6, 0, "OOOOOO",    "bananaOOOOOO"    },
+    { "banana",     6, 0, "OOOOOOO",   "bananaOOOOOOO"   },
+    { "banana",     6, 0, "OOOOOOOO",  "bananaOOOOOOOO"  },
   };
   // clang-format on
 
   {
     for (size_t i = 0; i < mutt_array_size(replace_tests); i++)
     {
-      struct InlineReplaceTest *t = &replace_tests[i];
+      const struct InlineReplaceTest *t = &replace_tests[i];
 
       struct Buffer *buf = NULL;
       if (t->initial)
