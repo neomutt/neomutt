2fd69fe17c3dc6ff8ffa4eed014305c8bdc1e214
Richard Russon <rich@flatcap.org>
2024-01-27 12:56:39 +0000

    docs: tidy cli output
    - unwrap suggestions
    - deindent license

--- version.c
+++ version.c
@@ -81,24 +81,24 @@ static const char *Copyright =
     "Copyright (C) 2020-2023 наб <nabijaczleweli@nabijaczleweli.xyz>\n";
 
 /// CLI Version: Thanks
-static const char *Thanks = N_("Many others not mentioned here contributed code, fixes,\n"
-                               "and suggestions.\n");
+static const char *Thanks = N_(
+    "Many others not mentioned here contributed code, fixes and suggestions.\n");
 
 /// CLI Version: License
 static const char *License = N_(
-    "    This program is free software; you can redistribute it and/or modify\n"
-    "    it under the terms of the GNU General Public License as published by\n"
-    "    the Free Software Foundation; either version 2 of the License, or\n"
-    "    (at your option) any later version.\n"
+    "This program is free software; you can redistribute it and/or modify\n"
+    "it under the terms of the GNU General Public License as published by\n"
+    "the Free Software Foundation; either version 2 of the License, or\n"
+    "(at your option) any later version.\n"
     "\n"
-    "    This program is distributed in the hope that it will be useful,\n"
-    "    but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-    "    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-    "    GNU General Public License for more details.\n"
+    "This program is distributed in the hope that it will be useful,\n"
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+    "GNU General Public License for more details.\n"
     "\n"
-    "    You should have received a copy of the GNU General Public License\n"
-    "    along with this program; if not, write to the Free Software\n"
-    "    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.\n");
+    "You should have received a copy of the GNU General Public License\n"
+    "along with this program; if not, write to the Free Software\n"
+    "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.\n");
 
 /// CLI Version: How to reach the NeoMutt Team
 static const char *ReachingUs = N_("To learn more about NeoMutt, visit: https://neomutt.org\n"
