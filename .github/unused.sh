e4be7a7e5c1b7a1e2c16cf681bbd12829b4225da
Richard Russon <rich@flatcap.org>
2024-03-25 15:11:24 +0000

    unused: ignore some expando debug functions
    Ignore some functions used by the tests:
    - name_expando_domain
    - name_expando_node_type
    - name_expando_pad_type
    - name_expando_uid
    - name_format_justify
    
    Add some comments
    Run shellcheck

--- .github/unused.sh
+++ .github/unused.sh
@@ -4,17 +4,27 @@ set -euo pipefail
 
 EXIT_CODE=0
 IGNORELIST=(
-  "log_multiline_full"
-  "mutt_socket_buffer_readln_d"
-  "mutt_str_upper"
-  "progress_set_size"
+    # Used by macros
+    "log_multiline_full"
+    "mutt_socket_buffer_readln_d"
+
+    # Used by tests
+    "mutt_str_upper"
+    "name_expando_domain"
+    "name_expando_node_type"
+    "name_expando_pad_type"
+    "name_expando_uid"
+    "name_format_justify"
+
+    # Will be used by future code
+    "progress_set_size"
 )
 
 while IFS= read -r line; do
     if ! [[ $line =~ "is unused" ]]; then
         continue
     fi
-    read -r func def <<<$(echo $line | sed -rn "s/^(.*:[[:digit:]]+):.*Function '(.*)'.*$/\2 \1/p")
+    read -r func def <<< "$(echo "$line" | sed -rn "s/^(.*:[[:digit:]]+):.*Function '(.*)'.*$/\2 \1/p")"
     for i in "${IGNORELIST[@]}"; do
         if [[ $i == "$func" ]]; then
             continue 2
