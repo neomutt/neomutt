d0743da2e0c82d76b94ae8ed85fd0d80c6ec5d8b
reido2012 <oreid52@googlemail.com>
2024-03-29 16:40:17 +0000

    Add missing semicolon

--- browser/dlg_browser.c
+++ browser/dlg_browser.c
@@ -575,7 +575,8 @@ void folder_s(const struct ExpandoNode *node, void *data, MuttFormatFlags flags,
 {
   const struct Folder *folder = data;
 
-  buf_reset(buf) mutt_str_pretty_size(buf, folder->ff->size);
+  buf_reset(buf);
+  mutt_str_pretty_size(buf, folder->ff->size);
 }
 
 /**
