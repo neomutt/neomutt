c3c03954624c1ca94caa724a6c93687fa9e85874
Richard Russon <rich@flatcap.org>
2024-03-29 01:12:50 +0000

    expando: fix leak on parse

--- expando/expando.c
+++ expando/expando.c
@@ -78,19 +78,21 @@ struct Expando *expando_parse(const char *str, const struct ExpandoDefinition *d
   if (!str || !defs)
     return NULL;
 
+  struct Expando *exp = expando_new(str);
+
   struct ExpandoParseError error = { 0 };
   struct ExpandoNode *root = NULL;
 
-  node_tree_parse(&root, str, defs, &error);
+  node_tree_parse(&root, exp->string, defs, &error);
 
   if (error.position)
   {
     buf_strcpy(err, error.message);
     node_tree_free(&root);
+    expando_free(&exp);
     return NULL;
   }
 
-  struct Expando *exp = expando_new(str);
   exp->tree = root;
   return exp;
 }
