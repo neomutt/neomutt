46d4aac54d1348b921c7baf82008f33e175cd469
Carlos Henrique Lima Melara <charlesmelara@riseup.net>
2024-04-21 22:54:00 -0300

    mutt: fix copyright boilerplate in base64.h and filter.h

--- mutt/base64.h
+++ mutt/base64.h
@@ -3,11 +3,12 @@
  * Conversion to/from base64 encoding
  *
  * @authors
- * @copyright
  * Copyright (C) 2017-2018 Richard Russon <rich@flatcap.org>
- * Foundation, either version 2 of the License, or (at your option) any later
- * the terms of the GNU General Public License as published by the Free Software
+ *
+ * @copyright
  * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
  * version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
--- mutt/filter.h
+++ mutt/filter.h
@@ -3,11 +3,12 @@
  * Pass files through external commands (filters)
  *
  * @authors
- * @copyright
  * Copyright (C) 2020 Richard Russon <rich@flatcap.org>
- * Foundation, either version 2 of the License, or (at your option) any later
- * the terms of the GNU General Public License as published by the Free Software
+ *
+ * @copyright
  * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
  * version.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
