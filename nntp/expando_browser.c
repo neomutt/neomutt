e75d84489e86bca48dba401d4220cf0cccf118dc
Richard Russon <rich@flatcap.org>
2024-11-30 01:50:06 +0000

    expando: rename nntp browser callbacks

--- nntp/expando_browser.c
+++ nntp/expando_browser.c
@@ -40,30 +40,10 @@
 #include "mdata.h"
 
 /**
- * group_index_a_num - NNTP: Alert for new mail - Implements ::get_number_t - @ingroup expando_get_number_api
+ * group_index_description - NNTP: Description - Implements ::get_string_t - @ingroup expando_get_string_api
  */
-static long group_index_a_num(const struct ExpandoNode *node, void *data, MuttFormatFlags flags)
-{
-  const struct Folder *folder = data;
-
-  return folder->ff->notify_user;
-}
-
-/**
- * group_index_C_num - NNTP: Index number - Implements ::get_number_t - @ingroup expando_get_number_api
- */
-static long group_index_C_num(const struct ExpandoNode *node, void *data, MuttFormatFlags flags)
-{
-  const struct Folder *folder = data;
-
-  return folder->num + 1;
-}
-
-/**
- * group_index_d - NNTP: Description - Implements ::get_string_t - @ingroup expando_get_string_api
- */
-static void group_index_d(const struct ExpandoNode *node, void *data,
-                          MuttFormatFlags flags, struct Buffer *buf)
+static void group_index_description(const struct ExpandoNode *node, void *data,
+                                    MuttFormatFlags flags, struct Buffer *buf)
 {
   const struct Folder *folder = data;
 
@@ -86,22 +66,10 @@ static void group_index_d(const struct ExpandoNode *node, void *data,
 }
 
 /**
- * group_index_f - NNTP: Newsgroup name - Implements ::get_string_t - @ingroup expando_get_string_api
+ * group_index_flags - NNTP: Moderated flag - Implements ::get_string_t - @ingroup expando_get_string_api
  */
-static void group_index_f(const struct ExpandoNode *node, void *data,
-                          MuttFormatFlags flags, struct Buffer *buf)
-{
-  const struct Folder *folder = data;
-
-  const char *s = folder->ff->name;
-  buf_strcpy(buf, s);
-}
-
-/**
- * group_index_M - NNTP: Moderated flag - Implements ::get_string_t - @ingroup expando_get_string_api
- */
-static void group_index_M(const struct ExpandoNode *node, void *data,
-                          MuttFormatFlags flags, struct Buffer *buf)
+static void group_index_flags(const struct ExpandoNode *node, void *data,
+                              MuttFormatFlags flags, struct Buffer *buf)
 {
   const struct Folder *folder = data;
 
@@ -120,29 +88,10 @@ static void group_index_M(const struct ExpandoNode *node, void *data,
 }
 
 /**
- * group_index_n_num - NNTP: Number of new articles - Implements ::get_number_t - @ingroup expando_get_number_api
+ * group_index_flags2 - NNTP: New flag - Implements ::get_string_t - @ingroup expando_get_string_api
  */
-static long group_index_n_num(const struct ExpandoNode *node, void *data, MuttFormatFlags flags)
-{
-  const struct Folder *folder = data;
-  const struct NntpMboxData *nd = folder->ff->nd;
-
-  const bool c_mark_old = cs_subset_bool(NeoMutt->sub, "mark_old");
-
-  if (c_mark_old && (nd->last_cached >= nd->first_message) &&
-      (nd->last_cached <= nd->last_message))
-  {
-    return nd->last_message - nd->last_cached;
-  }
-
-  return nd->unread;
-}
-
-/**
- * group_index_N - NNTP: New flag - Implements ::get_string_t - @ingroup expando_get_string_api
- */
-static void group_index_N(const struct ExpandoNode *node, void *data,
-                          MuttFormatFlags flags, struct Buffer *buf)
+static void group_index_flags2(const struct ExpandoNode *node, void *data,
+                               MuttFormatFlags flags, struct Buffer *buf)
 {
   const struct Folder *folder = data;
 
@@ -161,9 +110,61 @@ static void group_index_N(const struct ExpandoNode *node, void *data,
 }
 
 /**
- * group_index_p_num - NNTP: Poll for new mail - Implements ::get_number_t - @ingroup expando_get_number_api
+ * group_index_newsgroup - NNTP: Newsgroup name - Implements ::get_string_t - @ingroup expando_get_string_api
  */
-static long group_index_p_num(const struct ExpandoNode *node, void *data, MuttFormatFlags flags)
+static void group_index_newsgroup(const struct ExpandoNode *node, void *data,
+                                  MuttFormatFlags flags, struct Buffer *buf)
+{
+  const struct Folder *folder = data;
+
+  const char *s = folder->ff->name;
+  buf_strcpy(buf, s);
+}
+
+/**
+ * group_index_notify_num - NNTP: Alert for new mail - Implements ::get_number_t - @ingroup expando_get_number_api
+ */
+static long group_index_notify_num(const struct ExpandoNode *node, void *data, MuttFormatFlags flags)
+{
+  const struct Folder *folder = data;
+
+  return folder->ff->notify_user;
+}
+
+/**
+ * group_index_new_count_num - NNTP: Number of new articles - Implements ::get_number_t - @ingroup expando_get_number_api
+ */
+static long group_index_new_count_num(const struct ExpandoNode *node,
+                                      void *data, MuttFormatFlags flags)
+{
+  const struct Folder *folder = data;
+  const struct NntpMboxData *nd = folder->ff->nd;
+
+  const bool c_mark_old = cs_subset_bool(NeoMutt->sub, "mark_old");
+
+  if (c_mark_old && (nd->last_cached >= nd->first_message) &&
+      (nd->last_cached <= nd->last_message))
+  {
+    return nd->last_message - nd->last_cached;
+  }
+
+  return nd->unread;
+}
+
+/**
+ * group_index_number_num - NNTP: Index number - Implements ::get_number_t - @ingroup expando_get_number_api
+ */
+static long group_index_number_num(const struct ExpandoNode *node, void *data, MuttFormatFlags flags)
+{
+  const struct Folder *folder = data;
+
+  return folder->num + 1;
+}
+
+/**
+ * group_index_poll_num - NNTP: Poll for new mail - Implements ::get_number_t - @ingroup expando_get_number_api
+ */
+static long group_index_poll_num(const struct ExpandoNode *node, void *data, MuttFormatFlags flags)
 {
   const struct Folder *folder = data;
 
@@ -171,9 +172,10 @@ static long group_index_p_num(const struct ExpandoNode *node, void *data, MuttFo
 }
 
 /**
- * group_index_s_num - NNTP: Number of unread articles - Implements ::get_number_t - @ingroup expando_get_number_api
+ * group_index_unread_count_num - NNTP: Number of unread articles - Implements ::get_number_t - @ingroup expando_get_number_api
  */
-static long group_index_s_num(const struct ExpandoNode *node, void *data, MuttFormatFlags flags)
+static long group_index_unread_count_num(const struct ExpandoNode *node,
+                                         void *data, MuttFormatFlags flags)
 {
   const struct Folder *folder = data;
 
@@ -187,15 +189,15 @@ static long group_index_s_num(const struct ExpandoNode *node, void *data, MuttFo
  */
 const struct ExpandoRenderCallback GroupIndexRenderCallbacks[] = {
   // clang-format off
-  { ED_FOLDER, ED_FOL_NOTIFY,       NULL,          group_index_a_num },
-  { ED_FOLDER, ED_FOL_NUMBER,       NULL,          group_index_C_num },
-  { ED_FOLDER, ED_FOL_DESCRIPTION,  group_index_d, NULL },
-  { ED_FOLDER, ED_FOL_NEWSGROUP,    group_index_f, NULL },
-  { ED_FOLDER, ED_FOL_FLAGS,        group_index_M, NULL },
-  { ED_FOLDER, ED_FOL_FLAGS2,       group_index_N, NULL },
-  { ED_FOLDER, ED_FOL_NEW_COUNT,    NULL,          group_index_n_num },
-  { ED_FOLDER, ED_FOL_POLL,         NULL,          group_index_p_num },
-  { ED_FOLDER, ED_FOL_UNREAD_COUNT, NULL,          group_index_s_num },
+  { ED_FOLDER, ED_FOL_DESCRIPTION,  group_index_description, NULL },
+  { ED_FOLDER, ED_FOL_FLAGS,        group_index_flags,       NULL },
+  { ED_FOLDER, ED_FOL_FLAGS2,       group_index_flags2,      NULL },
+  { ED_FOLDER, ED_FOL_NEWSGROUP,    group_index_newsgroup,   NULL },
+  { ED_FOLDER, ED_FOL_NEW_COUNT,    NULL,                    group_index_new_count_num },
+  { ED_FOLDER, ED_FOL_NOTIFY,       NULL,                    group_index_notify_num },
+  { ED_FOLDER, ED_FOL_NUMBER,       NULL,                    group_index_number_num },
+  { ED_FOLDER, ED_FOL_POLL,         NULL,                    group_index_poll_num },
+  { ED_FOLDER, ED_FOL_UNREAD_COUNT, NULL,                    group_index_unread_count_num },
   { -1, -1, NULL, NULL },
   // clang-format on
 };
