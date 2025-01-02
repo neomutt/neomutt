62806dcf4148179e4b29aa5699192813a07ee61b
Richard Russon <rich@flatcap.org>
2025-01-02 21:10:46 +0000

    factor out mutt_window_mvaddstr()
    mutt_window_mvaddstr() was only used in one place.
    
    Replace it with the equivalent:
    - mutt_window_move()
    - mutt_window_addstr()

--- gui/mutt_window.h
+++ gui/mutt_window.h
@@ -258,7 +258,6 @@ void mutt_window_clearline(struct MuttWindow *win, int row);
 void mutt_window_clear    (struct MuttWindow *win);
 void mutt_window_clrtoeol (struct MuttWindow *win);
 int  mutt_window_move     (struct MuttWindow *win, int col, int row);
-int  mutt_window_mvaddstr (struct MuttWindow *win, int col, int row, const char *str);
 int  mutt_window_mvprintw (struct MuttWindow *win, int col, int row, const char *fmt, ...)
                             __attribute__((__format__(__printf__, 4, 5)));
 int  mutt_window_printf   (struct MuttWindow *win, const char *format, ...)
