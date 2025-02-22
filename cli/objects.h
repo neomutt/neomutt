neomutt/upstream

cli: add help sections
Add an optional parameter to '-h' to provide detailed help on that topic.
e.g.
    neomutt -h config

cli/objects.h
@@ -29,6 +29,20 @@
 ARRAY_HEAD(StringArray, char *);
 
 /**
+ * enum HelpMode - Show detailed help
+ */
+enum HelpMode
+{
+  HM_NONE,         ///< No extra help
+  HM_SHARED,       ///< Help about shared config options
+  HM_HELP,         ///< Help about help
+  HM_INFO,         ///< Help about info options
+  HM_SEND,         ///< Help about sending email options
+  HM_GUI,          ///< Help about starting the gui options
+  HM_ALL,          ///< Help about all options
+};
+
+/**
  * struct CliShared - Shared Command Line options
  */
 struct CliShared
@@ -54,6 +68,8 @@ struct CliHelp
   bool help;                        ///< `-h`  Print help
   bool version;                     ///< `-v`  Print version
   bool license;                     ///< `-vv` Print license
+
+  enum HelpMode mode;               ///< Display detailed help
 };
 
 /**

cli/parse.c
@@ -33,6 +33,28 @@
 #include "objects.h"
 
 /**
+ * check_help_mode - Check for help mode
+ * @param mode String to check
+ * @retval enum #HelpMode, e.g. HM_CONFIG
+ */
+int check_help_mode(const char *mode)
+{
+  if (mutt_istr_equal(mode, "shared"))
+    return HM_SHARED;
+  if (mutt_istr_equal(mode, "help"))
+    return HM_HELP;
+  if (mutt_istr_equal(mode, "info"))
+    return HM_INFO;
+  if (mutt_istr_equal(mode, "send"))
+    return HM_SEND;
+  if (mutt_istr_equal(mode, "gui"))
+    return HM_GUI;
+  if (mutt_istr_equal(mode, "all"))
+    return HM_ALL;
+  return 0;
+}
+
+/**
  * mop_up - Eat multiple arguments
  * @param[in]  argc  Size of argument array
  * @param[in]  argv  Array of argument strings
@@ -133,6 +155,12 @@ bool cli_parse(int argc, char **argv, struct CommandLine *cli)
       // Help
       case 'h': // help
       {
+        if (optind < argc)
+        {
+          cli->help.mode = check_help_mode(argv[optind]);
+          if (cli->help.mode != HM_NONE)
+            optind++;
+        }
         cli->help.help = true;
         cli->help.is_set = true;
         break;

test/cli/cli_parse.c
@@ -72,6 +72,8 @@ static void serialise_help(struct CliHelp *help, struct Buffer *res)
   serialise_bool(help->version, res);
   serialise_bool(help->license, res);
 
+  buf_addch(res, '0' + help->mode);
+
   buf_addstr(res, ")");
 }
 
@@ -239,12 +241,12 @@ void test_cli_parse(void)
       { "",                      "" },
 
       // Help
-      { "-h",                    "H(YNN)" },
-      { "-v",                    "H(NYN)" },
-      { "-h -v",                 "H(YYN)" },
-      { "-v -v",                 "H(NYY)" },
-      { "-vv",                   "H(NYY)" },
-      { "-vhv",                  "H(YYY)" },
+      { "-h",                    "H(YNN0)" },
+      { "-v",                    "H(NYN0)" },
+      { "-h -v",                 "H(YYN0)" },
+      { "-v -v",                 "H(NYY0)" },
+      { "-vv",                   "H(NYY0)" },
+      { "-vhv",                  "H(YYY0)" },
 
       // Shared
       { "-n",                    "X(:{}Y:{}:-:-:-)" },
@@ -319,6 +321,15 @@ void test_cli_parse(void)
       { "apple -- banana",                  "S(NN:{}:{}:{}:{apple,banana}:-:-:-)" },
       { "-A apple banana -- cherry",        "I(NNNN:{apple,banana}:{})S(NN:{}:{}:{}:{cherry}:-:-:-)" },
       { "-Q apple banana -- cherry damson", "I(NNNN:{}:{apple,banana})S(NN:{}:{}:{}:{cherry,damson}:-:-:-)" },
+
+      // Help modes
+      { "-h",                    "H(YNN0)" },
+      { "-h shared",             "H(YNN1)" },
+      { "-h help",               "H(YNN2)" },
+      { "-h info",               "H(YNN3)" },
+      { "-h send",               "H(YNN4)" },
+      { "-h gui",                "H(YNN5)" },
+      { "-h all",                "H(YNN6)" },
       // clang-format on
     };
 
@@ -369,7 +380,7 @@ void test_cli_parse(void)
       TEST_CHECK(rc == false);
 
       serialise_cli(cli, res);
-      TEST_CHECK_STR_EQ(buf_string(res), "H(YNN)");
+      TEST_CHECK_STR_EQ(buf_string(res), "H(YNN0)");
 
       args_clear(&sa);
       command_line_free(&cli);

neomutt/neomutt

cli/objects.h
@@ -29,6 +29,20 @@
 ARRAY_HEAD(StringArray, char *);
 
 /**
+ * enum HelpMode - Show detailed help
+ */
+enum HelpMode
+{
+  HM_NONE,         ///< No extra help
+  HM_SHARED,       ///< Help about shared config options
+  HM_HELP,         ///< Help about help
+  HM_INFO,         ///< Help about info options
+  HM_SEND,         ///< Help about sending email options
+  HM_TUI,          ///< Help about starting the tui options
+  HM_ALL,          ///< Help about all options
+};
+
+/**
  * struct CliShared - Shared Command Line options
  */
 struct CliShared
@@ -54,6 +68,8 @@ struct CliHelp
   bool help;                        ///< `-h`  Print help
   bool version;                     ///< `-v`  Print version
   bool license;                     ///< `-vv` Print license
+
+  enum HelpMode mode;               ///< Display detailed help
 };
 
 /**

cli/parse.c
@@ -34,6 +34,28 @@
 #include "objects.h"
 
 /**
+ * check_help_mode - Check for help mode
+ * @param mode String to check
+ * @retval enum #HelpMode, e.g. HM_CONFIG
+ */
+int check_help_mode(const char *mode)
+{
+  if (mutt_istr_equal(mode, "shared"))
+    return HM_SHARED;
+  if (mutt_istr_equal(mode, "help"))
+    return HM_HELP;
+  if (mutt_istr_equal(mode, "info"))
+    return HM_INFO;
+  if (mutt_istr_equal(mode, "send"))
+    return HM_SEND;
+  if (mutt_istr_equal(mode, "tui"))
+    return HM_TUI;
+  if (mutt_istr_equal(mode, "all"))
+    return HM_ALL;
+  return 0;
+}
+
+/**
  * mop_up - Eat multiple arguments
  * @param[in]  argc  Size of argument array
  * @param[in]  argv  Array of argument strings
@@ -141,6 +163,12 @@ bool cli_parse(int argc, char **argv, struct CommandLine *cli)
       // Help
       case 'h': // help
       {
+        if (optind < argc)
+        {
+          cli->help.mode = check_help_mode(argv[optind]);
+          if (cli->help.mode != HM_NONE)
+            optind++;
+        }
         cli->help.help = true;
         cli->help.is_set = true;
         break;

test/cli/cli_parse.c
@@ -72,6 +72,8 @@ static void serialise_help(struct CliHelp *help, struct Buffer *res)
   serialise_bool(help->version, res);
   serialise_bool(help->license, res);
 
+  buf_addch(res, '0' + help->mode);
+
   buf_addstr(res, ")");
 }
 
@@ -239,12 +241,12 @@ void test_cli_parse(void)
       { "",                      "" },
 
       // Help
-      { "-h",                    "H(YNN)" },
-      { "-v",                    "H(NYN)" },
-      { "-h -v",                 "H(YYN)" },
-      { "-v -v",                 "H(NYY)" },
-      { "-vv",                   "H(NYY)" },
-      { "-vhv",                  "H(YYY)" },
+      { "-h",                    "H(YNN0)" },
+      { "-v",                    "H(NYN0)" },
+      { "-h -v",                 "H(YYN0)" },
+      { "-v -v",                 "H(NYY0)" },
+      { "-vv",                   "H(NYY0)" },
+      { "-vhv",                  "H(YYY0)" },
 
       // Shared
       { "-n",                    "X(:{}Y:{}:-:-:-)" },
@@ -319,6 +321,15 @@ void test_cli_parse(void)
       { "apple -- banana",                  "S(NN:{}:{}:{}:{apple,banana}:-:-:-)" },
       { "-A apple banana -- cherry",        "I(NNNN:{apple,banana}:{})S(NN:{}:{}:{}:{cherry}:-:-:-)" },
       { "-Q apple banana -- cherry damson", "I(NNNN:{}:{apple,banana})S(NN:{}:{}:{}:{cherry,damson}:-:-:-)" },
+
+      // Help modes
+      { "-h",                    "H(YNN0)" },
+      { "-h shared",             "H(YNN1)" },
+      { "-h help",               "H(YNN2)" },
+      { "-h info",               "H(YNN3)" },
+      { "-h send",               "H(YNN4)" },
+      { "-h tui",                "H(YNN5)" },
+      { "-h all",                "H(YNN6)" },
       // clang-format on
     };
 
@@ -369,7 +380,7 @@ void test_cli_parse(void)
       TEST_CHECK(rc == false);
 
       serialise_cli(cli, res);
-      TEST_CHECK_STR_EQ(buf_string(res), "H(YNN)");
+      TEST_CHECK_STR_EQ(buf_string(res), "H(YNN0)");
 
       args_clear(&sa);
       command_line_free(&cli);