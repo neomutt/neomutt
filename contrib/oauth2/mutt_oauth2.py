ae5074af57c793a68933f2e4c22284c7b1c4f636
Mateus Eto <mateus.etto@gmail.com>
2025-03-16 21:31:26 +0900

    Remove unused import from mutt_oauth2.py

--- contrib/oauth2/mutt_oauth2.py
+++ contrib/oauth2/mutt_oauth2.py
@@ -39,7 +39,6 @@ import shlex
 import socket
 import http.server
 import subprocess
-import readline
 
 # The token file must be encrypted because it contains multi-use bearer tokens
 # whose usage does not require additional verification. Specify whichever
