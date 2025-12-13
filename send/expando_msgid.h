12b0345dc2ee9b5efdcdc06074ac70f162e8d620
Richard Russon <rich@flatcap.org>
2025-12-13 22:01:37 +0000

    triv: fix trailing whitespace

--- send/expando_msgid.h
+++ send/expando_msgid.h
@@ -43,18 +43,18 @@ struct MsgIdData
  */
 enum ExpandoDataMsgId
 {
-  ED_MSG_COUNTER = 1,   ///< Step counter looping from ``A'' to ``Z''                          
-  ED_MSG_DAY,           ///< Current day of the month (GMT)                                    
-  ED_MSG_HOSTNAME,      ///< $hostname                                                        
-  ED_MSG_HOUR,          ///< Current hour using a 24-hour clock (GMT)                          
-  ED_MSG_MINUTE,        ///< Current month number (GMT)                                        
-  ED_MSG_MONTH,         ///< Current minute of the hour (GMT)                                  
-  ED_MSG_PID,           ///< PID of the running mutt process                                   
-  ED_MSG_RANDOM_1,      ///< 3 bytes of pseudo-random data encoded in Base64                    
-  ED_MSG_RANDOM_3,      ///< Current second of the minute (GMT)                                
-  ED_MSG_RANDOM_12,     ///< 1 byte of pseudo-random data hex encoded (example: '1b')           
-  ED_MSG_SECOND,        ///< Current year using 4 digits (GMT)                                 
-  ED_MSG_YEAR,          ///< 4 byte timestamp + 8 bytes of pseudo-random data encoded in Base64 
+  ED_MSG_COUNTER = 1,   ///< Step counter looping from 'A' to 'Z'
+  ED_MSG_DAY,           ///< Current day of the month (GMT)
+  ED_MSG_HOSTNAME,      ///< $hostname
+  ED_MSG_HOUR,          ///< Current hour using a 24-hour clock (GMT)
+  ED_MSG_MINUTE,        ///< Current month number (GMT)
+  ED_MSG_MONTH,         ///< Current minute of the hour (GMT)
+  ED_MSG_PID,           ///< PID of the running mutt process
+  ED_MSG_RANDOM_1,      ///< 3 bytes of pseudo-random data encoded in Base64
+  ED_MSG_RANDOM_3,      ///< Current second of the minute (GMT)
+  ED_MSG_RANDOM_12,     ///< 1 byte of pseudo-random data hex encoded (example: '1b')
+  ED_MSG_SECOND,        ///< Current year using 4 digits (GMT)
+  ED_MSG_YEAR,          ///< 4 byte timestamp + 8 bytes of pseudo-random data encoded in Base64
 };
 
 extern const struct ExpandoRenderCallback MsgIdRenderCallbacks[];
