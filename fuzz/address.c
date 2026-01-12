c1a713c8d4a6da956b01ce54715c0d1ae454b8fd
Richard Russon <rich@flatcap.org>
2026-01-12 03:29:36 +0000

    fuzz: fix build

--- fuzz/address.c
+++ fuzz/address.c
@@ -12,6 +12,58 @@
 
 bool StartupComplete = true;
 
+// clang-format off
+extern const struct Module ModuleMain;
+extern const struct Module ModuleAddress;  extern const struct Module ModuleAlias;    extern const struct Module ModuleAttach;   extern const struct Module ModuleAutocrypt;
+extern const struct Module ModuleBcache;   extern const struct Module ModuleBrowser;  extern const struct Module ModuleColor;    extern const struct Module ModuleCommands;
+extern const struct Module ModuleComplete; extern const struct Module ModuleCompmbox; extern const struct Module ModuleCompose;  extern const struct Module ModuleCompress;
+extern const struct Module ModuleConfig;   extern const struct Module ModuleConn;     extern const struct Module ModuleConvert;  extern const struct Module ModuleCore;
+extern const struct Module ModuleEditor;   extern const struct Module ModuleEmail;    extern const struct Module ModuleEnvelope; extern const struct Module ModuleExpando;
+extern const struct Module ModuleGui;      extern const struct Module ModuleHcache;   extern const struct Module ModuleHelpbar;  extern const struct Module ModuleHistory;
+extern const struct Module ModuleHooks;    extern const struct Module ModuleImap;     extern const struct Module ModuleIndex;    extern const struct Module ModuleKey;
+extern const struct Module ModuleLua;      extern const struct Module ModuleMaildir;  extern const struct Module ModuleMbox;     extern const struct Module ModuleMenu;
+extern const struct Module ModuleMh;       extern const struct Module ModuleMutt;     extern const struct Module ModuleNcrypt;   extern const struct Module ModuleNntp;
+extern const struct Module ModuleNotmuch;  extern const struct Module ModulePager;    extern const struct Module ModuleParse;    extern const struct Module ModulePattern;
+extern const struct Module ModulePop;      extern const struct Module ModulePostpone; extern const struct Module ModuleProgress; extern const struct Module ModuleQuestion;
+extern const struct Module ModuleSend;     extern const struct Module ModuleSidebar;  extern const struct Module ModuleStore;
+// clang-format on
+
+/**
+ * Modules - All the library Modules
+ */
+static const struct Module *Modules[] = {
+  // clang-format off
+  &ModuleMain,     &ModuleGui,      // These two have priority
+  &ModuleAddress,  &ModuleAlias,    &ModuleAttach,   &ModuleBcache,   &ModuleBrowser,
+  &ModuleColor,    &ModuleCommands, &ModuleComplete, &ModuleCompmbox, &ModuleCompose,
+  &ModuleConfig,   &ModuleConn,     &ModuleConvert,  &ModuleCore,     &ModuleEditor,
+  &ModuleEmail,    &ModuleEnvelope, &ModuleExpando,  &ModuleHelpbar,  &ModuleHistory,
+  &ModuleHooks,    &ModuleImap,     &ModuleIndex,    &ModuleKey,      &ModuleMaildir,
+  &ModuleMbox,     &ModuleMenu,     &ModuleMh,       &ModuleMutt,     &ModuleNcrypt,
+  &ModuleNntp,     &ModulePager,    &ModuleParse,    &ModulePattern,  &ModulePop,
+  &ModulePostpone, &ModuleProgress, &ModuleQuestion, &ModuleSend,     &ModuleSidebar,
+// clang-format on
+#ifdef USE_AUTOCRYPT
+  &ModuleAutocrypt,
+#endif
+#ifdef USE_HCACHE_COMPRESSION
+  &ModuleCompress,
+#endif
+#ifdef USE_HCACHE
+  &ModuleHcache,
+#endif
+#ifdef USE_LUA
+  &ModuleLua,
+#endif
+#ifdef USE_NOTMUCH
+  &ModuleNotmuch,
+#endif
+#ifdef USE_HCACHE
+  &ModuleStore,
+#endif
+  NULL,
+};
+
 /**
  * log_disp_null - Discard log lines - Implements ::log_dispatcher_t - @ingroup logging_api
  */
@@ -24,9 +76,12 @@ static int log_disp_null(time_t stamp, const char *file, int line, const char *f
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
   MuttLogger = log_disp_null;
-  struct ConfigSet *cs = cs_new(16);
-  NeoMutt = neomutt_new(cs);
-  init_config(cs);
+
+  NeoMutt = neomutt_new();
+  char **tmp_env = MUTT_MEM_CALLOC(2, char *);
+  neomutt_init(NeoMutt, tmp_env, Modules);
+  FREE(&tmp_env);
+
   char file[] = "/tmp/mutt-fuzz";
   FILE *fp = fopen(file, "wb");
   if (!fp)
@@ -44,7 +99,7 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
   email_free(&e);
   mutt_env_free(&env);
   fclose(fp);
+  neomutt_cleanup(NeoMutt);
   neomutt_free(&NeoMutt);
-  cs_free(&cs);
   return 0;
 }
