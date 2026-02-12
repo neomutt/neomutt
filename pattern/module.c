b2cf4d3ced93dfef05b7b6e3e7d622aa8f6e47d5
Richard Russon <rich@flatcap.org>
2026-02-12 16:35:06 +0000

    mod: pattern init/cleanup/data
    Add:
    - Module.init()
    - Module.cleanup()
    - Module.data

--- pattern/module.c
+++ pattern/module.c
@@ -29,10 +29,23 @@
 #include "config.h"
 #include <stdbool.h>
 #include <stddef.h>
+#include "mutt/lib.h"
 #include "config/lib.h"
 #include "core/lib.h"
+#include "module_data.h"
 
 extern struct ConfigDef PatternVars[];
+
+/**
+ * pattern_init - Initialise a Module - Implements Module::init()
+ */
+static bool pattern_init(struct NeoMutt *n)
+{
+  // struct PatternModuleData *md = MUTT_MEM_CALLOC(1, struct PatternModuleData);
+  // neomutt_set_module_data(n, MODULE_ID_PATTERN, md);
+
+  return true;
+}
 
 /**
  * pattern_config_define_variables - Define the Config Variables - Implements Module::config_define_variables()
@@ -43,16 +56,28 @@ static bool pattern_config_define_variables(struct NeoMutt *n, struct ConfigSet 
 }
 
 /**
+ * pattern_cleanup - Clean up a Module - Implements Module::cleanup()
+ */
+static bool pattern_cleanup(struct NeoMutt *n)
+{
+  // struct PatternModuleData *md = neomutt_get_module_data(n, MODULE_ID_PATTERN);
+  // ASSERT(md);
+
+  // FREE(&md);
+  return true;
+}
+
+/**
  * ModulePattern - Module for the Pattern library
  */
 const struct Module ModulePattern = {
   MODULE_ID_PATTERN,
   "pattern",
-  NULL, // init
+  pattern_init,
   NULL, // config_define_types
   pattern_config_define_variables,
   NULL, // commands_register
   NULL, // gui_init
   NULL, // gui_cleanup
-  NULL, // cleanup
+  pattern_cleanup,
 };
