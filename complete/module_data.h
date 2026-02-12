4e8eb2ad7eec4a50241770d24da097841092071c
Richard Russon <rich@flatcap.org>
2026-02-12 16:35:06 +0000

    mod: complete init/cleanup/data
    Add:
    - Module.init()
    - Module.cleanup()
    - Module.data

---
+++ complete/module_data.h
@@ -0,0 +1,34 @@
+/**
+ * @file
+ * Complete private Module data
+ *
+ * @authors
+ * Copyright (C) 2026 Richard Russon <rich@flatcap.org>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MUTT_COMPLETE_MODULE_DATA_H
+#define MUTT_COMPLETE_MODULE_DATA_H
+
+/**
+ * struct CompleteModuleData - Complete private Module data
+ */
+struct CompleteModuleData
+{
+  int dummy;
+};
+
+#endif /* MUTT_COMPLETE_MODULE_DATA_H */
