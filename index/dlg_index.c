8e990d04327ffbd4ead192543ce68e3f79da5aee
Richard Russon <rich@flatcap.org>
2026-02-27 10:31:42 +0000

    index: fix crash in update_index_threaded()

--- index/dlg_index.c
+++ index/dlg_index.c
@@ -434,6 +434,8 @@ static void update_index_threaded(struct MailboxView *mv, enum MxStatus check, i
     for (int i = 0; i < m->msg_count; i++)
     {
       struct Email *e = m->emails[i];
+      if (!e)
+        continue;
 
       if ((e->limit_visited && e->visible) ||
           mutt_pattern_exec(SLIST_FIRST(mv->limit_pattern),
