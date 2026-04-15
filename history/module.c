fab3f716622350cabedcacc0d3ed4e08d4ef7e8b
Richard Russon <rich@flatcap.org>
2026-04-15 18:35:21 +0100

    history: move globals to HistoryModuleData
    - Move struct History definition to module_data.h
    - Add histories[] and old_size to struct HistoryModuleData
    - Enable init/cleanup in module.c
    - Update history.c functions to use mod_data
    - Pass old_size as parameter to init_history()

--- history/module.c
+++ history/module.c
@@ -42,8 +42,8 @@ extern struct ConfigDef HistoryVars[];
  */
 static bool history_init(struct NeoMutt *n)
 {
-  // struct HistoryModuleData *mod_data = MUTT_MEM_CALLOC(1, struct HistoryModuleData);
-  // neomutt_set_module_data(n, MODULE_ID_HISTORY, mod_data);
+  struct HistoryModuleData *mod_data = MUTT_MEM_CALLOC(1, struct HistoryModuleData);
+  neomutt_set_module_data(n, MODULE_ID_HISTORY, mod_data);
 
   return true;
 }
@@ -61,10 +61,10 @@ static bool history_config_define_variables(struct NeoMutt *n, struct ConfigSet 
  */
 static bool history_cleanup(struct NeoMutt *n)
 {
-  // struct HistoryModuleData *mod_data = neomutt_get_module_data(n, MODULE_ID_HISTORY);
-  // ASSERT(mod_data);
+  struct HistoryModuleData *mod_data = neomutt_get_module_data(n, MODULE_ID_HISTORY);
+  ASSERT(mod_data);
 
-  // FREE(&mod_data);
+  FREE(&mod_data);
   return true;
 }
 
